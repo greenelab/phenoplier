bf8db87882d2a470cde95e3d218347f80b54e1e3
Milton Pividori <miltondp@gmail.com>
2020-11-23 16:03:56 -0500

    Improve settings to specify N_JOBS

--- libs/settings.py
+++ libs/settings.py
@@ -10,16 +10,19 @@ also provides an alternative way to specify its value using an environment varia
 # Default paths
 #
 
-# Specifies the main directory where all data and results generated are stored. When
-# setting up the environment for the first time, input data will be automatically
-# downloaded into a subfolder of ROOT_DIR. If not specified, it defaults to the
-# 'phenoplier' subfolder in the temporary directory of the operating system (i.e.
-# '/tmp/phenoplier' in Unix systems).
+# Specifies the main directory where all data and results generated are stored.
+# When setting up the environment for the first time, input data will be
+# automatically downloaded into a subfolder of ROOT_DIR. If not specified, it
+# defaults to the 'phenoplier' subfolder in the temporary directory of the
+# operating system (i.e. '/tmp/phenoplier' in Unix systems).
+#
 # Environment variable: PHENOPLIER_ROOT_DIR
 ROOT_DIR = "phenoplier_data"
 
-# Specifies the directory where the manuscript git repository was cloned/downloaded
-# to. If None, manuscript figures and other related files will not be generated.
+# Specifies the directory where the manuscript git repository was
+# cloned/downloaded to. If None, manuscript figures and other related files will
+# not be generated.
+#
 # Environment variable: PHENOPLIER_MANUSCRIPT_DIR
 MANUSCRIPT_DIR = None
 
@@ -32,6 +35,7 @@ MANUSCRIPT_DIR = None
 # Default: half of available cores.
 N_JOBS = None
 
-# Amount of cores to use for low-computational tasks (IO, etc).
-# Default: all available cores.
+# Amount of cores to use for low-computational tasks (IO, etc). This number
+# could be higher than N_JOBS.
+# Default: same as N_JOBS.
 N_JOBS_HIGH = None
