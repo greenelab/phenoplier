b600c14ed2d4a32c5a812bf0451bcdc05019b480
Milton Pividori <miltondp@gmail.com>
2020-12-04 16:34:27 -0500

    Add tqdm dependency for progress bar support

--- environment/scripts/install_other_packages.sh
+++ environment/scripts/install_other_packages.sh
@@ -4,6 +4,10 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Fix tqdm with JupyterLab: https://github.com/tqdm/tqdm/issues/394#issuecomment-384743637
+jupyter nbextension enable --py widgetsnbextension
+
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
 
 #
 # R dependencies
