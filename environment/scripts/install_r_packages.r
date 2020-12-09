c166c9d8eb582196da486c2167e05e0cb13c2f35
Milton Pividori <miltondp@gmail.com>
2020-12-09 14:18:43 -0500

    Reorganize R dependencies installation
    * add clusterProfiler and org.Hs.eg.db
    * do not update packages automatically

--- environment/scripts/install_r_packages.r
+++ environment/scripts/install_r_packages.r
@@ -1,7 +1,6 @@
 # This script installs R packages. When installing BiocManager, the script updates all R packages
 # currently installed (options update=TRUE, ask=FALSE in BiocManager::install).
 
-library(devtools)
 
 default_repo = 'http://cran.us.r-project.org'
 
@@ -10,11 +9,12 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 BiocManager::install(version = "3.10", update=TRUE, ask=FALSE)
 
 # clusterProfiler
-#BiocManager::install("clusterProfiler", update=TRUE, ask=FALSE)
+BiocManager::install("clusterProfiler", update=FALSE, ask=FALSE)
 
 # org.Hs.eg.db
-#BiocManager::install("org.Hs.eg.db", update=TRUE, ask=FALSE)
+BiocManager::install("org.Hs.eg.db", update=FALSE, ask=FALSE)
 
 # PLIER
+library(devtools)
 install_github("wgmao/PLIER", ref="v0.1.4")
 
