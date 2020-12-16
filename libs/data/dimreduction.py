604b41d97da4e37d8b1bfc1b1d23f18d9943567e
Milton Pividori <miltondp@gmail.com>
2020-12-16 09:11:40 -0500

    Improve docstrings

--- libs/data/dimreduction.py
+++ libs/data/dimreduction.py
@@ -4,14 +4,18 @@ from IPython.display import display
 
 def get_data_proj(dim_red_method_obj, data: pd.DataFrame) -> pd.DataFrame:
     """
-    TODO: complete
+    It applies a dimensionality reduction method on a data set, preserving row
+    and column names.
 
     Args:
-        dim_red_method_obj:
-        data:
+        dim_red_method_obj: any object that has a fit_transform method (using
+            sklearn API) to perform dimensionality reduction. The __name__
+            attribute of this object is used to rename the columns of the final
+            data frame.
+        data: the data set where the dimensionality method will be applied to.
 
     Returns:
-
+        a new data frame with the data projected using the method provided.
     """
     method_name = type(dim_red_method_obj).__name__
     display(f'{method_name} object: {dim_red_method_obj}')
@@ -27,14 +31,14 @@ def get_data_proj(dim_red_method_obj, data: pd.DataFrame) -> pd.DataFrame:
 
 def get_pca_proj(data: pd.DataFrame, options: dict) -> pd.DataFrame:
     """
-    TODO: complete
+    Performs PCA dimensionality reduction with the given options.
 
     Args:
-        data:
-        options:
+        data: data where PCA will be applied to.
+        options: PCA options from sklearn's PCA implementation.
 
     Returns:
-
+        a new data frame with the PCA version of the data.
     """
     from sklearn.decomposition import PCA
 
@@ -44,17 +48,14 @@ def get_pca_proj(data: pd.DataFrame, options: dict) -> pd.DataFrame:
 
 def get_umap_proj(data: pd.DataFrame, options: dict) -> pd.DataFrame:
     """
-    Returns a UMAP representation of orig_data with the options specified
-    globally in DR_OPTIONS.
-
-    TDOO: complete
+    Returns a UMAP representation of data with the options specified in options.
 
     Args:
-        data:
-        options:
+        data: data where UMAP will be applied to.
+        options: UMAP options from umap-learn implementation.
 
     Returns:
-
+        a new data frame with the UMAP version of the data.
     """
     from umap import UMAP
 
