4ffb1a8e0c2b110c14927286070bc4de88935d30
Milton Pividori <miltondp@gmail.com>
2020-12-22 15:40:17 -0500

    Add notebook to empty data folder for predictions

---
+++ nbs/15_drug_disease_associations/py/000-data-empty_directory.py
@@ -0,0 +1,63 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py//py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] papermill={"duration": 0.044577, "end_time": "2020-12-18T22:38:21.345879", "exception": false, "start_time": "2020-12-18T22:38:21.301302", "status": "completed"} tags=[]
+# # Description
+
+# %% [markdown] papermill={"duration": 0.011764, "end_time": "2020-12-18T22:38:21.398073", "exception": false, "start_time": "2020-12-18T22:38:21.386309", "status": "completed"} tags=[]
+# It makes sure that the data directory for predictions is empty before these notebooks are run.
+#
+# Do I need this?
+
+# %% [markdown] papermill={"duration": 0.011799, "end_time": "2020-12-18T22:38:21.422136", "exception": false, "start_time": "2020-12-18T22:38:21.410337", "status": "completed"} tags=[]
+# # Modules loading
+
+# %% papermill={"duration": 0.022467, "end_time": "2020-12-18T22:38:21.456126", "exception": false, "start_time": "2020-12-18T22:38:21.433659", "status": "completed"} tags=[]
+# %load_ext autoreload
+# %autoreload 2
+
+# %% papermill={"duration": 0.192251, "end_time": "2020-12-18T22:38:21.659996", "exception": false, "start_time": "2020-12-18T22:38:21.467745", "status": "completed"} tags=[]
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+import conf
+
+# %% [markdown] papermill={"duration": 0.011416, "end_time": "2020-12-18T22:38:21.683356", "exception": false, "start_time": "2020-12-18T22:38:21.671940", "status": "completed"} tags=[]
+# # Settings
+
+# %%
+OUTPUT_DIR = conf.RESULTS["DRUG_DISEASE_ANALYSES"]
+display(OUTPUT_DIR)
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+# %%
+OUTPUT_DATA_DIR = Path(OUTPUT_DIR, "data")
+display(OUTPUT_DATA_DIR)
+OUTPUT_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+# %% [markdown]
+# # Remove previous predictions
+
+# %%
+current_prediction_files = OUTPUT_DATA_DIR.rglob("*.pkl")
+
+# %%
+for f in current_prediction_files:
+    display(f)
+    f.unlink()
+
+# %%
