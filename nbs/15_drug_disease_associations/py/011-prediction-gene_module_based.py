5f8efc259d97060565be6766bfb403d3cfe2fb1b
Milton Pividori <miltondp@gmail.com>
2020-12-23 16:26:10 -0500

    run notebooks with papermill

--- nbs/15_drug_disease_associations/py/011-prediction-gene_module_based.py
+++ nbs/15_drug_disease_associations/py/011-prediction-gene_module_based.py
@@ -13,20 +13,20 @@
 #     name: python3
 # ---
 
-# %% [markdown] papermill={"duration": 0.04089, "end_time": "2020-12-23T18:05:20.790112", "exception": false, "start_time": "2020-12-23T18:05:20.749222", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.036359, "end_time": "2020-12-23T19:50:41.165143", "exception": false, "start_time": "2020-12-23T19:50:41.128784", "status": "completed"} tags=[]
 # # Description
 
-# %% [markdown] papermill={"duration": 0.010506, "end_time": "2020-12-23T18:05:20.814922", "exception": false, "start_time": "2020-12-23T18:05:20.804416", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.009766, "end_time": "2020-12-23T19:50:41.189531", "exception": false, "start_time": "2020-12-23T19:50:41.179765", "status": "completed"} tags=[]
 # **TODO**
 
-# %% [markdown] papermill={"duration": 0.010014, "end_time": "2020-12-23T18:05:20.835300", "exception": false, "start_time": "2020-12-23T18:05:20.825286", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.00867, "end_time": "2020-12-23T19:50:41.206942", "exception": false, "start_time": "2020-12-23T19:50:41.198272", "status": "completed"} tags=[]
 # # Modules loading
 
-# %% papermill={"duration": 0.02428, "end_time": "2020-12-23T18:05:20.869748", "exception": false, "start_time": "2020-12-23T18:05:20.845468", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.020623, "end_time": "2020-12-23T19:50:41.236311", "exception": false, "start_time": "2020-12-23T19:50:41.215688", "status": "completed"} tags=[]
 # %load_ext autoreload
 # %autoreload 2
 
-# %% papermill={"duration": 0.196929, "end_time": "2020-12-23T18:05:21.077057", "exception": false, "start_time": "2020-12-23T18:05:20.880128", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.247588, "end_time": "2020-12-23T19:50:41.493393", "exception": false, "start_time": "2020-12-23T19:50:41.245805", "status": "completed"} tags=[]
 from IPython.display import display
 from pathlib import Path
 
@@ -34,27 +34,27 @@ import pandas as pd
 
 import conf
 
-# %% [markdown] papermill={"duration": 0.010457, "end_time": "2020-12-23T18:05:21.098286", "exception": false, "start_time": "2020-12-23T18:05:21.087829", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.008919, "end_time": "2020-12-23T19:50:41.511867", "exception": false, "start_time": "2020-12-23T19:50:41.502948", "status": "completed"} tags=[]
 # # Settings
 
-# %% papermill={"duration": 0.020419, "end_time": "2020-12-23T18:05:21.129046", "exception": false, "start_time": "2020-12-23T18:05:21.108627", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.018418, "end_time": "2020-12-23T19:50:41.539027", "exception": false, "start_time": "2020-12-23T19:50:41.520609", "status": "completed"} tags=[]
 # if True, then it doesn't check if result files already exist and runs everything again
 FORCE_RUN = False
 
-# %% papermill={"duration": 0.020341, "end_time": "2020-12-23T18:05:21.159804", "exception": false, "start_time": "2020-12-23T18:05:21.139463", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.019205, "end_time": "2020-12-23T19:50:41.567597", "exception": false, "start_time": "2020-12-23T19:50:41.548392", "status": "completed"} tags=[]
 PREDICTION_METHOD = "Module-based"
 
-# %% papermill={"duration": 0.025232, "end_time": "2020-12-23T18:05:21.196196", "exception": false, "start_time": "2020-12-23T18:05:21.170964", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.024172, "end_time": "2020-12-23T19:50:41.601289", "exception": false, "start_time": "2020-12-23T19:50:41.577117", "status": "completed"} tags=[]
 OUTPUT_DIR = conf.RESULTS["DRUG_DISEASE_ANALYSES"]
 display(OUTPUT_DIR)
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% papermill={"duration": 0.023756, "end_time": "2020-12-23T18:05:21.230855", "exception": false, "start_time": "2020-12-23T18:05:21.207099", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.023388, "end_time": "2020-12-23T19:50:41.634609", "exception": false, "start_time": "2020-12-23T19:50:41.611221", "status": "completed"} tags=[]
 # OUTPUT_DATA_DIR = Path(OUTPUT_DIR, "data")
 # display(OUTPUT_DATA_DIR)
 # OUTPUT_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% papermill={"duration": 0.032363, "end_time": "2020-12-23T18:05:21.275472", "exception": false, "start_time": "2020-12-23T18:05:21.243109", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.022741, "end_time": "2020-12-23T19:50:41.676575", "exception": false, "start_time": "2020-12-23T19:50:41.653834", "status": "completed"} tags=[]
 INPUT_DATA_DIR = Path(
     OUTPUT_DIR,
     "data",
@@ -63,68 +63,68 @@ INPUT_DATA_DIR = Path(
 display(INPUT_DATA_DIR)
 INPUT_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% papermill={"duration": 0.022844, "end_time": "2020-12-23T18:05:21.310394", "exception": false, "start_time": "2020-12-23T18:05:21.287550", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.022117, "end_time": "2020-12-23T19:50:41.708701", "exception": false, "start_time": "2020-12-23T19:50:41.686584", "status": "completed"} tags=[]
 OUTPUT_PREDICTIONS_DIR = Path(OUTPUT_DIR, "predictions")
 display(OUTPUT_PREDICTIONS_DIR)
 OUTPUT_PREDICTIONS_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% [markdown] papermill={"duration": 0.011243, "end_time": "2020-12-23T18:05:21.333015", "exception": false, "start_time": "2020-12-23T18:05:21.321772", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.009765, "end_time": "2020-12-23T19:50:41.728870", "exception": false, "start_time": "2020-12-23T19:50:41.719105", "status": "completed"} tags=[]
 # # Load PharmacotherapyDB gold standard
 
-# %% papermill={"duration": 0.021633, "end_time": "2020-12-23T18:05:21.366060", "exception": false, "start_time": "2020-12-23T18:05:21.344427", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.025208, "end_time": "2020-12-23T19:50:41.763784", "exception": false, "start_time": "2020-12-23T19:50:41.738576", "status": "completed"} tags=[]
 gold_standard = pd.read_pickle(
     Path(OUTPUT_DIR, "gold_standard.pkl"),
 )
 
-# %% papermill={"duration": 0.021866, "end_time": "2020-12-23T18:05:21.399127", "exception": false, "start_time": "2020-12-23T18:05:21.377261", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.021155, "end_time": "2020-12-23T19:50:41.796102", "exception": false, "start_time": "2020-12-23T19:50:41.774947", "status": "completed"} tags=[]
 display(gold_standard.shape)
 
-# %% papermill={"duration": 0.02686, "end_time": "2020-12-23T18:05:21.437229", "exception": false, "start_time": "2020-12-23T18:05:21.410369", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.024921, "end_time": "2020-12-23T19:50:41.831886", "exception": false, "start_time": "2020-12-23T19:50:41.806965", "status": "completed"} tags=[]
 display(gold_standard.head())
 
-# %% papermill={"duration": 0.021401, "end_time": "2020-12-23T18:05:21.470364", "exception": false, "start_time": "2020-12-23T18:05:21.448963", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.02118, "end_time": "2020-12-23T19:50:41.864638", "exception": false, "start_time": "2020-12-23T19:50:41.843458", "status": "completed"} tags=[]
 doids_in_gold_standard = set(gold_standard["trait"])
 
-# %% [markdown] papermill={"duration": 0.011701, "end_time": "2020-12-23T18:05:21.493765", "exception": false, "start_time": "2020-12-23T18:05:21.482064", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.010857, "end_time": "2020-12-23T19:50:41.886745", "exception": false, "start_time": "2020-12-23T19:50:41.875888", "status": "completed"} tags=[]
 # # Load LINCS
 
-# %% [markdown] papermill={"duration": 0.011496, "end_time": "2020-12-23T18:05:21.517009", "exception": false, "start_time": "2020-12-23T18:05:21.505513", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.010454, "end_time": "2020-12-23T19:50:41.907921", "exception": false, "start_time": "2020-12-23T19:50:41.897467", "status": "completed"} tags=[]
 # ## Projected data
 
-# %% papermill={"duration": 0.022224, "end_time": "2020-12-23T18:05:21.550584", "exception": false, "start_time": "2020-12-23T18:05:21.528360", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.021901, "end_time": "2020-12-23T19:50:41.940473", "exception": false, "start_time": "2020-12-23T19:50:41.918572", "status": "completed"} tags=[]
 input_file = Path(INPUT_DATA_DIR, "lincs-projection.pkl").resolve()
 
 display(input_file)
 
-# %% papermill={"duration": 0.026689, "end_time": "2020-12-23T18:05:21.589109", "exception": false, "start_time": "2020-12-23T18:05:21.562420", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.026636, "end_time": "2020-12-23T19:50:41.978518", "exception": false, "start_time": "2020-12-23T19:50:41.951882", "status": "completed"} tags=[]
 lincs_projection = pd.read_pickle(input_file)
 
-# %% papermill={"duration": 0.023879, "end_time": "2020-12-23T18:05:21.625255", "exception": false, "start_time": "2020-12-23T18:05:21.601376", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.024037, "end_time": "2020-12-23T19:50:42.015614", "exception": false, "start_time": "2020-12-23T19:50:41.991577", "status": "completed"} tags=[]
 display(lincs_projection.shape)
 
-# %% papermill={"duration": 0.03622, "end_time": "2020-12-23T18:05:21.675585", "exception": false, "start_time": "2020-12-23T18:05:21.639365", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.034451, "end_time": "2020-12-23T19:50:42.062781", "exception": false, "start_time": "2020-12-23T19:50:42.028330", "status": "completed"} tags=[]
 display(lincs_projection.head())
 
-# %% [markdown] papermill={"duration": 0.012484, "end_time": "2020-12-23T18:05:21.700768", "exception": false, "start_time": "2020-12-23T18:05:21.688284", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.012417, "end_time": "2020-12-23T19:50:42.087747", "exception": false, "start_time": "2020-12-23T19:50:42.075330", "status": "completed"} tags=[]
 # # Load S-PrediXcan
 
-# %% papermill={"duration": 0.225438, "end_time": "2020-12-23T18:05:21.938669", "exception": false, "start_time": "2020-12-23T18:05:21.713231", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.217486, "end_time": "2020-12-23T19:50:42.317086", "exception": false, "start_time": "2020-12-23T19:50:42.099600", "status": "completed"} tags=[]
 from entity import Trait
 
-# %% papermill={"duration": 0.025545, "end_time": "2020-12-23T18:05:21.979243", "exception": false, "start_time": "2020-12-23T18:05:21.953698", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.024685, "end_time": "2020-12-23T19:50:42.356631", "exception": false, "start_time": "2020-12-23T19:50:42.331946", "status": "completed"} tags=[]
 phenomexcan_input_file_list = [
     f
     for f in INPUT_DATA_DIR.glob("*.pkl")
     if f.name.startswith(("smultixcan-", "spredixcan-"))
 ]
 
-# %% papermill={"duration": 0.023138, "end_time": "2020-12-23T18:05:22.016221", "exception": false, "start_time": "2020-12-23T18:05:21.993083", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.02244, "end_time": "2020-12-23T19:50:42.392015", "exception": false, "start_time": "2020-12-23T19:50:42.369575", "status": "completed"} tags=[]
 display(len(phenomexcan_input_file_list))
 
-# %% [markdown] papermill={"duration": 0.01245, "end_time": "2020-12-23T18:05:22.042111", "exception": false, "start_time": "2020-12-23T18:05:22.029661", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.012838, "end_time": "2020-12-23T19:50:42.417450", "exception": false, "start_time": "2020-12-23T19:50:42.404612", "status": "completed"} tags=[]
 # # Predict drug-disease associations
 
-# %% papermill={"duration": 0.028271, "end_time": "2020-12-23T18:05:22.083712", "exception": false, "start_time": "2020-12-23T18:05:22.055441", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.623517, "end_time": "2020-12-23T19:50:43.053428", "exception": false, "start_time": "2020-12-23T19:50:42.429911", "status": "completed"} tags=[]
 from drug_disease import (
     predict_dotprod,
     predict_dotprod_neg,
@@ -134,7 +134,7 @@ from drug_disease import (
     predict_spearman_neg,
 )
 
-# %% papermill={"duration": 2056.463755, "end_time": "2020-12-23T18:39:38.560240", "exception": false, "start_time": "2020-12-23T18:05:22.096485", "status": "completed"} tags=[]
+# %% papermill={"duration": 4836.090114, "end_time": "2020-12-23T21:11:19.157026", "exception": false, "start_time": "2020-12-23T19:50:43.066912", "status": "completed"} tags=[]
 for phenomexcan_input_file in phenomexcan_input_file_list:
     print(phenomexcan_input_file.name)
 
@@ -204,4 +204,4 @@ for phenomexcan_input_file in phenomexcan_input_file_list:
 
     print("\n")
 
-# %% papermill={"duration": 0.112221, "end_time": "2020-12-23T18:39:38.786608", "exception": false, "start_time": "2020-12-23T18:39:38.674387", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.211738, "end_time": "2020-12-23T21:11:19.579207", "exception": false, "start_time": "2020-12-23T21:11:19.367469", "status": "completed"} tags=[]
--- nbs/15_drug_disease_associations/py/011-prediction-single_gene_based.py
+++ nbs/15_drug_disease_associations/py/011-prediction-single_gene_based.py
@@ -13,20 +13,20 @@
 #     name: python3
 # ---
 
-# %% [markdown] papermill={"duration": 0.039383, "end_time": "2020-12-23T18:05:20.790329", "exception": false, "start_time": "2020-12-23T18:05:20.750946", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.026465, "end_time": "2020-12-23T19:50:41.173233", "exception": false, "start_time": "2020-12-23T19:50:41.146768", "status": "completed"} tags=[]
 # # Description
 
-# %% [markdown] papermill={"duration": 0.010248, "end_time": "2020-12-23T18:05:20.814612", "exception": false, "start_time": "2020-12-23T18:05:20.804364", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.008954, "end_time": "2020-12-23T19:50:41.193904", "exception": false, "start_time": "2020-12-23T19:50:41.184950", "status": "completed"} tags=[]
 # **TODO**
 
-# %% [markdown] papermill={"duration": 0.009786, "end_time": "2020-12-23T18:05:20.834637", "exception": false, "start_time": "2020-12-23T18:05:20.824851", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.008741, "end_time": "2020-12-23T19:50:41.211367", "exception": false, "start_time": "2020-12-23T19:50:41.202626", "status": "completed"} tags=[]
 # # Modules loading
 
-# %% papermill={"duration": 0.022074, "end_time": "2020-12-23T18:05:20.866594", "exception": false, "start_time": "2020-12-23T18:05:20.844520", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.020906, "end_time": "2020-12-23T19:50:41.241264", "exception": false, "start_time": "2020-12-23T19:50:41.220358", "status": "completed"} tags=[]
 # %load_ext autoreload
 # %autoreload 2
 
-# %% papermill={"duration": 0.238689, "end_time": "2020-12-23T18:05:21.115690", "exception": false, "start_time": "2020-12-23T18:05:20.877001", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.241664, "end_time": "2020-12-23T19:50:41.492228", "exception": false, "start_time": "2020-12-23T19:50:41.250564", "status": "completed"} tags=[]
 from IPython.display import display
 from pathlib import Path
 
@@ -34,27 +34,27 @@ import pandas as pd
 
 import conf
 
-# %% [markdown] papermill={"duration": 0.010343, "end_time": "2020-12-23T18:05:21.136621", "exception": false, "start_time": "2020-12-23T18:05:21.126278", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.009209, "end_time": "2020-12-23T19:50:41.511289", "exception": false, "start_time": "2020-12-23T19:50:41.502080", "status": "completed"} tags=[]
 # # Settings
 
-# %% papermill={"duration": 0.020294, "end_time": "2020-12-23T18:05:21.166974", "exception": false, "start_time": "2020-12-23T18:05:21.146680", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.01881, "end_time": "2020-12-23T19:50:41.539028", "exception": false, "start_time": "2020-12-23T19:50:41.520218", "status": "completed"} tags=[]
 # if True, then it doesn't check if result files already exist and runs everything again
 FORCE_RUN = False
 
-# %% papermill={"duration": 0.020017, "end_time": "2020-12-23T18:05:21.197349", "exception": false, "start_time": "2020-12-23T18:05:21.177332", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.019014, "end_time": "2020-12-23T19:50:41.567599", "exception": false, "start_time": "2020-12-23T19:50:41.548585", "status": "completed"} tags=[]
 PREDICTION_METHOD = "Gene-based"
 
-# %% papermill={"duration": 0.026973, "end_time": "2020-12-23T18:05:21.234826", "exception": false, "start_time": "2020-12-23T18:05:21.207853", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.023785, "end_time": "2020-12-23T19:50:41.600953", "exception": false, "start_time": "2020-12-23T19:50:41.577168", "status": "completed"} tags=[]
 OUTPUT_DIR = conf.RESULTS["DRUG_DISEASE_ANALYSES"]
 display(OUTPUT_DIR)
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% papermill={"duration": 0.022135, "end_time": "2020-12-23T18:05:21.268833", "exception": false, "start_time": "2020-12-23T18:05:21.246698", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.023779, "end_time": "2020-12-23T19:50:41.634743", "exception": false, "start_time": "2020-12-23T19:50:41.610964", "status": "completed"} tags=[]
 # OUTPUT_DATA_DIR = Path(OUTPUT_DIR, "data")
 # display(OUTPUT_DATA_DIR)
 # OUTPUT_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% papermill={"duration": 0.025055, "end_time": "2020-12-23T18:05:21.305648", "exception": false, "start_time": "2020-12-23T18:05:21.280593", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.022631, "end_time": "2020-12-23T19:50:41.676613", "exception": false, "start_time": "2020-12-23T19:50:41.653982", "status": "completed"} tags=[]
 INPUT_DATA_DIR = Path(
     OUTPUT_DIR,
     "data",
@@ -63,68 +63,68 @@ INPUT_DATA_DIR = Path(
 display(INPUT_DATA_DIR)
 INPUT_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% papermill={"duration": 0.021082, "end_time": "2020-12-23T18:05:21.337916", "exception": false, "start_time": "2020-12-23T18:05:21.316834", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.020424, "end_time": "2020-12-23T19:50:41.707114", "exception": false, "start_time": "2020-12-23T19:50:41.686690", "status": "completed"} tags=[]
 OUTPUT_PREDICTIONS_DIR = Path(OUTPUT_DIR, "predictions")
 display(OUTPUT_PREDICTIONS_DIR)
 OUTPUT_PREDICTIONS_DIR.mkdir(parents=True, exist_ok=True)
 
-# %% [markdown] papermill={"duration": 0.010727, "end_time": "2020-12-23T18:05:21.359594", "exception": false, "start_time": "2020-12-23T18:05:21.348867", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.009936, "end_time": "2020-12-23T19:50:41.727668", "exception": false, "start_time": "2020-12-23T19:50:41.717732", "status": "completed"} tags=[]
 # # Load PharmacotherapyDB gold standard
 
-# %% papermill={"duration": 0.021616, "end_time": "2020-12-23T18:05:21.392509", "exception": false, "start_time": "2020-12-23T18:05:21.370893", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.020685, "end_time": "2020-12-23T19:50:41.758217", "exception": false, "start_time": "2020-12-23T19:50:41.737532", "status": "completed"} tags=[]
 gold_standard = pd.read_pickle(
     Path(OUTPUT_DIR, "gold_standard.pkl"),
 )
 
-# %% papermill={"duration": 0.021491, "end_time": "2020-12-23T18:05:21.425152", "exception": false, "start_time": "2020-12-23T18:05:21.403661", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.021437, "end_time": "2020-12-23T19:50:41.790924", "exception": false, "start_time": "2020-12-23T19:50:41.769487", "status": "completed"} tags=[]
 display(gold_standard.shape)
 
-# %% papermill={"duration": 0.02498, "end_time": "2020-12-23T18:05:21.461457", "exception": false, "start_time": "2020-12-23T18:05:21.436477", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.025095, "end_time": "2020-12-23T19:50:41.827547", "exception": false, "start_time": "2020-12-23T19:50:41.802452", "status": "completed"} tags=[]
 display(gold_standard.head())
 
-# %% papermill={"duration": 0.02083, "end_time": "2020-12-23T18:05:21.493934", "exception": false, "start_time": "2020-12-23T18:05:21.473104", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.021356, "end_time": "2020-12-23T19:50:41.861048", "exception": false, "start_time": "2020-12-23T19:50:41.839692", "status": "completed"} tags=[]
 doids_in_gold_standard = set(gold_standard["trait"])
 
-# %% [markdown] papermill={"duration": 0.011148, "end_time": "2020-12-23T18:05:21.516609", "exception": false, "start_time": "2020-12-23T18:05:21.505461", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.011051, "end_time": "2020-12-23T19:50:41.883598", "exception": false, "start_time": "2020-12-23T19:50:41.872547", "status": "completed"} tags=[]
 # # Load LINCS
 
-# %% [markdown] papermill={"duration": 0.010843, "end_time": "2020-12-23T18:05:21.538630", "exception": false, "start_time": "2020-12-23T18:05:21.527787", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.010577, "end_time": "2020-12-23T19:50:41.904997", "exception": false, "start_time": "2020-12-23T19:50:41.894420", "status": "completed"} tags=[]
 # ## Raw data
 
-# %% papermill={"duration": 0.021893, "end_time": "2020-12-23T18:05:21.572955", "exception": false, "start_time": "2020-12-23T18:05:21.551062", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.021823, "end_time": "2020-12-23T19:50:41.937362", "exception": false, "start_time": "2020-12-23T19:50:41.915539", "status": "completed"} tags=[]
 input_file = Path(INPUT_DATA_DIR, "lincs-data.pkl").resolve()
 
 display(input_file)
 
-# %% papermill={"duration": 0.050725, "end_time": "2020-12-23T18:05:21.635650", "exception": false, "start_time": "2020-12-23T18:05:21.584925", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.05252, "end_time": "2020-12-23T19:50:42.001671", "exception": false, "start_time": "2020-12-23T19:50:41.949151", "status": "completed"} tags=[]
 lincs_projection = pd.read_pickle(input_file)
 
-# %% papermill={"duration": 0.022481, "end_time": "2020-12-23T18:05:21.671018", "exception": false, "start_time": "2020-12-23T18:05:21.648537", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.022634, "end_time": "2020-12-23T19:50:42.037283", "exception": false, "start_time": "2020-12-23T19:50:42.014649", "status": "completed"} tags=[]
 display(lincs_projection.shape)
 
-# %% papermill={"duration": 0.034702, "end_time": "2020-12-23T18:05:21.718059", "exception": false, "start_time": "2020-12-23T18:05:21.683357", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.035095, "end_time": "2020-12-23T19:50:42.084412", "exception": false, "start_time": "2020-12-23T19:50:42.049317", "status": "completed"} tags=[]
 display(lincs_projection.head())
 
-# %% [markdown] papermill={"duration": 0.012171, "end_time": "2020-12-23T18:05:21.743039", "exception": false, "start_time": "2020-12-23T18:05:21.730868", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.011754, "end_time": "2020-12-23T19:50:42.108487", "exception": false, "start_time": "2020-12-23T19:50:42.096733", "status": "completed"} tags=[]
 # # Load S-PrediXcan
 
-# %% papermill={"duration": 0.224496, "end_time": "2020-12-23T18:05:21.980123", "exception": false, "start_time": "2020-12-23T18:05:21.755627", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.224618, "end_time": "2020-12-23T19:50:42.345643", "exception": false, "start_time": "2020-12-23T19:50:42.121025", "status": "completed"} tags=[]
 from entity import Trait
 
-# %% papermill={"duration": 0.023597, "end_time": "2020-12-23T18:05:22.017319", "exception": false, "start_time": "2020-12-23T18:05:21.993722", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.022572, "end_time": "2020-12-23T19:50:42.381817", "exception": false, "start_time": "2020-12-23T19:50:42.359245", "status": "completed"} tags=[]
 phenomexcan_input_file_list = [
     f
     for f in INPUT_DATA_DIR.glob("*.pkl")
     if f.name.startswith(("smultixcan-", "spredixcan-"))
 ]
 
-# %% papermill={"duration": 0.022527, "end_time": "2020-12-23T18:05:22.052846", "exception": false, "start_time": "2020-12-23T18:05:22.030319", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.022258, "end_time": "2020-12-23T19:50:42.417071", "exception": false, "start_time": "2020-12-23T19:50:42.394813", "status": "completed"} tags=[]
 display(len(phenomexcan_input_file_list))
 
-# %% [markdown] papermill={"duration": 0.013435, "end_time": "2020-12-23T18:05:22.078841", "exception": false, "start_time": "2020-12-23T18:05:22.065406", "status": "completed"} tags=[]
+# %% [markdown] papermill={"duration": 0.012297, "end_time": "2020-12-23T19:50:42.442121", "exception": false, "start_time": "2020-12-23T19:50:42.429824", "status": "completed"} tags=[]
 # # Predict drug-disease associations
 
-# %% papermill={"duration": 0.028271, "end_time": "2020-12-23T18:05:22.083712", "exception": false, "start_time": "2020-12-23T18:05:22.055441", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.594541, "end_time": "2020-12-23T19:50:43.049740", "exception": false, "start_time": "2020-12-23T19:50:42.455199", "status": "completed"} tags=[]
 from drug_disease import (
     predict_dotprod,
     predict_dotprod_neg,
@@ -134,7 +134,7 @@ from drug_disease import (
     predict_spearman_neg,
 )
 
-# %% papermill={"duration": 2059.56362, "end_time": "2020-12-23T18:39:41.691237", "exception": false, "start_time": "2020-12-23T18:05:22.127617", "status": "completed"} tags=[]
+# %% papermill={"duration": 5362.660602, "end_time": "2020-12-23T21:20:05.724677", "exception": false, "start_time": "2020-12-23T19:50:43.064075", "status": "completed"} tags=[]
 for phenomexcan_input_file in phenomexcan_input_file_list:
     print(phenomexcan_input_file.name)
 
@@ -210,4 +210,4 @@ for phenomexcan_input_file in phenomexcan_input_file_list:
 
     print("\n")
 
-# %% papermill={"duration": 0.113934, "end_time": "2020-12-23T18:39:41.917951", "exception": false, "start_time": "2020-12-23T18:39:41.804017", "status": "completed"} tags=[]
+# %% papermill={"duration": 0.216001, "end_time": "2020-12-23T21:20:06.153906", "exception": false, "start_time": "2020-12-23T21:20:05.937905", "status": "completed"} tags=[]
