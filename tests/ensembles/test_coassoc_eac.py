cbd3c836efe0d1cc3248f3f79f121efa5ffed72c
Milton Pividori <miltondp@gmail.com>
2021-01-04 16:29:40 -0500

    Convert unit tests to pytest format

--- tests/ensembles/test_coassoc_eac.py
+++ tests/ensembles/test_coassoc_eac.py
@@ -1,4 +1,3 @@
-import unittest
 import sklearn.datasets as datasets
 from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score as ari
@@ -10,217 +9,218 @@ iris_data = datasets.load_iris().data
 iris_ref = datasets.load_iris().target
 
 
-class CoassocEACTest(unittest.TestCase):
-    def test_get_ensemble_distance_matrix(self):
-        # Prepare
-        ensemble = np.array(
-            [
-                [1, 1, 1, 2],
-                [2, 2, 2, 3],
-                [1, 1, 2, 2],
-                [3, 1, 2, 2],
-                [1, 1, 3, 2],
-                [2, 1, 2, 1],
-            ]
-        )
+def test_get_ensemble_distance_matrix():
+    # Prepare
+    ensemble = np.array(
+        [
+            [1, 1, 1, 2],
+            [2, 2, 2, 3],
+            [1, 1, 2, 2],
+            [3, 1, 2, 2],
+            [1, 1, 3, 2],
+            [2, 1, 2, 1],
+        ]
+    )
 
-        ensemble_distance_matrix = get_ensemble_distance_matrix(ensemble)
-        assert ensemble_distance_matrix is not None
-        assert ensemble_distance_matrix.shape == (4, 4)
+    ensemble_distance_matrix = get_ensemble_distance_matrix(ensemble)
+    assert ensemble_distance_matrix is not None
+    assert ensemble_distance_matrix.shape == (4, 4)
 
-        np.testing.assert_array_equal(
-            np.around(ensemble_distance_matrix, 2),
-            np.around(
-                np.array(
+    np.testing.assert_array_equal(
+        np.around(ensemble_distance_matrix, 2),
+        np.around(
+            np.array(
+                [
                     [
-                        [
-                            0,
-                            2 / 6.0,
-                            3 / 6.0,
-                            6 / 6.0,
-                        ],
-                        [
-                            2 / 6.0,
-                            0,
-                            4 / 6.0,
-                            5 / 6.0,
-                        ],
-                        [3 / 6.0, 4 / 6.0, 0, 4 / 6.0],
-                        [
-                            6 / 6.0,
-                            5 / 6.0,
-                            4 / 6.0,
-                            0,
-                        ],
-                    ]
-                ),
-                2,
-            ),
-        )
-
-    def test_get_ensemble_distance_matrix_with_nan(self):
-        # Prepare
-        ensemble = np.array(
-            [
-                [1, 1, 1, 2],
-                [2, 2, 2, 3],
-                [np.nan, 1, 2, 2],
-                [3, 1, 2, 2],
-                [1, 1, 3, 2],
-                [2, 1, 2, 1],
-            ]
-        )
-
-        ensemble_distance_matrix = get_ensemble_distance_matrix(ensemble)
-        assert ensemble_distance_matrix is not None
-        assert ensemble_distance_matrix.shape == (4, 4)
-
-        np.testing.assert_array_equal(
-            np.around(ensemble_distance_matrix, 2),
-            np.around(
-                np.array(
+                        0,
+                        2 / 6.0,
+                        3 / 6.0,
+                        6 / 6.0,
+                    ],
                     [
-                        [
-                            0,
-                            2 / 5.0,
-                            2 / 5.0,
-                            5 / 5.0,
-                        ],
-                        [
-                            2 / 5.0,
-                            0,
-                            4 / 6.0,
-                            5 / 6.0,
-                        ],
-                        [2 / 5.0, 4 / 6.0, 0, 4 / 6.0],
-                        [
-                            5 / 5.0,
-                            5 / 6.0,
-                            4 / 6.0,
-                            0,
-                        ],
-                    ]
-                ),
-                2,
-            ),
-        )
-
-    def test_get_ensemble_distance_matrix_with_more_nan(self):
-        # Prepare
-        ensemble = np.array(
-            [
-                [1, 1, 1, 2],
-                [2, 2, 2, 3],
-                [np.nan, 1, 2, 2],
-                [3, np.nan, 2, 2],
-                [1, 1, 3, 2],
-                [2, 1, 2, 1],
-            ]
-        )
-
-        ensemble_distance_matrix = get_ensemble_distance_matrix(ensemble)
-        assert ensemble_distance_matrix is not None
-        assert ensemble_distance_matrix.shape == (4, 4)
-
-        np.testing.assert_array_equal(
-            np.around(ensemble_distance_matrix, 2),
-            np.around(
-                np.array(
+                        2 / 6.0,
+                        0,
+                        4 / 6.0,
+                        5 / 6.0,
+                    ],
+                    [3 / 6.0, 4 / 6.0, 0, 4 / 6.0],
                     [
-                        [
-                            0,
-                            1 / 4.0,
-                            2 / 5.0,
-                            5 / 5.0,
-                        ],
-                        [
-                            1 / 4.0,
-                            0,
-                            3 / 5.0,
-                            4 / 5.0,
-                        ],
-                        [2 / 5.0, 3 / 5.0, 0, 4 / 6.0],
-                        [
-                            5 / 5.0,
-                            4 / 5.0,
-                            4 / 6.0,
-                            0,
-                        ],
-                    ]
-                ),
-                2,
+                        6 / 6.0,
+                        5 / 6.0,
+                        4 / 6.0,
+                        0,
+                    ],
+                ]
             ),
-        )
+            2,
+        ),
+    )
 
-    def test_get_ensemble_distance_matrix_all_nan(self):
-        # Prepare
-        ensemble = np.array(
-            [
-                [1, np.nan, 1, 2],
-                [np.nan, 2, 2, 3],
-                [np.nan, 1, 2, 2],
-                [3, np.nan, 2, 2],
-                [np.nan, 1, 3, 2],
-                [np.nan, 1, 2, 1],
-            ]
-        )
 
-        ensemble_distance_matrix = get_ensemble_distance_matrix(ensemble)
-        assert ensemble_distance_matrix is not None
-        assert ensemble_distance_matrix.shape == (4, 4)
+def test_get_ensemble_distance_matrix_with_nan():
+    # Prepare
+    ensemble = np.array(
+        [
+            [1, 1, 1, 2],
+            [2, 2, 2, 3],
+            [np.nan, 1, 2, 2],
+            [3, 1, 2, 2],
+            [1, 1, 3, 2],
+            [2, 1, 2, 1],
+        ]
+    )
 
-        np.testing.assert_array_equal(
-            np.around(ensemble_distance_matrix, 2),
-            np.around(
-                np.array(
+    ensemble_distance_matrix = get_ensemble_distance_matrix(ensemble)
+    assert ensemble_distance_matrix is not None
+    assert ensemble_distance_matrix.shape == (4, 4)
+
+    np.testing.assert_array_equal(
+        np.around(ensemble_distance_matrix, 2),
+        np.around(
+            np.array(
+                [
                     [
-                        [
-                            0,
-                            np.nan,
-                            1 / 2.0,
-                            2 / 2.0,
-                        ],
-                        [
-                            np.nan,
-                            0,
-                            3 / 4.0,
-                            3 / 4.0,
-                        ],
-                        [1 / 2.0, 3 / 4.0, 0, 4 / 6.0],
-                        [
-                            2 / 2.0,
-                            3 / 4.0,
-                            4 / 6.0,
-                            0,
-                        ],
-                    ]
-                ),
-                2,
+                        0,
+                        2 / 5.0,
+                        2 / 5.0,
+                        5 / 5.0,
+                    ],
+                    [
+                        2 / 5.0,
+                        0,
+                        4 / 6.0,
+                        5 / 6.0,
+                    ],
+                    [2 / 5.0, 4 / 6.0, 0, 4 / 6.0],
+                    [
+                        5 / 5.0,
+                        5 / 6.0,
+                        4 / 6.0,
+                        0,
+                    ],
+                ]
             ),
-        )
+            2,
+        ),
+    )
 
-    def test_iris_ensemble_with_2to10(self):
-        # Prepare
-        np.random.seed(1)
 
-        ensemble = np.array(
-            [
-                KMeans(n_clusters=k, init="random", n_init=1).fit_predict(iris_data)
-                for k in range(2, 10)
-            ]
-        )
+def test_get_ensemble_distance_matrix_with_more_nan():
+    # Prepare
+    ensemble = np.array(
+        [
+            [1, 1, 1, 2],
+            [2, 2, 2, 3],
+            [np.nan, 1, 2, 2],
+            [3, np.nan, 2, 2],
+            [1, 1, 3, 2],
+            [2, 1, 2, 1],
+        ]
+    )
 
-        # Run
-        consensus_partition = eac(ensemble, 3)
+    ensemble_distance_matrix = get_ensemble_distance_matrix(ensemble)
+    assert ensemble_distance_matrix is not None
+    assert ensemble_distance_matrix.shape == (4, 4)
 
-        # Validate
-        assert len(np.unique(consensus_partition)) == 3
-        self.assertIsNotNone(consensus_partition)
-        self.assertEqual(150, len(consensus_partition))
+    np.testing.assert_array_equal(
+        np.around(ensemble_distance_matrix, 2),
+        np.around(
+            np.array(
+                [
+                    [
+                        0,
+                        1 / 4.0,
+                        2 / 5.0,
+                        5 / 5.0,
+                    ],
+                    [
+                        1 / 4.0,
+                        0,
+                        3 / 5.0,
+                        4 / 5.0,
+                    ],
+                    [2 / 5.0, 3 / 5.0, 0, 4 / 6.0],
+                    [
+                        5 / 5.0,
+                        4 / 5.0,
+                        4 / 6.0,
+                        0,
+                    ],
+                ]
+            ),
+            2,
+        ),
+    )
 
-        # Check that the ARI obtained by the consensus partition is
-        # greater than any ensemble member
-        for ensemble_member in ensemble:
-            self.assertGreaterEqual(
-                ari(iris_ref, consensus_partition), ari(iris_ref, ensemble_member)
-            )
+
+def test_get_ensemble_distance_matrix_all_nan():
+    # Prepare
+    ensemble = np.array(
+        [
+            [1, np.nan, 1, 2],
+            [np.nan, 2, 2, 3],
+            [np.nan, 1, 2, 2],
+            [3, np.nan, 2, 2],
+            [np.nan, 1, 3, 2],
+            [np.nan, 1, 2, 1],
+        ]
+    )
+
+    ensemble_distance_matrix = get_ensemble_distance_matrix(ensemble)
+    assert ensemble_distance_matrix is not None
+    assert ensemble_distance_matrix.shape == (4, 4)
+
+    np.testing.assert_array_equal(
+        np.around(ensemble_distance_matrix, 2),
+        np.around(
+            np.array(
+                [
+                    [
+                        0,
+                        np.nan,
+                        1 / 2.0,
+                        2 / 2.0,
+                    ],
+                    [
+                        np.nan,
+                        0,
+                        3 / 4.0,
+                        3 / 4.0,
+                    ],
+                    [1 / 2.0, 3 / 4.0, 0, 4 / 6.0],
+                    [
+                        2 / 2.0,
+                        3 / 4.0,
+                        4 / 6.0,
+                        0,
+                    ],
+                ]
+            ),
+            2,
+        ),
+    )
+
+
+def test_iris_ensemble_with_2to10():
+    # Prepare
+    np.random.seed(1)
+
+    ensemble = np.array(
+        [
+            KMeans(n_clusters=k, init="random", n_init=1).fit_predict(iris_data)
+            for k in range(2, 10)
+        ]
+    )
+
+    # Run
+    consensus_partition = eac(ensemble, 3)
+
+    # Validate
+    assert len(np.unique(consensus_partition)) == 3
+    assert consensus_partition is not None
+    assert len(consensus_partition) == 150
+
+    # Check that the ARI obtained by the consensus partition is
+    # greater than any ensemble member
+    for ensemble_member in ensemble:
+        assert ari(iris_ref, consensus_partition) >= ari(iris_ref, ensemble_member)
--- tests/ensembles/test_supraconsensus.py
+++ tests/ensembles/test_supraconsensus.py
@@ -1,4 +1,3 @@
-import unittest
 import numpy as np
 import sklearn.datasets as datasets
 from sklearn.metrics import adjusted_rand_score as ari
@@ -13,142 +12,144 @@ from clustering.ensemble import supraconsensus
 from clustering.ensemble import anmi
 
 
-class SupraconsensusTest(unittest.TestCase):
-    def test_iris(self):
-        # Prepare
-        np.random.seed(1)
+def test_iris():
+    # Prepare
+    np.random.seed(1)
 
-        data = datasets.load_iris().data
-        ref_part = datasets.load_iris().target
+    data = datasets.load_iris().data
+    ref_part = datasets.load_iris().target
 
-        ensemble = np.array(
-            [
-                KMeans(n_clusters=k, init="random", n_init=1).fit_predict(data)
-                for k in range(2, 20)
-            ]
-        )
+    ensemble = np.array(
+        [
+            KMeans(n_clusters=k, init="random", n_init=1).fit_predict(data)
+            for k in range(2, 20)
+        ]
+    )
 
-        # Run
-        consensus_results = supraconsensus(ensemble, 3)
-        consensus_part = consensus_results[0]
-        consensus_best_method = consensus_results[1]
+    # Run
+    consensus_results = supraconsensus(ensemble, 3)
+    consensus_part = consensus_results[0]
+    consensus_best_method = consensus_results[1]
 
-        # Validate
-        assert consensus_part is not None
-        assert consensus_part.shape == (150,)
-        assert len(np.unique(consensus_part)) == 3
-        assert ari(ref_part, consensus_part) >= 0.70
+    # Validate
+    assert consensus_part is not None
+    assert consensus_part.shape == (150,)
+    assert len(np.unique(consensus_part)) == 3
+    assert ari(ref_part, consensus_part) >= 0.70
 
-        assert consensus_best_method in ("eac_single", "eac_complete", "eac_average")
+    assert consensus_best_method in ("eac_single", "eac_complete", "eac_average")
 
-        # Check that the ARI obtained by the consensus partition is
-        # greater than any ensemble member
-        for ensemble_member in ensemble:
-            assert ari(ref_part, consensus_part) > ari(ref_part, ensemble_member)
+    # Check that the ARI obtained by the consensus partition is
+    # greater than any ensemble member
+    for ensemble_member in ensemble:
+        assert ari(ref_part, consensus_part) > ari(ref_part, ensemble_member)
 
-    def test_circles(self):
-        # Prepare
-        np.random.seed(1)
 
-        data, ref_part = datasets.make_circles(n_samples=150, factor=0.3, noise=0.05)
+def test_circles():
+    # Prepare
+    np.random.seed(1)
 
-        ensemble = np.array(
-            [
-                KMeans(n_clusters=k, init="random", n_init=1).fit_predict(data)
-                for k in range(2, 20)
-            ]
-        )
+    data, ref_part = datasets.make_circles(n_samples=150, factor=0.3, noise=0.05)
 
-        # Run
-        consensus_part = supraconsensus(ensemble, 2)[0]
+    ensemble = np.array(
+        [
+            KMeans(n_clusters=k, init="random", n_init=1).fit_predict(data)
+            for k in range(2, 20)
+        ]
+    )
 
-        # Validate
-        assert consensus_part is not None
-        assert consensus_part.shape == (150,)
-        assert len(np.unique(consensus_part)) == 2
-        assert ari(ref_part, consensus_part) >= 0.95
+    # Run
+    consensus_part = supraconsensus(ensemble, 2)[0]
 
-        # Check that the ARI obtained by the consensus partition is
-        # greater than any ensemble member
-        for ensemble_member in ensemble:
-            assert ari(ref_part, consensus_part) > ari(ref_part, ensemble_member)
+    # Validate
+    assert consensus_part is not None
+    assert consensus_part.shape == (150,)
+    assert len(np.unique(consensus_part)) == 2
+    assert ari(ref_part, consensus_part) >= 0.95
 
-    def test_anmi_max_eac(self):
-        # Prepare
-        np.random.seed(1)
+    # Check that the ARI obtained by the consensus partition is
+    # greater than any ensemble member
+    for ensemble_member in ensemble:
+        assert ari(ref_part, consensus_part) > ari(ref_part, ensemble_member)
 
-        data, ref_part = datasets.make_circles(n_samples=150, factor=0.3, noise=0.05)
 
-        ensemble = np.array(
-            [
-                KMeans(n_clusters=k, init="random", n_init=1).fit_predict(data)
-                for k in range(2, 10)
-            ]
-        )
+def test_anmi_max_eac():
+    # Prepare
+    np.random.seed(1)
 
-        # Run
-        consensus_part = supraconsensus(ensemble, 2)[0]
+    data, ref_part = datasets.make_circles(n_samples=150, factor=0.3, noise=0.05)
 
-        eac_single_part = eac_single(ensemble, 2)
-        eac_complete_part = eac_complete(ensemble, 2)
-        eac_average_part = eac_average(ensemble, 2)
+    ensemble = np.array(
+        [
+            KMeans(n_clusters=k, init="random", n_init=1).fit_predict(data)
+            for k in range(2, 10)
+        ]
+    )
 
-        # supraconsensus partition should be one of the others
-        assert (
-            ari(consensus_part, eac_single_part) == 1.0
-            or ari(consensus_part, eac_complete_part) == 1.0
-            or ari(consensus_part, eac_average_part) == 1.0
-        )
+    # Run
+    consensus_part = supraconsensus(ensemble, 2)[0]
 
-        # ANMI should be greater or equal for the supraconsensus
-        assert anmi(ensemble, consensus_part) >= anmi(ensemble, eac_single_part)
-        assert anmi(ensemble, consensus_part) >= anmi(ensemble, eac_complete_part)
-        assert anmi(ensemble, consensus_part) >= anmi(ensemble, eac_average_part)
+    eac_single_part = eac_single(ensemble, 2)
+    eac_complete_part = eac_complete(ensemble, 2)
+    eac_average_part = eac_average(ensemble, 2)
 
-    def test_supraconsensus_returns_stats(self):
-        # Prepare
-        np.random.seed(1)
+    # supraconsensus partition should be one of the others
+    assert (
+        ari(consensus_part, eac_single_part) == 1.0
+        or ari(consensus_part, eac_complete_part) == 1.0
+        or ari(consensus_part, eac_average_part) == 1.0
+    )
 
-        data = datasets.load_iris().data
-        ref_part = datasets.load_iris().target
+    # ANMI should be greater or equal for the supraconsensus
+    assert anmi(ensemble, consensus_part) >= anmi(ensemble, eac_single_part)
+    assert anmi(ensemble, consensus_part) >= anmi(ensemble, eac_complete_part)
+    assert anmi(ensemble, consensus_part) >= anmi(ensemble, eac_average_part)
 
-        ensemble = np.array(
-            [
-                KMeans(n_clusters=k, init="random", n_init=1).fit_predict(data)
-                for k in range(2, 20)
-            ]
-        )
 
-        consensus_methods = (eac_single, eac_complete, eac_average)
-        consensus_methods_names = (
-            "eac_single",
-            "eac_complete",
-            "eac_average",
-        )
+def test_supraconsensus_returns_stats():
+    # Prepare
+    np.random.seed(1)
 
-        # Run
-        consensus_part, max_method, max_value = supraconsensus(
-            ensemble, 3, methods=consensus_methods
-        )
+    data = datasets.load_iris().data
+    ref_part = datasets.load_iris().target
 
-        # Validate
-        assert consensus_part is not None
-        assert consensus_part.shape == (150,)
-        assert len(np.unique(consensus_part)) == 3
-        assert ari(ref_part, consensus_part) >= 0.70
+    ensemble = np.array(
+        [
+            KMeans(n_clusters=k, init="random", n_init=1).fit_predict(data)
+            for k in range(2, 20)
+        ]
+    )
 
-        assert max_method is not None
-        assert max_method in consensus_methods_names
+    consensus_methods = (eac_single, eac_complete, eac_average)
+    consensus_methods_names = (
+        "eac_single",
+        "eac_complete",
+        "eac_average",
+    )
 
-        assert max_value > 0.0
-        assert max_value < 1.0
+    # Run
+    consensus_part, max_method, max_value = supraconsensus(
+        ensemble, 3, methods=consensus_methods
+    )
 
-        # Check that the ARI obtained by the consensus partition is
-        # greater than any ensemble member
-        for ensemble_member in ensemble:
-            assert ari(ref_part, consensus_part) > ari(ref_part, ensemble_member)
+    # Validate
+    assert consensus_part is not None
+    assert consensus_part.shape == (150,)
+    assert len(np.unique(consensus_part)) == 3
+    assert ari(ref_part, consensus_part) >= 0.70
 
-        # check that the max method is correct
-        real_max_part = eval(f"{max_method}(ensemble, 3)")
-        # real_max_part = eval(f'hgpa(ensemble, 3)')
-        assert ari(real_max_part, consensus_part) == 1.0
+    assert max_method is not None
+    assert max_method in consensus_methods_names
+
+    assert max_value > 0.0
+    assert max_value < 1.0
+
+    # Check that the ARI obtained by the consensus partition is
+    # greater than any ensemble member
+    for ensemble_member in ensemble:
+        assert ari(ref_part, consensus_part) > ari(ref_part, ensemble_member)
+
+    # check that the max method is correct
+    real_max_part = eval(f"{max_method}(ensemble, 3)")
+    # real_max_part = eval(f'hgpa(ensemble, 3)')
+    assert ari(real_max_part, consensus_part) == 1.0
