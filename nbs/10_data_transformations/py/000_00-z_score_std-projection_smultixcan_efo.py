a7be23c328d1eb8cc970f51bf5c74843214b5a96
Milton Pividori <miltondp@gmail.com>
2021-01-06 14:46:45 -0500

    nbs/10_data_transformations: remove unnecessary metadata from notebooks

--- nbs/10_data_transformations/py/000_00-z_score_std-projection_smultixcan_efo.py
+++ nbs/10_data_transformations/py/000_00-z_score_std-projection_smultixcan_efo.py
@@ -1,6 +1,7 @@
 # ---
 # jupyter:
 #   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill
 #     formats: ipynb,py//py:percent
 #     text_representation:
 #       extension: .py
@@ -13,20 +14,20 @@
 #     name: python3
 # ---
 
-# %% [markdown] papermill={"duration": 0.011523, "end_time": "2020-11-30T18:31:20.953979", "exception": false, "start_time": "2020-11-30T18:31:20.942456", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Description
 
-# %% [markdown] papermill={"duration": 0.006771, "end_time": "2020-11-30T18:31:20.967901", "exception": false, "start_time": "2020-11-30T18:31:20.961130", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # It standardize (z-score) S-MultiXcan results projected into the MultiPLIER latent space.
 
-# %% [markdown] papermill={"duration": 0.00691, "end_time": "2020-11-30T18:31:20.981753", "exception": false, "start_time": "2020-11-30T18:31:20.974843", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Modules loading
 
-# %% papermill={"duration": 0.018399, "end_time": "2020-11-30T18:31:21.007191", "exception": false, "start_time": "2020-11-30T18:31:20.988792", "status": "completed"} tags=[]
+# %% tags=[]
 # %load_ext autoreload
 # %autoreload 2
 
-# %% papermill={"duration": 0.455238, "end_time": "2020-11-30T18:31:21.470327", "exception": false, "start_time": "2020-11-30T18:31:21.015089", "status": "completed"} tags=[]
+# %% tags=[]
 from pathlib import Path
 from IPython.display import display
 
@@ -36,13 +37,13 @@ from sklearn.preprocessing import scale
 
 import conf
 
-# %% [markdown] papermill={"duration": 0.007125, "end_time": "2020-11-30T18:31:21.485093", "exception": false, "start_time": "2020-11-30T18:31:21.477968", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Settings
 
-# %% [markdown] papermill={"duration": 0.007, "end_time": "2020-11-30T18:31:21.499356", "exception": false, "start_time": "2020-11-30T18:31:21.492356", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # ## Input data
 
-# %% papermill={"duration": 0.025765, "end_time": "2020-11-30T18:31:21.532259", "exception": false, "start_time": "2020-11-30T18:31:21.506494", "status": "completed"} tags=[]
+# %% tags=[]
 INPUT_FILEPATH = Path(
     conf.RESULTS["PROJECTIONS_DIR"],
     "projection-smultixcan-efo_partial-mashr-zscores.pkl",
@@ -52,54 +53,54 @@ display(INPUT_FILEPATH)
 input_filepath_stem = INPUT_FILEPATH.stem
 display(input_filepath_stem)
 
-# %% [markdown] papermill={"duration": 0.00849, "end_time": "2020-11-30T18:31:21.549078", "exception": false, "start_time": "2020-11-30T18:31:21.540588", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # ## Output folder
 
-# %% papermill={"duration": 0.020543, "end_time": "2020-11-30T18:31:21.577496", "exception": false, "start_time": "2020-11-30T18:31:21.556953", "status": "completed"} tags=[]
+# %% tags=[]
 # output dir for this notebook
 RESULTS_DIR = Path(conf.RESULTS["DATA_TRANSFORMATIONS_DIR"], "z_score_std").resolve()
 RESULTS_DIR.mkdir(parents=True, exist_ok=True)
 
 display(RESULTS_DIR)
 
-# %% [markdown] papermill={"duration": 0.00823, "end_time": "2020-11-30T18:31:21.594080", "exception": false, "start_time": "2020-11-30T18:31:21.585850", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Load input file
 
-# %% papermill={"duration": 0.033123, "end_time": "2020-11-30T18:31:21.635051", "exception": false, "start_time": "2020-11-30T18:31:21.601928", "status": "completed"} tags=[]
+# %% tags=[]
 data = pd.read_pickle(INPUT_FILEPATH).T
 
-# %% papermill={"duration": 0.021271, "end_time": "2020-11-30T18:31:21.665729", "exception": false, "start_time": "2020-11-30T18:31:21.644458", "status": "completed"} tags=[]
+# %% tags=[]
 display(data.shape)
 
-# %% papermill={"duration": 0.034551, "end_time": "2020-11-30T18:31:21.709652", "exception": false, "start_time": "2020-11-30T18:31:21.675101", "status": "completed"} tags=[]
+# %% tags=[]
 display(data.head())
 
-# %% [markdown] papermill={"duration": 0.009354, "end_time": "2020-11-30T18:31:21.728656", "exception": false, "start_time": "2020-11-30T18:31:21.719302", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # z-score standardization
 
-# %% papermill={"duration": 0.04679, "end_time": "2020-11-30T18:31:21.784465", "exception": false, "start_time": "2020-11-30T18:31:21.737675", "status": "completed"} tags=[]
+# %% tags=[]
 data_stats = data.iloc[:, :10].describe()
 display(data_stats)
 
-# %% papermill={"duration": 0.11351, "end_time": "2020-11-30T18:31:21.907840", "exception": false, "start_time": "2020-11-30T18:31:21.794330", "status": "completed"} tags=[]
+# %% tags=[]
 scaled_data = pd.DataFrame(
     data=scale(data), index=data.index.copy(), columns=data.columns.copy()
 )
 
-# %% papermill={"duration": 0.02348, "end_time": "2020-11-30T18:31:21.942449", "exception": false, "start_time": "2020-11-30T18:31:21.918969", "status": "completed"} tags=[]
+# %% tags=[]
 display(scaled_data.shape)
 
-# %% papermill={"duration": 0.035982, "end_time": "2020-11-30T18:31:21.989620", "exception": false, "start_time": "2020-11-30T18:31:21.953638", "status": "completed"} tags=[]
+# %% tags=[]
 display(scaled_data.head())
 
-# %% papermill={"duration": 0.050483, "end_time": "2020-11-30T18:31:22.051794", "exception": false, "start_time": "2020-11-30T18:31:22.001311", "status": "completed"} tags=[]
+# %% tags=[]
 scaled_data_stats = scaled_data.iloc[:, :10].describe()
 display(scaled_data_stats)
 
-# %% [markdown] papermill={"duration": 0.010906, "end_time": "2020-11-30T18:31:22.073630", "exception": false, "start_time": "2020-11-30T18:31:22.062724", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # ## Testing
 
-# %% papermill={"duration": 0.023383, "end_time": "2020-11-30T18:31:22.107636", "exception": false, "start_time": "2020-11-30T18:31:22.084253", "status": "completed"} tags=[]
+# %% tags=[]
 assert np.all(
     [
         np.isclose(scaled_data_stats.loc["mean", c], 0.0)
@@ -107,7 +108,7 @@ assert np.all(
     ]
 )
 
-# %% papermill={"duration": 0.023189, "end_time": "2020-11-30T18:31:22.142127", "exception": false, "start_time": "2020-11-30T18:31:22.118938", "status": "completed"} tags=[]
+# %% tags=[]
 assert np.all(
     [
         np.isclose(scaled_data_stats.loc["std", c], 1.0, atol=1e-03)
@@ -115,10 +116,10 @@ assert np.all(
     ]
 )
 
-# %% [markdown] papermill={"duration": 0.010762, "end_time": "2020-11-30T18:31:22.164014", "exception": false, "start_time": "2020-11-30T18:31:22.153252", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Save
 
-# %% papermill={"duration": 0.024102, "end_time": "2020-11-30T18:31:22.199112", "exception": false, "start_time": "2020-11-30T18:31:22.175010", "status": "completed"} tags=[]
+# %% tags=[]
 output_file = Path(
     RESULTS_DIR,
     f"z_score_std-{input_filepath_stem}.pkl",
@@ -126,7 +127,7 @@ output_file = Path(
 
 display(output_file)
 
-# %% papermill={"duration": 0.052456, "end_time": "2020-11-30T18:31:22.263115", "exception": false, "start_time": "2020-11-30T18:31:22.210659", "status": "completed"} tags=[]
+# %% tags=[]
 scaled_data.to_pickle(output_file)
 
-# %% papermill={"duration": 0.011676, "end_time": "2020-11-30T18:31:22.286862", "exception": false, "start_time": "2020-11-30T18:31:22.275186", "status": "completed"} tags=[]
+# %% tags=[]
--- nbs/10_data_transformations/py/000_01-pca.py
+++ nbs/10_data_transformations/py/000_01-pca.py
@@ -1,6 +1,7 @@
 # ---
 # jupyter:
 #   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill
 #     formats: ipynb,py//py:percent
 #     text_representation:
 #       extension: .py
@@ -13,20 +14,20 @@
 #     name: python3
 # ---
 
-# %% [markdown] papermill={"duration": 0.011683, "end_time": "2020-11-30T18:31:23.723335", "exception": false, "start_time": "2020-11-30T18:31:23.711652", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Description
 
-# %% [markdown] papermill={"duration": 0.007375, "end_time": "2020-11-30T18:31:23.738363", "exception": false, "start_time": "2020-11-30T18:31:23.730988", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # It gets the PCA transformation of an input file.
 
-# %% [markdown] papermill={"duration": 0.007433, "end_time": "2020-11-30T18:31:23.753677", "exception": false, "start_time": "2020-11-30T18:31:23.746244", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Modules loading
 
-# %% papermill={"duration": 0.018492, "end_time": "2020-11-30T18:31:23.779689", "exception": false, "start_time": "2020-11-30T18:31:23.761197", "status": "completed"} tags=[]
+# %% tags=[]
 # %load_ext autoreload
 # %autoreload 2
 
-# %% papermill={"duration": 0.584415, "end_time": "2020-11-30T18:31:24.372370", "exception": false, "start_time": "2020-11-30T18:31:23.787955", "status": "completed"} tags=[]
+# %% tags=[]
 from pathlib import Path
 from IPython.display import display
 
@@ -36,16 +37,16 @@ import seaborn as sns
 import conf
 from utils import generate_result_set_name
 
-# %% [markdown] papermill={"duration": 0.009842, "end_time": "2020-11-30T18:31:24.392652", "exception": false, "start_time": "2020-11-30T18:31:24.382810", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Settings
 
-# %% [markdown] papermill={"duration": 0.00862, "end_time": "2020-11-30T18:31:24.410732", "exception": false, "start_time": "2020-11-30T18:31:24.402112", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # ## Input data
 
-# %% papermill={"duration": 0.020189, "end_time": "2020-11-30T18:31:24.439057", "exception": false, "start_time": "2020-11-30T18:31:24.418868", "status": "completed"} tags=[]
+# %% tags=[]
 INPUT_FILEPATH_STEM = "projection-smultixcan-efo_partial-mashr-zscores"
 
-# %% papermill={"duration": 0.023337, "end_time": "2020-11-30T18:31:24.470745", "exception": false, "start_time": "2020-11-30T18:31:24.447408", "status": "completed"} tags=[]
+# %% tags=[]
 INPUT_FILEPATH = Path(
     conf.RESULTS["DATA_TRANSFORMATIONS_DIR"],
     "z_score_std",
@@ -56,20 +57,20 @@ display(INPUT_FILEPATH)
 input_filepath_stem = INPUT_FILEPATH.stem
 display(input_filepath_stem)
 
-# %% [markdown] papermill={"duration": 0.008226, "end_time": "2020-11-30T18:31:24.488089", "exception": false, "start_time": "2020-11-30T18:31:24.479863", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # ## Output folder
 
-# %% papermill={"duration": 0.021432, "end_time": "2020-11-30T18:31:24.517997", "exception": false, "start_time": "2020-11-30T18:31:24.496565", "status": "completed"} tags=[]
+# %% tags=[]
 # output dir for this notebook
 RESULTS_DIR = Path(conf.RESULTS["DATA_TRANSFORMATIONS_DIR"], "pca").resolve()
 RESULTS_DIR.mkdir(parents=True, exist_ok=True)
 
 display(RESULTS_DIR)
 
-# %% [markdown] papermill={"duration": 0.0088, "end_time": "2020-11-30T18:31:24.535866", "exception": false, "start_time": "2020-11-30T18:31:24.527066", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # ## PCA options
 
-# %% papermill={"duration": 0.021201, "end_time": "2020-11-30T18:31:24.565989", "exception": false, "start_time": "2020-11-30T18:31:24.544788", "status": "completed"} tags=[]
+# %% tags=[]
 # parameters of the dimentionality reduction steps
 DR_OPTIONS = {
     "n_components": 50,
@@ -77,49 +78,49 @@ DR_OPTIONS = {
     "random_state": 0,
 }
 
-# %% papermill={"duration": 0.021208, "end_time": "2020-11-30T18:31:24.596221", "exception": false, "start_time": "2020-11-30T18:31:24.575013", "status": "completed"} tags=[]
+# %% tags=[]
 # dictionary containing all options/settings (used to generate filenames)
 ALL_OPTIONS = DR_OPTIONS.copy()
 
 display(ALL_OPTIONS)
 
-# %% [markdown] papermill={"duration": 0.00903, "end_time": "2020-11-30T18:31:24.614354", "exception": false, "start_time": "2020-11-30T18:31:24.605324", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Load input file
 
-# %% papermill={"duration": 0.033326, "end_time": "2020-11-30T18:31:24.656646", "exception": false, "start_time": "2020-11-30T18:31:24.623320", "status": "completed"} tags=[]
+# %% tags=[]
 data = pd.read_pickle(INPUT_FILEPATH)
 
-# %% papermill={"duration": 0.023498, "end_time": "2020-11-30T18:31:24.690475", "exception": false, "start_time": "2020-11-30T18:31:24.666977", "status": "completed"} tags=[]
+# %% tags=[]
 display(data.shape)
 
-# %% papermill={"duration": 0.037456, "end_time": "2020-11-30T18:31:24.738706", "exception": false, "start_time": "2020-11-30T18:31:24.701250", "status": "completed"} tags=[]
+# %% tags=[]
 display(data.head())
 
-# %% [markdown] papermill={"duration": 0.010934, "end_time": "2020-11-30T18:31:24.760894", "exception": false, "start_time": "2020-11-30T18:31:24.749960", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # PCA
 
-# %% papermill={"duration": 0.021928, "end_time": "2020-11-30T18:31:24.793479", "exception": false, "start_time": "2020-11-30T18:31:24.771551", "status": "completed"} tags=[]
+# %% tags=[]
 from data.dimreduction import get_pca_proj
 
-# %% papermill={"duration": 0.698385, "end_time": "2020-11-30T18:31:25.502170", "exception": false, "start_time": "2020-11-30T18:31:24.803785", "status": "completed"} tags=[]
+# %% tags=[]
 dr_data = get_pca_proj(data, DR_OPTIONS)
 
-# %% papermill={"duration": 0.02372, "end_time": "2020-11-30T18:31:25.537039", "exception": false, "start_time": "2020-11-30T18:31:25.513319", "status": "completed"} tags=[]
+# %% tags=[]
 display(dr_data.shape)
 
-# %% papermill={"duration": 0.039962, "end_time": "2020-11-30T18:31:25.588307", "exception": false, "start_time": "2020-11-30T18:31:25.548345", "status": "completed"} tags=[]
+# %% tags=[]
 display(dr_data.head())
 
-# %% [markdown] papermill={"duration": 0.010926, "end_time": "2020-11-30T18:31:25.610594", "exception": false, "start_time": "2020-11-30T18:31:25.599668", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # ## Plot
 
-# %% papermill={"duration": 5.586547, "end_time": "2020-11-30T18:31:31.208070", "exception": false, "start_time": "2020-11-30T18:31:25.621523", "status": "completed"} tags=[]
+# %% tags=[]
 g = sns.pairplot(data=dr_data.iloc[:, :5])
 
-# %% [markdown] papermill={"duration": 0.016597, "end_time": "2020-11-30T18:31:31.243103", "exception": false, "start_time": "2020-11-30T18:31:31.226506", "status": "completed"} tags=[]
+# %% [markdown] tags=[]
 # # Save
 
-# %% papermill={"duration": 0.028239, "end_time": "2020-11-30T18:31:31.286166", "exception": false, "start_time": "2020-11-30T18:31:31.257927", "status": "completed"} tags=[]
+# %% tags=[]
 output_file = Path(
     RESULTS_DIR,
     generate_result_set_name(
@@ -129,7 +130,7 @@ output_file = Path(
 
 display(output_file)
 
-# %% papermill={"duration": 0.029247, "end_time": "2020-11-30T18:31:31.330604", "exception": false, "start_time": "2020-11-30T18:31:31.301357", "status": "completed"} tags=[]
+# %% tags=[]
 dr_data.to_pickle(output_file)
 
-# %% papermill={"duration": 0.014751, "end_time": "2020-11-30T18:31:31.360662", "exception": false, "start_time": "2020-11-30T18:31:31.345911", "status": "completed"} tags=[]
+# %% tags=[]
