81901c55b96d972bd30db1bab512dcfaa87be4d9
Milton Pividori <miltondp@gmail.com>
2021-01-12 12:15:21 -0500

    Switch to --sync for nb conversion script

--- scripts/convert_ipynb_to_py.sh
+++ scripts/convert_ipynb_to_py.sh
@@ -6,25 +6,8 @@ if [ -z "${NOTEBOOK}" ]; then
   exit 1
 fi
 
-nbs_folder="$( cd "$(dirname "$NOTEBOOK")" >/dev/null 2>&1 ; pwd -P )"
-nbs_base=${NOTEBOOK##*/}
-nbs_fext=${nbs_base##*.}
-nbs_xpref=${nbs_base%.*}
-
-echo $nbs_folder
-echo $nbs_base
-echo $nbs_fext
-echo $nbs_xpref
-
-output_folder="${nbs_folder}/py"
-mkdir -p $output_folder
-
-output_file="${output_folder}/${nbs_xpref}.py"
-
 jupytext \
-  --to auto:percent \
-  ${NOTEBOOK} \
-  --output ${output_file}
-
-black ${output_file}
+  --sync \
+  --pipe black \
+  ${NOTEBOOK}
 
