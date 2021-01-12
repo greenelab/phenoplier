cfe0353fba24a1f55b6c5aca4be3957f96ab2a87
Milton Pividori <miltondp@gmail.com>
2021-01-12 00:00:10 -0500

    Update conclusions of gamma value for spectral clustering exploration notebooks

--- nbs/15_cluster_analysis/pre_analysis/py/05_02-spectral_clustering-pca.py
+++ nbs/15_cluster_analysis/pre_analysis/py/05_02-spectral_clustering-pca.py
@@ -492,4 +492,7 @@ with sns.plotting_context("talk", font_scale=0.75), sns.axes_style(
     plt.grid(True)
     plt.tight_layout()
 
+# %% [markdown] tags=[]
+# **CONCLUSION:** We choose `1e-10` as the `gamma` parameter for this data version.
+
 # %% tags=[]
