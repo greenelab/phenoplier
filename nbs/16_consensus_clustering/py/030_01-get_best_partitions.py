94539482227c083dc0e4dade76461cc03962d8db
Milton Pividori <miltondp@gmail.com>
2021-01-14 09:39:16 -0500

    get_best_partitions: improve documentation and final plot

--- nbs/16_consensus_clustering/py/030_01-get_best_partitions.py
+++ nbs/16_consensus_clustering/py/030_01-get_best_partitions.py
@@ -18,7 +18,7 @@
 # # Description
 
 # %% [markdown]
-# TODO
+# It analyzes how consensus partitions generated before agree with the ensemble, and selects the best ones for downstream analyses.
 
 # %% [markdown]
 # # Modules loading
@@ -64,65 +64,6 @@ consensus_clustering_results.head()
 # %% [markdown]
 # # Explore best partition per k
 
-# %% [markdown]
-# ## ARI
-
-# %%
-_col0, _col1 = "ari_mean", "ari_median"
-_tmp = (
-    consensus_clustering_results.groupby("k")
-    .apply(lambda x: x.sort_values(_col0, ascending=False).head(1))
-    .sort_values(_col0, ascending=False)[["method", "k", _col0, _col1]]
-)
-display(_tmp.head(10))
-
-# %%
-with sns.plotting_context("talk", font_scale=0.75), sns.axes_style(
-    "whitegrid", {"grid.linestyle": "--"}
-):
-    fig, ax = plt.subplots(figsize=(12, 6))
-    ax = sns.pointplot(data=_tmp, x="k", y=_col0, ci=None, label="One")
-    ax = sns.pointplot(
-        data=_tmp, x="k", y=_col1, ci=None, color="red", label="Two", ax=ax
-    )
-    ax.set_ylabel("Agreement with ensemble (ARI)")
-    ax.set_xlabel("Number of clusters ($k$)")
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-    plt.legend(labels=["Mean", "Median"])
-    plt.grid(True)
-    plt.tight_layout()
-
-# %% [markdown]
-# ## NMI
-
-# %%
-_col0, _col1 = "nmi_mean", "nmi_median"
-_tmp = (
-    consensus_clustering_results.groupby("k")
-    .apply(lambda x: x.sort_values(_col0, ascending=False).head(1))
-    .sort_values(_col0, ascending=False)[["method", "k", _col0, _col1]]
-)
-display(_tmp.head(10))
-
-# %%
-with sns.plotting_context("talk", font_scale=0.75), sns.axes_style(
-    "whitegrid", {"grid.linestyle": "--"}
-):
-    fig, ax = plt.subplots(figsize=(12, 6))
-    ax = sns.pointplot(data=_tmp, x="k", y=_col0, ci=None, label="One")
-    ax = sns.pointplot(
-        data=_tmp, x="k", y=_col1, ci=None, color="red", label="Two", ax=ax
-    )
-    ax.set_ylabel("Agreement with ensemble (NMI)")
-    ax.set_xlabel("Number of clusters ($k$)")
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-    plt.legend(labels=["Mean", "Median"])
-    plt.grid(True)
-    plt.tight_layout()
-
-# %% [markdown]
-# ## AMI
-
 # %%
 _col0, _col1 = "ami_mean", "ami_median"
 _tmp = (
@@ -147,16 +88,6 @@ with sns.plotting_context("talk", font_scale=0.75), sns.axes_style(
     plt.legend(labels=["Mean", "Median"])
     plt.grid(True)
     plt.tight_layout()
-
-# %% [markdown]
-# **TODO: THIS NEEDS TO BE UPDATED**
-#
-# **It is interesting to see that:**
-# 1. AMI values (an adjusted-for-chance index) very similar to NMI (not adjusted-for-chance).
-# 1. All measures coincide that a `k` around 4 or 5 gives the maximum agreement with the ensemble.
-# 1. For ARI, partitions with more clusters have a smaller agreement with the ensemble, which is expected. This is not the case with AMI and NMI.
-#
-# **I will use ARI in the next analysis.**
 
 # %% [markdown]
 # # Select best partition per k
@@ -185,6 +116,31 @@ best_parts.head(10)
 best_parts.sort_values("k")
 
 # %% [markdown]
+# ## Select partitions with highest agreement
+
+# %% [markdown]
+# We do not expect all partitions with different `k` to be good ones. Thus, here I select the partitions with an ensemble agreement that pass a specified threshold (median).
+
+# %%
+best_parts_stats = best_parts[_measure_col].describe()
+display(best_parts_stats)
+
+# %%
+best_threshold = best_parts_stats["50%"]
+best_threshold_description = "Median"
+display(best_threshold)
+
+best_parts = best_parts.assign(
+    selected=best_parts[_measure_col].apply(lambda x: x > best_threshold)
+)
+
+# %%
+best_parts.shape
+
+# %%
+best_parts.head()
+
+# %% [markdown]
 # ## Save best partitions per k
 
 # %% tags=[]
@@ -199,33 +155,36 @@ best_parts.to_pickle(output_file)
 
 # %%
 plot_data = best_parts.reset_index()
-
-# %%
-plot_data.head()
-
-# %%
-plot_data_stats = plot_data[_measure_col].describe()
-display(plot_data_stats)
-
-# %%
-plot_data = plot_data.assign(greater_than_median=plot_data[_measure_col].apply(lambda x: x > plot_data_stats['50%']))
+display(plot_data.head(5))
 
 # %%
 with sns.plotting_context("talk", font_scale=0.75), sns.axes_style(
     "whitegrid", {"grid.linestyle": "--"}
 ), sns.color_palette("muted"):
     current_palette = iter(sns.color_palette())
-    
+
     fig, ax = plt.subplots(figsize=(12, 6))
-    ax = sns.pointplot(data=plot_data, x="k", y=_measure_col, color=next(current_palette))
-    ax.axhline(plot_data_stats["50%"], ls="--", color=next(current_palette))
+    ax = sns.pointplot(
+        data=plot_data, x="k", y=_measure_col, color=next(current_palette)
+    )
+    ax.axhline(
+        best_threshold,
+        ls="--",
+        color=next(current_palette),
+        label=best_threshold_description,
+    )
     ax.set_ylabel("Agreement with ensemble\n(Average AMI)")
     ax.set_xlabel("Number of clusters ($k$)")
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+    plt.legend()
     plt.grid(True)
     plt.tight_layout()
 
 # %%
-plot_data[plot_data["greater_than_median"]].sort_values("k")
+# this list shows the selected final partitions, and which methods achieved the highest agreement
+plot_data[plot_data["selected"]].sort_values("k")
+
+# %% [markdown]
+# Hierarchical clustering picks the best partitions for lower `k` values, whereas spectral clustering does it better for all the rest.
 
 # %%
