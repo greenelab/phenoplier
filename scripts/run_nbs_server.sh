75591c20c88eaeb42434dcd372537955bc066230
Milton Pividori <miltondp@gmail.com>
2021-01-26 09:49:20 -0500

    Export conf as env variables when running jupyter server

--- scripts/run_nbs_server.sh
+++ scripts/run_nbs_server.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# The script allows to run a JupyterLab server, listening to local connections only by default. If the only optional
-# argument is given, then the server will request a token from users and will listen to any address (*).
+# The script allows to run a JupyterLab server, listening to local connections
+# only by default. If the only optional argument is given, then the server will
+# request a token from users and will listen to any address (*).
 
 PORT=8892
 export PYTHONPATH=`pwd`/libs/
 echo "PYTHONPATH=${PYTHONPATH}"
+
+# export the PhenoPLIER configuration as environmental variables (this is
+# helpful if the configuration is needed outside python)
+eval `python libs/conf.py`
 
 IP="127.0.0.1"
 TOKEN=""
