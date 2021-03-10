403794e551cc691f0098a245d48dde7db365459d
Milton Pividori <miltondp@gmail.com>
2021-03-09 19:10:07 -0500

    Final adjustment for spectral clustering

--- nbs/15_cluster_analysis/pre_analysis/py/05_01-spectral_clustering-z_score_std.py
+++ nbs/15_cluster_analysis/pre_analysis/py/05_01-spectral_clustering-z_score_std.py
@@ -537,6 +537,6 @@ with sns.plotting_context("talk", font_scale=0.75), sns.axes_style(
 # # Conclusions
 
 # %% [markdown] tags=[]
-# **UPDATE** We choose `1e-10` as the `gamma` parameter for this data version.
+# We choose `1e-03` as the `gamma` parameter for this data version.
 
 # %% tags=[]
