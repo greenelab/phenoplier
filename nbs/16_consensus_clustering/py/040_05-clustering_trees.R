323b1d7f5dc48bbbb0c091fa488036e24f3c83da
Milton Pividori <miltondp@gmail.com>
2021-03-10 20:55:36 -0500

    Run notebooks with papermill

--- nbs/16_consensus_clustering/py/040_05-clustering_trees.R
+++ nbs/16_consensus_clustering/py/040_05-clustering_trees.R
@@ -14,67 +14,79 @@
 #     name: ir
 # ---
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Description
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # It uses the `clustree` package to generate clustering tree visualizations.
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Modules loading
 
-# %%
+# %% tags=[]
 library(clustree)
 library(tidyverse)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Settings
 
-# %%
+# %% tags=[]
 CLUSTERING_DIR <- Sys.getenv("PHENOPLIER_RESULTS_CLUSTERING_DIR")
 
-# %%
+# %% tags=[]
 CLUSTERING_DIR
 
-# %%
+# %% tags=[]
 CONSENSUS_CLUSTERING_DIR = file.path(CLUSTERING_DIR, "consensus_clustering")
 
-# %%
+# %% tags=[]
 CONSENSUS_CLUSTERING_DIR
 
-# %%
+# %% tags=[]
 MANUSCRIPT_FIGURES_DIR <- Sys.getenv("PHENOPLIER_MANUSCRIPT_FIGURES_DIR")
 
-# %%
+# %% tags=[]
+if (MANUSCRIPT_FIGURES_DIR == "") {
+    MANUSCRIPT_FIGURES_DIR = "/tmp"
+}
+
+# %% tags=[]
 MANUSCRIPT_FIGURES_DIR
 
-# %% [markdown]
+# %% tags=[]
+OUTPUT_FIG_DIR = file.path(MANUSCRIPT_FIGURES_DIR, "clustering")
+dir.create(OUTPUT_FIG_DIR, showWarnings = FALSE)
+
+# %% tags=[]
+OUTPUT_FIG_DIR
+
+# %% [markdown] tags=[]
 # # Load data
 
-# %%
+# %% tags=[]
 data <- read_tsv(file.path(CONSENSUS_CLUSTERING_DIR, "clustering_tree_data.tsv"))
 
-# %%
+# %% tags=[]
 dim(data)
 
-# %%
+# %% tags=[]
 head(data)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Plot clustering tree
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Plain
 
-# %%
+# %% tags=[]
 options(repr.plot.width = 20, repr.plot.height = 15)
 clustree(data, prefix = "k")
 
 ggsave(
-    file.path(MANUSCRIPT_FIGURES_DIR, "clustering", "clustering_tree.pdf"),
+    file.path(OUTPUT_FIG_DIR, "clustering_tree.pdf"),
     height=15,
     width=20,
     scale=1,
 )
 
-# %%
+# %% tags=[]
