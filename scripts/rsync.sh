02fb55a41920a09b90929a6332c2311d6048bd21
Milton Pividori <miltondp@gmail.com>
2021-03-16 14:05:51 -0400

    Add script description

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This script runs rsync with some common parameters to sync with a remote
+# machine running PhenoPLIER. For instance, it checks files' hashes instead of
+# timestamp, and excludes some huge files from recount2.
+
 rsync \
         -chavzP \
         --stats \
