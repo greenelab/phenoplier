09fc0b4e0cbef670b1bc15cb3e77d5c78bc67212
Milton Pividori <miltondp@gmail.com>
2021-03-25 15:53:45 -0400

    Improve code for analysis on single-genes

--- nbs/30_validation/crispr_screen/py/040_00-deg_genes-single_gene-decrease.py
+++ nbs/30_validation/crispr_screen/py/040_00-deg_genes-single_gene-decrease.py
@@ -28,44 +28,39 @@
 # %autoreload 2
 
 # %%
-import re
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from IPython.display import HTML
 
-from entity import Gene
-
-# from clustering.methods import ClusterInterpreter
-# from data.recount2 import LVAnalysis
+from entity import Trait, Gene
 from data.cache import read_data
-
-# from utils import generate_result_set_name
 import conf
 
 # %% [markdown]
 # # Settings
 
+# %%
+EXPERIMENT_NAME = "single_gene"
+
+LIPIDS_GENE_SET = "gene_set_decrease"
+LIPIDS_GENE_SET_QUERY = "(rank == -3) | (rank == -2)"
+
+# %%
+OUTPUT_DIR = Path(
+    conf.RESULTS["CRISPR_ANALYSES"]["BASE_DIR"], f"{EXPERIMENT_NAME}-{LIPIDS_GENE_SET}"
+)
+OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
+display(OUTPUT_DIR)
+
 # %% [markdown]
 # # Data loading
 
 # %% [markdown]
-# ## MultiPLIER Z matrix
-
-# %%
-multiplier_z = read_data(conf.MULTIPLIER["MODEL_Z_MATRIX_FILE"])
-
-# %%
-multiplier_z.shape
-
-# %%
-multiplier_z.head()
+# ## S-MultiXcan results
 
 # %% [markdown]
-# ## S-MultiXcan results
+# ### Load
 
 # %% tags=[]
 smultixcan_results_filename = conf.PHENOMEXCAN[
@@ -75,11 +70,10 @@ smultixcan_results_filename = conf.PHENOMEXCAN[
 display(smultixcan_results_filename)
 
 # %% tags=[]
-results_filename_stem = smultixcan_results_filename.stem
-display(results_filename_stem)
-
-# %% tags=[]
 smultixcan_results = pd.read_pickle(smultixcan_results_filename)
+
+# %% [markdown]
+# ### Rename genes and remove repeated ones
 
 # %% tags=[]
 smultixcan_results = smultixcan_results.rename(index=Gene.GENE_ID_TO_NAME_MAP)
@@ -98,8 +92,10 @@ smultixcan_results.shape
 # %% tags=[]
 smultixcan_results.head()
 
+# %% [markdown]
+# ### Standardize by trait
+
 # %% tags=[]
-# standardize
 _tmp = smultixcan_results.apply(lambda x: x / x.sum())
 
 # %% tags=[]
@@ -142,14 +138,10 @@ assert (
 smultixcan_results = _tmp
 
 # %% [markdown]
-# ## fastENLOC results
-
-# %% tags=[]
-# input_filename = conf.PHENOMEXCAN["FASTENLOC_EFO_PARTIAL_TORUS_RCP_FILE"]
-# display(input_filename)
+# ## Differentially expressed genes
 
 # %% [markdown]
-# ## Differentially expressed genes
+# ### Load
 
 # %%
 input_filepath = Path(conf.CRISPR["BASE_DIR"], "lipid_DEG.csv")
@@ -157,18 +149,6 @@ display(input_filepath)
 
 # %%
 deg_genes = pd.read_csv(input_filepath)
-
-# %%
-# _bool_cols = [c for c in deg_genes.columns if c.endswith(".DEG")]
-
-# %%
-# _bool_cols
-
-# %%
-# deg_genes[_bool_cols] = deg_genes[_bool_cols].astype(str)
-
-# %%
-# deg_genes.dtypes
 
 # %%
 deg_genes.shape
@@ -180,63 +160,43 @@ deg_genes.head()
 # ### Select gene set
 
 # %%
-df = deg_genes.query("(rank == -3) | (rank == -2)")
-# df = deg_genes.query("(rank == -3)")
-
-# %%
-# assert df.shape[0] == n_exp_genes
+df = deg_genes.query(LIPIDS_GENE_SET_QUERY)
 
 # %%
 df.shape
 
 # %%
 df_genes = df["gene_name"].unique().tolist()
-# assert len(df_genes) == n_exp_genes
 
 display(len(df_genes))
 display(df_genes[:10])
 
+assert len(df_genes) == 96
+
 # %%
 # keep genes present in S-MultiXcan results
 df_genes_present = smultixcan_results.index.intersection(df_genes).tolist()
+
 display(len(df_genes_present))
 display(df_genes_present[:10])
 
-# %%
-# comment out if want to further filter by MultiPLIER models
-
-# # keep only those genes present in the MultiPLIER models
-# df_genes_present = multiplier_z.index.intersection(df_genes).tolist()
-# display(len(df_genes_present))
-# display(df_genes_present[:10])
+assert len(df_genes_present) == 85
 
 # %% [markdown]
-# # Significant threshold
-
-# %% [markdown]
-# # Look at individual genes
-
-# %%
-smultixcan_results.loc[df_genes_present[0]].sort_values(ascending=False).head(20)
-
-# %% [markdown]
-# # Analysis
+# # Get top traits
 
 # %%
 traits = []
 
 for g in df_genes_present:
     _tmp = smultixcan_results.loc[g]
-
-    #     _tmp = _tmp.head(20)
-
     _tmp = _tmp[_tmp > 0.0].sort_values(ascending=False)
 
     #     _tmp = _tmp.head(50)
     traits.append(_tmp)
 
 # %%
-_tmp = (
+traits_df = (
     pd.concat(traits)
     .reset_index()
     .groupby("index")
@@ -246,7 +206,40 @@ _tmp = (
 ).rename(columns={"index": "trait", 0: "value"})
 
 # %%
-top_traits = _tmp.head(100)
+# add trait category
+trait_code_to_trait_obj = [
+    Trait.get_trait(full_code=t)
+    if not Trait.is_efo_label(t)
+    else Trait.get_traits_from_efo(t)
+    for t in traits_df["trait"]
+]
+
+# %%
+traits_df = traits_df.assign(
+    category=[
+        t.category if not isinstance(t, list) else t[0].category
+        for t in trait_code_to_trait_obj
+    ]
+)
+
+# %%
+traits_df.shape
+
+# %%
+traits_df.head()
+
+# %%
+output_file = Path(OUTPUT_DIR, "traits.pkl").resolve()
+display(output_file)
+
+# %%
+traits_df.to_pickle(output_file)
+
+# %% [markdown]
+# # Summary
+
+# %%
+top_traits = traits_df.head(100)
 
 # %%
 with pd.option_context(
@@ -258,60 +251,7 @@ with pd.option_context(
 # # Summary using trait categories
 
 # %%
-from entity import Trait
-
-# %%
-trait_code_to_trait_obj = [
-    Trait.get_trait(full_code=t)
-    if not Trait.is_efo_label(t)
-    else Trait.get_traits_from_efo(t)
-    for t in top_traits["trait"]
-]
-
-# %%
-top_traits = top_traits.assign(
-    category=[
-        t.category
-        if not isinstance(t, list)
-        else t[0].category  # FIXME just taking the first one
-        for t in trait_code_to_trait_obj
-    ]
-)
-
-# for t in trait_code_to_trait_obj:
-#     _tmp = multiplier_model_summary[
-#         (multiplier_model_summary["LV index"] == lv[2:])
-#         & (
-#             (multiplier_model_summary["FDR"] < 0.05)
-#             | (multiplier_model_summary["AUC"] > 0.75)
-#         )
-#     ]
-#     pathways.extend(_tmp["pathway"].tolist())
-
-#     _tmp = phenomexcan_data[lv]
-#     _tmp = _tmp[_tmp > 0.0].sort_values(ascending=False)
-
-#     _tmp = _tmp.head(50)
-
-#     _tmp = _tmp.reset_index().rename(columns={lv: "lv", "index": "trait"})
-
-#     _tmp["trait_category"] = [
-#         trait_code_to_trait_obj[t].category
-#         if not isinstance(trait_code_to_trait_obj[t], list)
-#         else trait_code_to_trait_obj[t][0].category
-#         for t in _tmp["trait"]
-#     ]
-
-#     traits.append(_tmp)
-
-# %%
-top_traits.shape
-
-# %%
-top_traits.head()
-
-# %%
-_tmp = (
+top_traits_categories = (
     top_traits.groupby("category")
     .mean()
     .sort_values("value", ascending=False)
@@ -319,16 +259,16 @@ _tmp = (
 )
 
 # %%
-_tmp.head()
+top_traits_categories.head()
 
 # %%
 with pd.option_context(
     "display.max_rows", None, "display.max_columns", None, "max_colwidth", None
 ):
-    display(_tmp)
+    display(top_traits_categories)
 
 # %%
-for row_idx, row in _tmp.iterrows():
+for row_idx, row in top_traits_categories.iterrows():
     category = row["category"]
     display(HTML(f"<h2>{category}</h2>"))
 
