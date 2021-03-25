db9ef7aa93b599faa1beaa27f443c5eaa3f0ff81
Milton Pividori <miltondp@gmail.com>
2021-03-25 16:02:15 -0400

    Run notebooks with papermill

--- nbs/30_validation/crispr_screen/py/030_20-deg_genes-lvs-decrease.py
+++ nbs/30_validation/crispr_screen/py/030_20-deg_genes-lvs-decrease.py
@@ -14,20 +14,20 @@
 #     name: python3
 # ---
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Description
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # TODO
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Modules loading
 
-# %%
+# %% tags=[]
 # %load_ext autoreload
 # %autoreload 2
 
-# %%
+# %% tags=[]
 from pathlib import Path
 
 import pandas as pd
@@ -37,27 +37,27 @@ from entity import Trait
 from data.cache import read_data
 import conf
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Settings
 
-# %%
+# %% tags=[]
 EXPERIMENT_NAME = "lv"
 LIPIDS_GENE_SET = "gene_set_decrease"
 
-# %%
+# %% tags=[]
 OUTPUT_DIR = Path(
     conf.RESULTS["CRISPR_ANALYSES"]["BASE_DIR"], f"{EXPERIMENT_NAME}-{LIPIDS_GENE_SET}"
 )
 OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 display(OUTPUT_DIR)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Data loading
 
 # %% [markdown] tags=[]
 # ## PhenomeXcan projection
 
-# %%
+# %% tags=[]
 input_filepath = Path(
     conf.RESULTS["PROJECTIONS_DIR"],
     "projection-smultixcan-efo_partial-mashr-zscores.pkl",
@@ -73,69 +73,69 @@ phenomexcan_data.shape
 # %% tags=[]
 phenomexcan_data.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## LVs enrichment on DEG from CRISPR screen
 
-# %%
+# %% tags=[]
 deg_enrich = pd.read_csv(
     Path(conf.RESULTS["CRISPR_ANALYSES"]["BASE_DIR"], "fgsea-all_lvs.tsv").resolve(),
     sep="\t",
 )
 
-# %%
+# %% tags=[]
 deg_enrich.shape
 
-# %%
+# %% tags=[]
 deg_enrich.head()
 
-# %%
+# %% tags=[]
 deg_enrich_max_idx = deg_enrich.groupby(["lv", "pathway"])["padj"].idxmax()
 
-# %%
+# %% tags=[]
 deg_enrich = deg_enrich.loc[deg_enrich_max_idx].reset_index(drop=True)
 display(deg_enrich.shape)
 display(deg_enrich.head())
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## MultiPLIER summary
 
-# %%
+# %% tags=[]
 multiplier_model_summary = read_data(conf.MULTIPLIER["MODEL_SUMMARY_FILE"])
 
-# %%
+# %% tags=[]
 multiplier_model_summary.shape
 
-# %%
+# %% tags=[]
 multiplier_model_summary.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Analysis
 
-# %%
+# %% tags=[]
 df = deg_enrich[
     deg_enrich["pathway"].isin((LIPIDS_GENE_SET,)) & (deg_enrich["padj"] < 0.05)
 ].sort_values("padj", ascending=True)
 
-# %%
+# %% tags=[]
 df.shape
 
-# %%
+# %% tags=[]
 df
 
-# %%
+# %% tags=[]
 important_lvs = df["lv"].unique()
 
-# %%
+# %% tags=[]
 display(important_lvs.shape)
 assert important_lvs.shape[0] == 24
 
-# %%
+# %% tags=[]
 important_lvs
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Get top traits and pathways
 
-# %%
+# %% tags=[]
 pathways = []
 traits = []
 
@@ -155,24 +155,24 @@ for lv in important_lvs:
     #     _tmp = _tmp.head(50)
     traits.append(_tmp)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Pathways
 
-# %%
+# %% tags=[]
 pathways_df = pd.Series(pathways).value_counts()
 display(pathways_df)
 
-# %%
+# %% tags=[]
 output_file = Path(OUTPUT_DIR, "pathways_counts.pkl").resolve()
 display(output_file)
 
-# %%
+# %% tags=[]
 pathways_df.to_pickle(output_file)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Traits
 
-# %%
+# %% tags=[]
 traits_df = (
     pd.concat(traits)
     .reset_index()
@@ -182,7 +182,7 @@ traits_df = (
     .reset_index()
 ).rename(columns={"index": "trait", 0: "value"})
 
-# %%
+# %% tags=[]
 # add trait category
 trait_code_to_trait_obj = [
     Trait.get_trait(full_code=t)
@@ -191,7 +191,7 @@ trait_code_to_trait_obj = [
     for t in traits_df["trait"]
 ]
 
-# %%
+# %% tags=[]
 traits_df = traits_df.assign(
     category=[
         t.category if not isinstance(t, list) else t[0].category
@@ -199,35 +199,35 @@ traits_df = traits_df.assign(
     ]
 )
 
-# %%
+# %% tags=[]
 traits_df.shape
 
-# %%
+# %% tags=[]
 traits_df.head()
 
-# %%
+# %% tags=[]
 output_file = Path(OUTPUT_DIR, "traits.pkl").resolve()
 display(output_file)
 
-# %%
+# %% tags=[]
 traits_df.to_pickle(output_file)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Summary
 
-# %%
+# %% tags=[]
 top_traits = traits_df.head(100)
 
-# %%
+# %% tags=[]
 with pd.option_context(
     "display.max_rows", None, "display.max_columns", None, "max_colwidth", None
 ):
     display(top_traits)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Summary using trait categories
 
-# %%
+# %% tags=[]
 top_traits_categories = (
     top_traits.groupby("category")
     .mean()
@@ -235,16 +235,16 @@ top_traits_categories = (
     .reset_index()
 )
 
-# %%
+# %% tags=[]
 top_traits_categories.head()
 
-# %%
+# %% tags=[]
 with pd.option_context(
     "display.max_rows", None, "display.max_columns", None, "max_colwidth", None
 ):
     display(top_traits_categories)
 
-# %%
+# %% tags=[]
 for row_idx, row in top_traits_categories.iterrows():
     category = row["category"]
     display(HTML(f"<h2>{category}</h2>"))
@@ -257,4 +257,4 @@ for row_idx, row in top_traits_categories.iterrows():
     )
     display(_df)
 
-# %%
+# %% tags=[]
