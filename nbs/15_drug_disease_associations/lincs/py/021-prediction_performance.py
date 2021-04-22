e79168641ad31cbbf1a6e7bdae8e9e5c87ee2fff
Milton Pividori <miltondp@gmail.com>
2021-04-22 15:34:28 -0400

    Prediction performance: documentation and code clean up

--- nbs/15_drug_disease_associations/lincs/py/021-prediction_performance.py
+++ nbs/15_drug_disease_associations/lincs/py/021-prediction_performance.py
@@ -18,7 +18,7 @@
 # # Description
 
 # %% [markdown] tags=[]
-# **TODO**
+# This notebook reads the prediction results generated with the `011-prediction-*` notebooks and computes the final performance measures using the gold standard (PharmacotherapyDB).
 
 # %% [markdown] tags=[]
 # # Modules loading
@@ -41,7 +41,6 @@ import conf
 
 # %% tags=[]
 N_TISSUES = 49
-# N_TISSUES = 1
 N_THRESHOLDS = 5
 N_PREDICTIONS = 646
 
@@ -82,6 +81,8 @@ gold_standard["true_class"].value_counts(normalize=True)
 from collections import defaultdict
 
 # %% tags=[]
+# get all prediction files
+
 current_prediction_files = list(OUTPUT_PREDICTIONS_DIR.glob("*.h5"))
 display(len(current_prediction_files))
 
@@ -93,31 +94,35 @@ assert len(current_prediction_files) == 2 * (
 current_prediction_files[:5]
 
 # %% tags=[]
+# iterate for each prediction file and perform some preprocessing
+# each prediction file (.h5) has the predictions of one method (either module-based
+# or gene-based) for all drug-disease pairs across all S-PrediXcan tissues
+
 predictions = []
 
 for f in tqdm(current_prediction_files, ncols=100):
-    # FIXME: it shouldn't be necessary to include this anymore
-    # exclude S-MultiXcan results, since they have no direction of effect
-    if f.name.startswith("smultixcan-"):
-        continue
-
+    # get predictions and merge with gold standard, keeping only the drug-disease pairs present there
     prediction_data = pd.read_hdf(f, key="prediction")
     prediction_data = pd.merge(
         prediction_data, gold_standard, on=["trait", "drug"], how="inner"
     )
 
-    metadata = pd.read_hdf(f, key="metadata")
-
+    # transform scores into ranks, and change the type of columns to save memory
     prediction_data["score"] = prediction_data["score"].rank()
     prediction_data["trait"] = prediction_data["trait"].astype("category")
     prediction_data["drug"] = prediction_data["drug"].astype("category")
 
+    # read metadata
+    metadata = pd.read_hdf(f, key="metadata")
+    
+    # add the "method" column
     prediction_data = prediction_data.assign(method=metadata.method.values[0])
     prediction_data["method"] = prediction_data["method"].astype("category")
 
+    # add the "n_top_genes" columns, which indicates the top genes/LVs used for this prediction
     prediction_data = prediction_data.assign(n_top_genes=metadata.n_top_genes.values[0])
-    #     prediction_data["n_top_genes"] = prediction_data["data"].astype("category")
 
+    # add the "data" column, which has the tissue name
     prediction_data = prediction_data.assign(data=metadata.data.values[0])
     prediction_data["data"] = prediction_data["data"].astype("category")
 
@@ -129,13 +134,29 @@ assert np.all(pred.shape[0] == N_PREDICTIONS for pred in predictions)
 # %% tags=[]
 predictions = pd.concat(predictions, ignore_index=True)
 
+
+# %% tags=[]
+# extract the tissue name from the "data" column
+
+def _get_tissue(x):
+    if x.endswith("-projection"):
+        return x.split("spredixcan-mashr-zscores-")[1].split("-projection")[0]
+    else:
+        return x.split("spredixcan-mashr-zscores-")[1].split("-data")[0]
+
+
+predictions = predictions.assign(tissue=predictions["data"].apply(_get_tissue))
+
+# %% tags=[]
+predictions.head()
+
+# %% [markdown] tags=[]
+# ## Testing
+
 # %% tags=[]
 display(predictions.shape)
 
 assert predictions.shape[0] == 2 * (N_TISSUES * N_THRESHOLDS) * N_PREDICTIONS
-
-# %% tags=[]
-predictions.head()
 
 # %% tags=[]
 assert not predictions.isna().any().any()
@@ -153,33 +174,12 @@ display(_tmp)
 
 assert np.all(_tmp == N_TISSUES * N_PREDICTIONS)
 
-
-# %% tags=[]
-# FIXME: add this to the 011 notebooks... or maybe it's fine here (after submitting draft)
-def _get_tissue(x):
-    if x.endswith("-projection"):
-        return x.split("spredixcan-mashr-zscores-")[1].split("-projection")[0]
-    else:
-        return x.split("spredixcan-mashr-zscores-")[1].split("-data")[0]
-
-
-predictions = predictions.assign(tissue=predictions["data"].apply(_get_tissue))
-
-# # FIXME: remove or better document; here just for the most_signif version
-# predictions = predictions.assign(tissue="most_signif")
-
-# %% tags=[]
-predictions.head()
-
 # %% tags=[]
 _tmp = predictions.groupby(["method", "tissue"]).count()
 display(_tmp)
 
 assert np.all(_tmp.loc["Gene-based"] == (N_PREDICTIONS * N_THRESHOLDS))
 assert np.all(_tmp.loc["Module-based"] == (N_PREDICTIONS * N_THRESHOLDS))
-
-# %% [markdown] tags=[]
-# ## Testing
 
 # %% tags=[]
 # all prediction tables should have the same shape
@@ -207,14 +207,20 @@ predictions.to_pickle(output_file)
 # %% [markdown] tags=[]
 # # Aggregate predictions
 
+# %% [markdown]
+# Here I get summaries from all predictions as follows:
+#
+#   1. Group by trait, drug, method, tissue, and average all scores across all gene/LVs thresholds. This is the same as it is done in the published method referenced before (the framework for drug-repositioning).
+#   1. Then, group by trait, drug, method and take the maximum score across all tissues. The rationale for this is that 1) we don't know which tissue might have more information for a particular disease, and 2) tissue-specific TWAS results are not meaningful to extract conclusions of real tissue-specific effects, since there is a lot of eQTL sharing across tissues.
+#
+# These correspond to the final drug-disease predictions for each method.
+
 # %% tags=[]
 def _reduce_mean(x):
     return pd.Series(
         {
             "score": x["score"].mean(),
             "true_class": x["true_class"].unique()[0]
-            #             if x["true_class"].unique().shape[0] == 1
-            #             else None,
         }
     )
 
@@ -224,39 +230,17 @@ def _reduce_max(x):
         {
             "score": x["score"].max(),
             "true_class": x["true_class"].unique()[0]
-            #             if x["true_class"].unique().shape[0] == 1
-            #             else None,
         }
     )
 
 
-# def _reduce_best(x):
-# #     assert x["true_class"].unique() == FINISH
-#     x_stand = (x["score"] - x["score"].mean()) / x["score"].std()
-
-#     x_max_score = x_stand.max()
-#     x_min_score = x_stand.min()
-
-#     # select best score
-#     x_selected = x["score"].max()
-#     if abs(x_min_score) > abs(x_max_score):
-#         x_selected = x["score"].min()
-
-#     return pd.Series(
-#         {
-#             "score": x_selected,
-#             "true_class": x["true_class"].unique()[0]
-#             #             if x["true_class"].unique().shape[0] == 1
-#             #             else None,
-#         }
-#     )
-
 # %% tags=[]
 predictions_avg = (
+    # average across n_top_genes
     predictions.groupby(["trait", "drug", "method", "tissue"])
-    #     predictions.groupby(["trait", "drug", "method"])
     .apply(_reduce_mean)
     .dropna()
+    # take maximum across tissues
     .groupby(["trait", "drug", "method"])
     .apply(_reduce_max)
     .dropna()
@@ -297,19 +281,19 @@ from sklearn.metrics import roc_auc_score
 # ## Predictions
 
 # %% tags=[]
-# by method/n_top_genes
+# AUROC by method/n_top_genes
 predictions.groupby(["method", "tissue", "n_top_genes"]).apply(
     lambda x: roc_auc_score(x["true_class"], x["score"])
 ).groupby(["method", "n_top_genes"]).describe()
 
 # %% tags=[]
-# by method/tissue
+# AUROC by method/tissue
 predictions.groupby(["method", "tissue", "n_top_genes"]).apply(
     lambda x: roc_auc_score(x["true_class"], x["score"])
 ).groupby(["method", "tissue"]).describe()
 
 # %% [markdown] tags=[]
-# ## Predictions avg
+# ## Predictions summaries
 
 # %% tags=[]
 predictions_avg.head()
@@ -318,6 +302,9 @@ predictions_avg.head()
 predictions_avg.groupby(["method"]).apply(
     lambda x: roc_auc_score(x["true_class"], x["score"])
 )
+
+# %% [markdown]
+# These are the final performance measures using AUROC.
 
 # %% [markdown] tags=[]
 # # PR
@@ -329,23 +316,26 @@ from sklearn.metrics import average_precision_score
 # ## Predictions
 
 # %% tags=[]
-# by method/n_top_genes
+# Average precision by method/n_top_genes
 predictions.groupby(["method", "tissue", "n_top_genes"]).apply(
     lambda x: average_precision_score(x["true_class"], x["score"])
 ).groupby(["method", "n_top_genes"]).describe()
 
 # %% tags=[]
-# by method/tissue
+# Average precision by method/tissue
 predictions.groupby(["method", "tissue", "n_top_genes"]).apply(
     lambda x: average_precision_score(x["true_class"], x["score"])
 ).groupby(["method", "tissue"]).describe()
 
 # %% [markdown] tags=[]
-# ## Predictions avg
+# ## Predictions summaries
 
 # %% tags=[]
 predictions_avg.groupby(["method"]).apply(
     lambda x: average_precision_score(x["true_class"], x["score"])
 )
+
+# %% [markdown]
+# These are the final performance measures using average precision.
 
 # %% tags=[]
