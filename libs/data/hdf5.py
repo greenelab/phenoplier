705bdd342cac741a46cf66f0b542581abf8d3310
Milton Pividori <miltondp@gmail.com>
2021-04-22 23:23:18 -0400

    Clean up code in hdf5.py

--- libs/data/hdf5.py
+++ libs/data/hdf5.py
@@ -1,8 +1,7 @@
+"""
+Contains functions and utilities to read data in HDF5 format.
+"""
 import re
-from pathlib import Path
-
-import pandas as pd
-
 
 HDF5_FILE_PATTERN = re.compile(r"spredixcan-(?P<tissue>.+)-zscore\.h5")
 HDF5_KEY_NO_PATTERN = re.compile(r"[^0-9a-zA-Z_]")
@@ -23,24 +22,3 @@ def simplify_trait_fullcode(trait_full_code: str) -> str:
     """
     clean_col = re.sub(HDF5_KEY_NO_PATTERN, "_", trait_full_code)
     return "c" + clean_col
-
-
-def read_spredixcan(folder, trait, tissue):
-    """
-    TODO: finish
-
-    Args:
-        folder:
-        trait:
-        tissue:
-
-    Returns:
-
-    """
-    filename = f"spredixcan-{tissue}-zscore.h5"
-    input_filepath = Path(folder, filename)
-
-    trait_hdf5 = simplify_trait_fullcode(trait)
-
-    with pd.HDFStore(input_filepath, mode="r") as store:
-        return store[trait_hdf5].rename_axis("gene_id")
