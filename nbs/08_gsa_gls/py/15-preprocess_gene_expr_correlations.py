a5927bf61ce372a064692b633a7bf1139bd6c48a
Milton Pividori <miltondp@gmail.com>
2021-05-01 18:39:05 -0400

    Run notebooks with papermill

--- nbs/08_gsa_gls/py/15-preprocess_gene_expr_correlations.py
+++ nbs/08_gsa_gls/py/15-preprocess_gene_expr_correlations.py
@@ -50,51 +50,51 @@ display(INPUT_DIR)
 # %% [markdown] tags=[]
 # ## Gene correlations
 
-# %%
+# %% tags=[]
 all_gene_corr_files = list(INPUT_DIR.rglob("*.pkl"))
 
-# %%
+# %% tags=[]
 len(all_gene_corr_files)
 
-# %%
+# %% tags=[]
 all_gene_corr_files[:5]
 
-# %%
+# %% tags=[]
 assert len(all_gene_corr_files) == 22 * 49
 
-# %%
+# %% tags=[]
 all_gene_corr_files_df = pd.DataFrame({"corr_file": [f for f in all_gene_corr_files]})
 
-# %%
+# %% tags=[]
 all_gene_corr_files_df = all_gene_corr_files_df.assign(
     file_name=all_gene_corr_files_df["corr_file"].apply(lambda x: x.name)
 )
 
-# %%
+# %% tags=[]
 all_gene_corr_files_df = all_gene_corr_files_df.assign(
     tissue=all_gene_corr_files_df["file_name"].apply(
         lambda x: x.split("-chr")[0].split("gene_corrs-")[1]
     )
 )
 
-# %%
+# %% tags=[]
 all_gene_corr_files_df = all_gene_corr_files_df.assign(
     chromosome=all_gene_corr_files_df["file_name"].apply(
         lambda x: int(x.split("-chr")[1].split(".")[0])
     )
 )
 
-# %%
+# %% tags=[]
 assert all_gene_corr_files_df["tissue"].unique().shape[0] == 49
 
-# %%
+# %% tags=[]
 assert all_gene_corr_files_df["chromosome"].unique().shape[0] == 22
 assert set(all_gene_corr_files_df["chromosome"]) == set(range(1, 23))
 
-# %%
+# %% tags=[]
 all_gene_corr_files_df.shape
 
-# %%
+# %% tags=[]
 all_gene_corr_files_df.head()
 
 # %% [markdown] tags=[]
@@ -162,7 +162,7 @@ tissues[:5]
 # %% [markdown] tags=[]
 # # Average correlations per chromosome
 
-# %%
+# %% tags=[]
 corrs_per_chr = {}
 
 for chr_num in range(1, 23):
@@ -209,57 +209,57 @@ for chr_num in range(1, 23):
 # %% [markdown] tags=[]
 # # Create full gene correlation matrix
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # This matrix has all genes in MultiPLIER Z
 
-# %%
+# %% tags=[]
 gene_corrs_df = pd.DataFrame(data=0.0, index=genes_info["id"], columns=genes_info["id"])
 
-# %%
+# %% tags=[]
 gene_corrs_df.shape
 
-# %%
+# %% tags=[]
 gene_corrs_df.head()
 
-# %%
+# %% tags=[]
 for chr_num, chr_data in corrs_per_chr.items():
     chr_data = chr_data.reindex(
         index=gene_corrs_df.index, columns=gene_corrs_df.columns
     )
     gene_corrs_df = gene_corrs_df + chr_data.fillna(0.0)
 
-# %%
+# %% tags=[]
 gene_corrs_df = gene_corrs_df.astype(np.float32)
 
-# %%
+# %% tags=[]
 gene_corrs_df.head()
 
-# %%
+# %% tags=[]
 assert np.all(gene_corrs_df.values.diagonal() == 1.0)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Stats
 
-# %%
+# %% tags=[]
 _gene_corrs_flat = squareform(gene_corrs_df.values, checks=False)
 
-# %%
+# %% tags=[]
 pd.Series(_gene_corrs_flat).describe()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Plot
 
-# %%
+# %% tags=[]
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
-# %%
+# %% tags=[]
 genes_order = genes_info.sort_values("chr")["id"].tolist()
 
-# %%
+# %% tags=[]
 cmap = ListedColormap(["w", "r"])
 
-# %%
+# %% tags=[]
 fig, ax = plt.subplots(figsize=(10, 10))
 ax.matshow(
     gene_corrs_df.loc[genes_order, genes_order].values, vmin=-0.05, vmax=0.05, cmap=cmap
@@ -269,80 +269,80 @@ ax.set_ylabel("Genes")
 ax.set_xticks([])
 ax.set_yticks([])
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Testing
 
-# %%
+# %% tags=[]
 # COL4A1 and COL4A2
 gene1 = "ENSG00000187498"
 gene2 = "ENSG00000134871"
 
 gene_corrs_df.loc[gene1, gene2]
 
-# %%
+# %% tags=[]
 _genes_files = all_gene_corr_files_df[all_gene_corr_files_df["chromosome"] == 13][
     "corr_file"
 ].tolist()
 assert len(_genes_files) == 49
 
-# %%
+# %% tags=[]
 _gene_values = []
 for f in _genes_files:
     gene1_gene2_corr = pd.read_pickle(f).loc[gene1, gene2]
     _gene_values.append(gene1_gene2_corr)
 
-# %%
+# %% tags=[]
 _gene_values = np.array(_gene_values)
 assert _gene_values.shape[0] == 49
 
-# %%
+# %% tags=[]
 display(_gene_values.mean())
 assert gene_corrs_df.loc[gene1, gene2] == _gene_values.mean()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Save
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## With ensemble ids
 
-# %%
+# %% tags=[]
 output_file = (
     conf.PHENOMEXCAN["LD_BLOCKS"]["BASE_DIR"]
     / "multiplier_genes-pred_expression_corr_avg.pkl"
 )
 display(output_file)
 
-# %%
+# %% tags=[]
 gene_corrs_df.to_pickle(output_file)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## With gene symbols
 
-# %%
+# %% tags=[]
 output_file = (
     conf.PHENOMEXCAN["LD_BLOCKS"]["BASE_DIR"]
     / "multiplier_genes-pred_expression_corr_avg-gene_names.pkl"
 )
 display(output_file)
 
-# %%
+# %% tags=[]
 gene_corrs_gene_names_df = gene_corrs_df.rename(
     index=Gene.GENE_ID_TO_NAME_MAP, columns=Gene.GENE_ID_TO_NAME_MAP
 )
 
-# %%
+# %% tags=[]
 assert gene_corrs_gene_names_df.index.is_unique
 
-# %%
+# %% tags=[]
 assert gene_corrs_gene_names_df.columns.is_unique
 
-# %%
+# %% tags=[]
 gene_corrs_gene_names_df.shape
 
-# %%
+# %% tags=[]
 gene_corrs_gene_names_df.head()
 
-# %%
+# %% tags=[]
 gene_corrs_gene_names_df.to_pickle(output_file)
 
-# %%
+# %% tags=[]
