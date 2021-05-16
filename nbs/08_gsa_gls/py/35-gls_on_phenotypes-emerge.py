f8cded301a10ffb542e0aa913634df2758c78430
Milton Pividori <miltondp@gmail.com>
2021-05-16 13:54:33 -0400

    Run notebooks with papermill

--- nbs/08_gsa_gls/py/35-gls_on_phenotypes-emerge.py
+++ nbs/08_gsa_gls/py/35-gls_on_phenotypes-emerge.py
@@ -17,11 +17,11 @@
 # %% [markdown] tags=[]
 # # Environment variables
 
-# %%
+# %% tags=[]
 # %load_ext autoreload
 # %autoreload 2
 
-# %%
+# %% tags=[]
 import conf
 
 # %% tags=[]
@@ -34,10 +34,10 @@ display(N_JOBS)
 # %env NUMEXPR_NUM_THREADS=$N_JOBS
 # %env OMP_NUM_THREADS=$N_JOBS
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Modules
 
-# %%
+# %% tags=[]
 from pathlib import Path
 
 import statsmodels.api as sm
@@ -48,22 +48,22 @@ from tqdm import tqdm
 
 from gls import GLSPhenoplier
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Settings
 
-# %%
+# %% tags=[]
 OUTPUT_DIR = conf.RESULTS["GLS"]
 display(OUTPUT_DIR)
 
 OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Load data
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## eMERGE to PhenomeXcan maps
 
-# %%
+# %% tags=[]
 # FIXME: hardcoded
 input_filepath = Path(
     "/home/miltondp/projects/labs/greenelab/phenoplier/base/data/emerge",
@@ -71,7 +71,7 @@ input_filepath = Path(
 ).resolve()
 display(input_filepath)
 
-# %%
+# %% tags=[]
 emerge_traits_df = pd.read_csv(
     input_filepath,
     sep="\t",
@@ -79,7 +79,7 @@ emerge_traits_df = pd.read_csv(
     usecols=["phecode", "phenotype", "category"],
 )
 
-# %%
+# %% tags=[]
 emerge_traits_df = emerge_traits_df.rename(
     columns={
         "phenotype": "phecode_phenotype",
@@ -87,16 +87,16 @@ emerge_traits_df = emerge_traits_df.rename(
     }
 )
 
-# %%
+# %% tags=[]
 emerge_traits_df.shape
 
-# %%
+# %% tags=[]
 emerge_traits_df.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## eMERGE to PhenomeXcan maps
 
-# %%
+# %% tags=[]
 # FIXME: hardcoded
 emerge_phenomexcan_maps_filepath = Path(
     "/home/miltondp/projects/labs/greenelab/phenoplier/base/data/emerge",
@@ -104,26 +104,26 @@ emerge_phenomexcan_maps_filepath = Path(
 ).resolve()
 display(emerge_phenomexcan_maps_filepath)
 
-# %%
+# %% tags=[]
 emerge_phenomexcan_maps = pd.read_csv(
     emerge_phenomexcan_maps_filepath, sep="\t", dtype={"phecode": str}
 )
 
-# %%
+# %% tags=[]
 emerge_phenomexcan_maps = emerge_phenomexcan_maps.dropna(
     subset=["phecode", "phenomexcan"], how="any"
 )
 
-# %%
+# %% tags=[]
 emerge_phenomexcan_maps.shape
 
-# %%
+# %% tags=[]
 emerge_phenomexcan_maps.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## eMERGE (S-MultiXcan) projection
 
-# %%
+# %% tags=[]
 # FIXME hardcoded
 input_filepath = Path(
     "/home/miltondp/projects/labs/greenelab/phenoplier/base/results/projections",
@@ -131,19 +131,19 @@ input_filepath = Path(
 ).resolve()
 display(input_filepath)
 
-# %%
+# %% tags=[]
 emerge_projection = pd.read_pickle(input_filepath)
 
-# %%
+# %% tags=[]
 emerge_projection.shape
 
-# %%
+# %% tags=[]
 emerge_projection.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## eMERGE (S-MultiXcan) projection
 
-# %%
+# %% tags=[]
 # FIXME: path hardcoded
 emerge_smultixcan_projection_filepath = Path(
     "/home/miltondp/projects/labs/greenelab/phenoplier/base/data/emerge/gene_assoc/emerge-smultixcan-mashr-zscores.pkl"
@@ -151,44 +151,44 @@ emerge_smultixcan_projection_filepath = Path(
 
 display(emerge_smultixcan_projection_filepath)
 
-# %%
+# %% tags=[]
 _tmp = pd.read_pickle(emerge_smultixcan_projection_filepath)
 
-# %%
+# %% tags=[]
 _tmp.shape
 
-# %%
+# %% tags=[]
 _tmp.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## GLS results on PhenomeXcan
 
-# %%
+# %% tags=[]
 input_filepath = conf.RESULTS["GLS"] / "gls_phenotypes.pkl"
 display(input_filepath)
 
-# %%
+# %% tags=[]
 gls_phenomexcan = pd.read_pickle(input_filepath)
 
-# %%
+# %% tags=[]
 gls_phenomexcan.shape
 
-# %%
+# %% tags=[]
 gls_phenomexcan.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## MultiPLIER summary
 
-# %%
+# %% tags=[]
 multiplier_model_summary = pd.read_pickle(conf.MULTIPLIER["MODEL_SUMMARY_FILE"])
 
-# %%
+# %% tags=[]
 multiplier_model_summary.shape
 
-# %%
+# %% tags=[]
 multiplier_model_summary.head()
 
-# %%
+# %% tags=[]
 well_aligned_lvs = multiplier_model_summary[
     (multiplier_model_summary["FDR"] < 0.05) | (multiplier_model_summary["AUC"] >= 0.75)
 ]
@@ -196,62 +196,62 @@ well_aligned_lvs = multiplier_model_summary[
 display(well_aligned_lvs.shape)
 display(well_aligned_lvs.head())
 
-# %%
+# %% tags=[]
 well_aligned_lv_codes = set([f"LV{lvi}" for lvi in well_aligned_lvs["LV index"]])
 
-# %%
+# %% tags=[]
 len(well_aligned_lv_codes)
 
-# %%
+# %% tags=[]
 list(well_aligned_lv_codes)[:5]
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Select LV from previous GLS run on PhenomeXcan
 
-# %%
+# %% tags=[]
 gls_phenomexcan_lvs = gls_phenomexcan["lv"].unique()
 
-# %%
+# %% tags=[]
 gls_phenomexcan_lvs.shape
 
-# %%
+# %% tags=[]
 gls_phenomexcan_lvs
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Select eMERGE traits
 
-# %%
+# %% tags=[]
 gls_phenomexcan_traits = gls_phenomexcan["phenotype"].unique()
 
-# %%
+# %% tags=[]
 gls_phenomexcan_traits.shape
 
-# %%
+# %% tags=[]
 gls_phenomexcan_in_emerge = emerge_phenomexcan_maps[
     (emerge_phenomexcan_maps["efo"].isin(gls_phenomexcan_traits))
     | (emerge_phenomexcan_maps["phenomexcan"].isin(gls_phenomexcan_traits))
 ]
 
-# %%
+# %% tags=[]
 gls_phenomexcan_in_emerge
 
-# %%
+# %% tags=[]
 gls_emerge_phecodes = gls_phenomexcan_in_emerge["phecode"].unique().tolist()
 
-# %%
+# %% tags=[]
 # these are the mapped traits from PhenomeXcan to phecodes
 gls_emerge_phecodes
 
-# %%
+# %% tags=[]
 # phecode_to_desc_map = emerge_traits_df[["phecode", "phecode_phenotype"]].set_index("phecode").squeeze().to_dict()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # GLSPhenoplier
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Get list of phenotypes/lvs pairs
 
-# %%
+# %% tags=[]
 phenotypes_lvs_pairs = []
 
 # for lvs run for PhenomeXcan, I take the top traits in eMERGE + global mapped phenotypes
@@ -270,25 +270,25 @@ for lv in gls_phenomexcan_lvs:
 
 phenotypes_lvs_pairs = pd.DataFrame(phenotypes_lvs_pairs).drop_duplicates()
 
-# %%
+# %% tags=[]
 phenotypes_lvs_pairs = phenotypes_lvs_pairs.sort_values("phenotype").reset_index(
     drop=True
 )
 
-# %%
+# %% tags=[]
 phenotypes_lvs_pairs.shape
 
-# %%
+# %% tags=[]
 phenotypes_lvs_pairs.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Run
 
-# %%
+# %% tags=[]
 output_file = OUTPUT_DIR / "gls_phenotypes-emerge.pkl"
 display(output_file)
 
-# %%
+# %% tags=[]
 results = []
 
 pbar = tqdm(total=phenotypes_lvs_pairs.shape[0])
@@ -323,22 +323,22 @@ for idx, row in phenotypes_lvs_pairs.iterrows():
 
 pbar.close()
 
-# %%
+# %% tags=[]
 results = pd.DataFrame(results)
 
-# %%
+# %% tags=[]
 results.shape
 
-# %%
+# %% tags=[]
 results.head()
 
-# %%
+# %% tags=[]
 results.sort_values("pvalue").head(10)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Save
 
-# %%
+# %% tags=[]
 results.to_pickle(output_file)
 
-# %%
+# %% tags=[]
