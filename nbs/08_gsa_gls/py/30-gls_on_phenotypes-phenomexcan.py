348192ef1d88573e675fbcee526120ef7590f8c6
Milton Pividori <miltondp@gmail.com>
2021-05-18 10:53:09 -0400

    Standardize output file naming

--- nbs/08_gsa_gls/py/30-gls_on_phenotypes-phenomexcan.py
+++ nbs/08_gsa_gls/py/30-gls_on_phenotypes-phenomexcan.py
@@ -63,6 +63,10 @@ display(OUTPUT_DIR)
 
 OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 
+# %% tags=[]
+OUTPUT_FILENAME = OUTPUT_DIR / "gls_phenotypes-phenomexcan.pkl"
+display(OUTPUT_FILENAME)
+
 # %% [markdown] tags=[]
 # # Load data
 
@@ -259,10 +263,6 @@ phenotypes_lvs_pairs.head()
 # ## Run
 
 # %% tags=[]
-output_file = OUTPUT_DIR / "gls_phenotypes.pkl"
-display(output_file)
-
-# %% tags=[]
 results = []
 
 pbar = tqdm(total=phenotypes_lvs_pairs.shape[0])
@@ -296,7 +296,7 @@ for idx, row in phenotypes_lvs_pairs.iterrows():
 
     # save results every 10 models trained
     if (idx % 10) == 0:
-        pd.DataFrame(results).to_pickle(output_file)
+        pd.DataFrame(results).to_pickle(OUTPUT_FILENAME)
 
     pbar.update(1)
 
@@ -318,6 +318,6 @@ results.sort_values("pvalue").head(10)
 # ## Save
 
 # %% tags=[]
-results.to_pickle(output_file)
+results.to_pickle(OUTPUT_FILENAME)
 
 # %% tags=[]
--- nbs/08_gsa_gls/py/40-gls_on_lipids_lvs-phenomexcan.py
+++ nbs/08_gsa_gls/py/40-gls_on_lipids_lvs-phenomexcan.py
@@ -61,6 +61,10 @@ display(OUTPUT_DIR)
 
 OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 
+# %% tags=[]
+OUTPUT_FILENAME = OUTPUT_DIR / "gls_phenotypes-crispr_lvs-phenomexcan.pkl"
+display(OUTPUT_FILENAME)
+
 # %% [markdown] tags=[]
 # # Load data
 
@@ -291,10 +295,6 @@ phenotypes_lvs_pairs.head()
 # ## Run
 
 # %% tags=[]
-output_file = OUTPUT_DIR / "gls_phenotypes-crispr_lvs.pkl"
-display(output_file)
-
-# %% tags=[]
 results = []
 
 pbar = tqdm(total=phenotypes_lvs_pairs.shape[0])
@@ -329,7 +329,7 @@ for idx, row in phenotypes_lvs_pairs.iterrows():
 
     # save results every 10 models trained
     if (idx % 10) == 0:
-        pd.DataFrame(results).to_pickle(output_file)
+        pd.DataFrame(results).to_pickle(OUTPUT_FILENAME)
 
     pbar.update(1)
 
@@ -351,6 +351,6 @@ results.sort_values("pvalue").head(10)
 # ## Save
 
 # %% tags=[]
-results.to_pickle(output_file)
+results.to_pickle(OUTPUT_FILENAME)
 
 # %% tags=[]
