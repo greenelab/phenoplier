efa36404fec0016ca43b36d5a4a523ba4fbe95d5
Milton Pividori <miltondp@gmail.com>
2021-05-21 09:47:06 -0400

    gsea_on_all_lvs: run with papermill

--- nbs/30_validation/crispr_screen/py/030_10-deg_genes-gsea_on_all_lvs.R
+++ nbs/30_validation/crispr_screen/py/030_10-deg_genes-gsea_on_all_lvs.R
@@ -14,10 +14,10 @@
 #     name: ir
 # ---
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Module loading
 
-# %%
+# %% tags=[]
 library(IRdisplay)
 library(readr)
 library(fgsea)
@@ -25,35 +25,35 @@ library(dplyr)
 library(tidyverse)
 library(reticulate)
 
-# %%
+# %% tags=[]
 pd <- import("pandas")
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Settings
 
-# %%
+# %% tags=[]
 OUTPUT_DIR <- Sys.getenv("PHENOPLIER_RESULTS_CRISPR_ANALYSES_BASE_DIR")
 
-# %%
+# %% tags=[]
 OUTPUT_DIR
 
-# %%
+# %% tags=[]
 dir.create(OUTPUT_DIR, recursive=TRUE)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Data loading
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Lipids gene sets
 
-# %%
+# %% tags=[]
 input_file <- Sys.getenv("PHENOPLIER_CRISPR_LIPIDS_GENE_SETS_FILE")
 display(input_file)
 
-# %%
+# %% tags=[]
 all_genes_ranked <- read_csv(input_file)
 
-# %%
+# %% tags=[]
 orig_deg_gene_sets <- list()
 
 for (r in unique(all_genes_ranked$rank)) {
@@ -67,45 +67,45 @@ for (r in unique(all_genes_ranked$rank)) {
     orig_deg_gene_sets[[paste0("gene_set_", r)]] <- data$gene_name
 }
 
-# %%
+# %% tags=[]
 length(orig_deg_gene_sets)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ### Combine gene sets into "increase lipids" and "decrease lipids"
 
-# %%
+# %% tags=[]
 deg_gene_sets <- list()
 
-# %%
+# %% tags=[]
 # genes that increase lipids
 deg_gene_sets[["gene_set_increase"]] <- c(
     orig_deg_gene_sets[["gene_set_2"]],
     orig_deg_gene_sets[["gene_set_3"]]
 )
 
-# %%
+# %% tags=[]
 # genes that decrease lipids
 deg_gene_sets[["gene_set_decrease"]] <- c(
     orig_deg_gene_sets[["gene_set_-2"]],
     orig_deg_gene_sets[["gene_set_-3"]]
 )
 
-# %%
+# %% tags=[]
 length(deg_gene_sets)
 
-# %%
+# %% tags=[]
 length(deg_gene_sets[["gene_set_increase"]])
 
-# %%
+# %% tags=[]
 stopifnot(length(deg_gene_sets[["gene_set_increase"]]) == 175)
 
-# %%
+# %% tags=[]
 length(deg_gene_sets[["gene_set_decrease"]])
 
-# %%
+# %% tags=[]
 stopifnot(length(deg_gene_sets[["gene_set_decrease"]]) == 96)
 
-# %%
+# %% tags=[]
 # test new increase set
 new_set <- deg_gene_sets[["gene_set_increase"]]
 expected_set <- union(
@@ -125,7 +125,7 @@ stopifnot(
     )
 )
 
-# %%
+# %% tags=[]
 # test new decrease set
 new_set <- deg_gene_sets[["gene_set_decrease"]]
 expected_set <- union(
@@ -145,24 +145,24 @@ stopifnot(
     )
 )
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## MultiPLIER Z
 
-# %%
+# %% tags=[]
 multiplier_z = pd$read_pickle(
     Sys.getenv("PHENOPLIER_MULTIPLIER_MODEL_Z_MATRIX_FILE")
 )
 
-# %%
+# %% tags=[]
 dim(multiplier_z)
 
-# %%
+# %% tags=[]
 head(multiplier_z)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Prepare LVs list
 
-# %%
+# %% tags=[]
 lvs = list()
 z_gene_names <- rownames(multiplier_z)
 
@@ -175,20 +175,20 @@ for (cidx in 1:ncol(multiplier_z)) {
     lvs[[paste0("LV", cidx)]] <- data[data > q]
 }
 
-# %%
+# %% tags=[]
 display(length(lvs))
 stopifnot(length(lvs) == 987)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Compute enrichment on all LVs
 
-# %%
+# %% tags=[]
 n_reps = 10
 
-# %%
+# %% tags=[]
 set.seed(0)
 
-# %%
+# %% tags=[]
 results = list()
 
 for (lv in names(lvs)) {
@@ -207,53 +207,53 @@ for (lv in names(lvs)) {
     results[[lv]] <- res
 }
 
-# %%
+# %% tags=[]
 length(results)
 
-# %%
+# %% tags=[]
 df <- do.call(rbind, results)
 
-# %%
+# %% tags=[]
 df <- df %>% mutate(leadingEdge = map_chr(leadingEdge, toString))
 
-# %%
+# %% tags=[]
 dim(df)
 
-# %%
+# %% tags=[]
 head(df)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Save
 
-# %%
+# %% tags=[]
 output_file <- file.path(OUTPUT_DIR, "fgsea-all_lvs.tsv")
 display(output_file)
 
-# %%
+# %% tags=[]
 write_tsv(df, output_file)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Quick analyses/tests
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## See how one LV looks like
 
-# %%
+# %% tags=[]
 df %>% filter(lv == "LV100" & pathway == "gene_set_increase") %>% arrange(desc(padj))
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Show significant LVs
 
-# %%
+# %% tags=[]
 df_signif <- df %>% group_by(lv, pathway) %>% summarize(max_pval = max(pval)) %>% filter(max_pval < 0.05)
 
-# %%
+# %% tags=[]
 nrow(df_signif)
 
-# %%
+# %% tags=[]
 stopifnot(nrow(df_signif) > 50)
 
-# %%
+# %% tags=[]
 df_signif %>% arrange(max_pval)
 
-# %%
+# %% tags=[]
