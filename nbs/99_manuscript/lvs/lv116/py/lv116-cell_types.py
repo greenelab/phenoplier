ecae48ca92bbc668f91e6105e491d43ac47ee5fc
Milton Pividori <miltondp@gmail.com>
2021-08-02 11:38:12 -0400

    lv116: add notebooks to generate figures/tables

---
+++ nbs/99_manuscript/lvs/lv116/py/lv116-cell_types.py
@@ -0,0 +1,407 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     formats: ipynb,py//py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] tags=[]
+# # Description
+
+# %% [markdown] tags=[]
+# Generates the figure for top cell types for a specified LV (in Settings section below).
+
+# %% [markdown] tags=[]
+# # Modules loading
+
+# %% tags=[]
+# %load_ext autoreload
+# %autoreload 2
+
+# %% tags=[]
+import re
+from pathlib import Path
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from data.recount2 import LVAnalysis
+from utils import chunker
+import conf
+
+# %% [markdown] tags=[]
+# # Settings
+
+# %% tags=["parameters"]
+LV_NAME = "LV116"
+
+# %%
+LV_AXIS_THRESHOLD = 3.0
+N_TOP_SAMPLES = 400
+N_TOP_ATTRS = 25
+
+# %%
+OUTPUT_FIGURES_DIR = Path(
+    conf.MANUSCRIPT["FIGURES_DIR"], "lvs_analysis", f"{LV_NAME.lower()}"
+).resolve()
+display(OUTPUT_FIGURES_DIR)
+OUTPUT_FIGURES_DIR.mkdir(parents=True, exist_ok=True)
+
+# %%
+OUTPUT_CELL_TYPE_FILEPATH = OUTPUT_FIGURES_DIR / f"{LV_NAME.lower()}-cell_types.svg"
+display(OUTPUT_CELL_TYPE_FILEPATH)
+
+# %% [markdown] tags=[]
+# # Load MultiPLIER summary
+
+# %% tags=[]
+multiplier_model_summary = pd.read_pickle(conf.MULTIPLIER["MODEL_SUMMARY_FILE"])
+
+# %% tags=[]
+multiplier_model_summary.shape
+
+# %% tags=[]
+multiplier_model_summary.head()
+
+# %% [markdown] tags=[]
+# # Load data
+
+# %% [markdown] tags=[]
+# ## Original data
+
+# %% tags=[]
+INPUT_SUBSET = "z_score_std"
+
+# %% tags=[]
+INPUT_STEM = "projection-smultixcan-efo_partial-mashr-zscores"
+
+# %% tags=[]
+input_filepath = Path(
+    conf.RESULTS["DATA_TRANSFORMATIONS_DIR"],
+    INPUT_SUBSET,
+    f"{INPUT_SUBSET}-{INPUT_STEM}.pkl",
+).resolve()
+display(input_filepath)
+
+assert input_filepath.exists(), "Input file does not exist"
+
+input_filepath_stem = input_filepath.stem
+display(input_filepath_stem)
+
+# %% tags=[]
+data = pd.read_pickle(input_filepath)
+
+# %% tags=[]
+data.shape
+
+# %% tags=[]
+data.head()
+
+# %% [markdown]
+# ## LV data
+
+# %%
+lv_obj = LVAnalysis(LV_NAME, data)
+
+# %%
+multiplier_model_summary[
+    multiplier_model_summary["LV index"].isin((LV_NAME[2:],))
+    & (
+        (multiplier_model_summary["FDR"] < 0.05)
+        | (multiplier_model_summary["AUC"] >= 0.75)
+    )
+]
+
+# %%
+lv_data = lv_obj.get_experiments_data()
+
+# %%
+lv_data.shape
+
+# %%
+lv_data.head()
+
+# %% [markdown]
+# # LV cell types analysis
+
+# %% [markdown]
+# ## Get top attributes
+
+# %%
+lv_attrs = lv_obj.get_attributes_variation_score()
+display(lv_attrs.head(20))
+
+# %%
+# show those with cell type or tissue in their name
+_tmp = pd.Series(lv_attrs.index)
+lv_attrs[
+    _tmp.str.match(
+        "(?:cell.+type$)|(?:tissue$)|(?:tissue.+type$)",
+        case=False,
+        flags=re.IGNORECASE,
+    ).values
+].sort_values(ascending=False)
+
+# %%
+_tmp = lv_data.loc[
+    :,
+    [
+        "tissue",
+        "cell type",
+        "cell subtype",
+        "tissue type",
+        LV_NAME,
+    ],
+]
+
+# %%
+_tmp_seq = list(chunker(_tmp.sort_values(LV_NAME, ascending=False), 25))
+
+# %%
+_tmp_seq[1]
+
+# %%
+SELECTED_ATTRIBUTE = "tissue"
+
+# %%
+# it has to be in the order desired for filling nans in the SELECTED_ATTRIBUTE
+SECOND_ATTRIBUTES = ["cell type", "celltype", "agent"]
+
+# %% [markdown]
+# ## Get plot data
+
+# %%
+plot_data = lv_data.loc[:, [SELECTED_ATTRIBUTE] + SECOND_ATTRIBUTES + [LV_NAME]]
+
+# %%
+# if blank/nan, fill cell type column with tissue content
+_new_column = plot_data[[SELECTED_ATTRIBUTE] + SECOND_ATTRIBUTES].fillna(
+    method="backfill", axis=1
+)[SELECTED_ATTRIBUTE]
+plot_data[SELECTED_ATTRIBUTE] = _new_column
+plot_data = plot_data.drop(columns=SECOND_ATTRIBUTES)
+plot_data = plot_data.fillna({SELECTED_ATTRIBUTE: "NOT CATEGORIZED"})
+# plot_data = plot_data.dropna(subset=[SELECTED_ATTRIBUTE])
+
+# %%
+plot_data = plot_data.sort_values(LV_NAME, ascending=False)
+
+# %%
+plot_data.head(20)
+
+# %% [markdown]
+# ## Customize x-axis values
+
+# %% [markdown]
+# When cell type values are not very clear, customize their names by looking at their specific studies to know exactly what the authors meant.
+
+# %%
+final_plot_data = plot_data.replace(
+    {
+        SELECTED_ATTRIBUTE: {
+            "whole blood": "Whole blood",
+            "PBMCs": "Peripheral blood mononuclear cells",
+            "monocyte-derived macrophages": "Monocyte-derived macrophages",
+            "peripheral blood monocytes": "Monocytes",
+            "dermal fibroblast": "Dermal fibroblasts",
+            "proximal tubular epithelial cells": "Proximal tubular epithelial cells",
+            "glioblastoma cell line": "Glioblastoma cell line",
+        }
+    }
+)
+
+# %%
+_srp_code = "SRP048804"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: x[SELECTED_ATTRIBUTE]
+    + f" ({lv_data.loc[(_srp_code, x.name), 'cell line']})",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+_srp_code = "SRP045352"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: "Monocytes" + f" ({lv_data.loc[(_srp_code, x.name), 'agent']})",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+
+_srp_code = "SRP056733"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: "Macrophages" + f" ({lv_data.loc[(_srp_code, x.name), 'agent']})",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+_srp_code = "SRP062958"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: x[SELECTED_ATTRIBUTE]
+    + f" ({lv_data.loc[(_srp_code, x.name), 'treatment']})",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+# # add also tissue information to these projects
+_srp_code = "SRP015670"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: x[SELECTED_ATTRIBUTE]
+    + f" ({lv_data.loc[(_srp_code, x.name), 'treatment']})",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+# # add also tissue information to these projects
+_srp_code = "SRP045500"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: x[SELECTED_ATTRIBUTE]
+    + f" ({lv_data.loc[(_srp_code, x.name), 'diseasestatus']})",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+# # add also tissue information to these projects
+_srp_code = "SRP045569"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: x[SELECTED_ATTRIBUTE]
+    + f" ({lv_data.loc[(_srp_code, x.name), 'treatment']})",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+
+# # add also tissue information to these projects
+_srp_code = "SRP062966"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: x[SELECTED_ATTRIBUTE]
+    + f" ({lv_data.loc[(_srp_code, x.name), 'ism']} SLE)",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+
+# # add also tissue information to these projects
+_srp_code = "SRP059039"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: x[SELECTED_ATTRIBUTE]
+    + f" ({lv_data.loc[(_srp_code, x.name), 'group']} cases)",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+
+# # add also tissue information to these projects
+_srp_code = "SRP060370"
+_tmp = final_plot_data.loc[(_srp_code,)].apply(
+    lambda x: x[SELECTED_ATTRIBUTE]
+    + f" ({lv_data.loc[(_srp_code, x.name), 'treatment']})",
+    axis=1,
+)
+final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+
+# %%
+final_plot_data = final_plot_data.replace(
+    {
+        SELECTED_ATTRIBUTE: {
+            "\(interferon-alpha\)": "(IFNa)",
+            "\(HSV-1\)": "(HSV)",
+            "\(West Nile virus \(WNV\)\)": "(WNV)",
+            "\(ISM_high SLE\)": "(ISM high SLE)",
+        }
+    },
+    regex=True,
+)
+
+# %% [markdown]
+# ## Threshold LV values
+
+# %%
+final_plot_data.loc[
+    final_plot_data[LV_NAME] > LV_AXIS_THRESHOLD, LV_NAME
+] = LV_AXIS_THRESHOLD
+
+# %% [markdown]
+# ## Delete samples with no tissue/cell type information
+
+# %%
+final_plot_data = final_plot_data[
+    final_plot_data[SELECTED_ATTRIBUTE] != "NOT CATEGORIZED"
+]
+
+# %% [markdown]
+# ## Set x-axis order
+
+# %%
+N_TOP_ATTRS = 15
+
+# %%
+attr_order = (
+    final_plot_data.groupby(SELECTED_ATTRIBUTE)
+    .max()
+    .sort_values(LV_NAME, ascending=False)
+    .index[:N_TOP_ATTRS]
+    .tolist()
+)
+
+# %%
+len(attr_order)
+
+# %%
+attr_order[:5]
+
+# %% [markdown]
+# ## Plot
+
+# %%
+with sns.plotting_context("paper", font_scale=2.0), sns.axes_style("whitegrid"):
+    sns.catplot(
+        data=final_plot_data,
+        y=LV_NAME,
+        x=SELECTED_ATTRIBUTE,
+        order=attr_order,
+        kind="strip",
+        height=5,
+        aspect=3,
+    )
+    plt.xticks(rotation=45, horizontalalignment="right")
+    plt.xlabel("")
+
+    plt.savefig(
+        OUTPUT_CELL_TYPE_FILEPATH,
+        bbox_inches="tight",
+        facecolor="white",
+    )
+
+# %% [markdown]
+# # Debug
+
+# %%
+# with pd.option_context(
+#     "display.max_rows", None, "display.max_columns", None, "display.max_colwidth", None
+# ):
+#     _tmp = final_plot_data[final_plot_data[SELECTED_ATTRIBUTE].str.contains("Salm")]
+#     display(_tmp)
+
+# %%
+# # what is there in these projects?
+# with pd.option_context(
+#     "display.max_rows", None, "display.max_columns", None, "display.max_colwidth", None
+# ):
+#     _tmp = (
+#         lv_data.loc[["SRP063059"]]
+#         .dropna(how="all", axis=1)
+#         .sort_values(LV_NAME, ascending=False)
+#     )
+#     display(_tmp)
+
+# %%
