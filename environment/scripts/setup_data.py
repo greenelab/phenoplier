a40059dcc6107b82beb1ea67aa0654e004eeac37
Milton Pividori <miltondp@gmail.com>
2021-08-04 10:22:30 -0400

    setup_data.py: add downloads for testing mode

--- environment/scripts/setup_data.py
+++ environment/scripts/setup_data.py
@@ -17,6 +17,15 @@ logger = get_logger("setup")
 #
 DATA_IN_TESTING_MODE_ONLY = {
     "download_phenomexcan_rapid_gwas_pheno_info",
+    "download_phenomexcan_rapid_gwas_data_dict_file",
+    "download_uk_biobank_coding_3",
+    "download_uk_biobank_coding_6",
+    "download_phenomexcan_gtex_gwas_pheno_info",
+    "download_gene_map_name_to_id",
+    "download_gene_map_id_to_name",
+    "download_biomart_genes_hg38",
+    "download_multiplier_model_z_pkl",
+    "download_multiplier_model_metadata_pkl",
 }
 
 
