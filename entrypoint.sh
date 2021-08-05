d1f6965d4c7d31fc9e2ff70ab1cad46a39d38e37
Milton Pividori <miltondp@gmail.com>
2021-08-05 10:29:07 -0400

    Dockerfile: fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash --login
+# Taken from here with modifications: https://pythonspeed.com/articles/activate-conda-dockerfile/
 # The --login ensures the bash configuration is loaded,
 # enabling Conda.
 set +eu
