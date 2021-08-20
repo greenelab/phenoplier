de921bf77708a1798982dd279b6c2dd28cd0913a
Milton Pividori <miltondp@gmail.com>
2021-08-20 12:20:10 -0400

    LV-gene-pathways: run notebook with papermill

--- nbs/99_manuscript/lvs/py/05-export_lv_gene_dataframe.py
+++ nbs/99_manuscript/lvs/py/05-export_lv_gene_dataframe.py
@@ -14,16 +14,16 @@
 #     name: python3
 # ---
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Description
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # This notebook generates more end-user-friendly Excel files of some of the data generated in PhenoPLIER, such as the LV-gene matrix and LV-pathways.
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Modules loading
 
-# %%
+# %% tags=[]
 import pandas as pd
 import openpyxl
 from openpyxl.utils import get_column_letter
@@ -31,7 +31,7 @@ from openpyxl.utils import get_column_letter
 import conf
 from utils import get_git_repository_path
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Settings
 
 # %% tags=[]
@@ -43,7 +43,7 @@ OUTPUT_DIR = DELIVERABLES_BASE_DIR / "multiplier"
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 display(OUTPUT_DIR)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # # Load data
 
 # %% [markdown] tags=[]
@@ -58,7 +58,7 @@ multiplier_model_summary.shape
 # %% tags=[]
 multiplier_model_summary.head()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Get pathway-aligned LVs
 
 # %% tags=[]
@@ -79,57 +79,57 @@ list(well_aligned_lv_codes)[:5]
 # %% [markdown] tags=[]
 # ## MultiPLIER Z (gene loadings)
 
-# %%
+# %% tags=[]
 multiplier_z = pd.read_pickle(conf.MULTIPLIER["MODEL_Z_MATRIX_FILE"])
 
-# %%
+# %% tags=[]
 multiplier_z.shape
 
-# %%
+# %% tags=[]
 multiplier_z.head()
 
 # %% [markdown] tags=[]
 # # Create LV-pathway dataframe
 
-# %%
+# %% tags=[]
 lv_pathway_df = multiplier_model_summary[
     ["LV index", "pathway", "AUC", "p-value", "FDR"]
 ]
 
-# %%
+# %% tags=[]
 lv_pathway_df["LV index"] = lv_pathway_df["LV index"].astype(int)
 
-# %%
+# %% tags=[]
 lv_pathway_df = lv_pathway_df.sort_values(["LV index", "FDR"])
 
-# %%
+# %% tags=[]
 lv_pathway_df["LV index"] = lv_pathway_df["LV index"].apply(lambda x: f"LV{x}")
 
-# %%
+# %% tags=[]
 lv_pathway_df.shape
 
-# %%
+# %% tags=[]
 lv_pathway_df = lv_pathway_df.rename(
     columns={"LV index": "LV identifier", "pathway": "Pathway"}
 )
 
-# %%
+# %% tags=[]
 lv_pathway_df.head()
 
-# %%
+# %% tags=[]
 lv_pathway_df.tail()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Save
 
-# %%
+# %% tags=[]
 output_file = OUTPUT_DIR / "lv-pathways.xlsx"
 display(output_file)
 
-# %%
+# %% tags=[]
 lv_pathway_df.to_excel(output_file, index=False)
 
-# %%
+# %% tags=[]
 # adjust column widths
 wb = openpyxl.load_workbook(filename=output_file)
 worksheet = wb.active
@@ -154,7 +154,7 @@ wb.save(output_file)
 # %% [markdown] tags=[]
 # # Create LV-gene dataframe
 
-# %%
+# %% tags=[]
 df = (
     multiplier_z.unstack()
     .to_frame()
@@ -162,41 +162,41 @@ df = (
     .rename(columns={0: "Weight", "level_0": "LV identifier", "level_1": "Gene symbol"})
 )
 
-# %%
+# %% tags=[]
 df = df.assign(lv_index=df["LV identifier"].apply(lambda x: int(x[2:])))
 
-# %%
+# %% tags=[]
 df = df.sort_values(["lv_index", "Weight"], ascending=[True, False]).drop(
     columns=["lv_index"]
 )
 
-# %%
+# %% tags=[]
 df.shape
 
-# %%
+# %% tags=[]
 df.head()
 
-# %%
+# %% tags=[]
 df.tail()
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Save as TSV
 
-# %%
+# %% tags=[]
 # output_file = OUTPUT_DIR / "lv-gene_weights.tsv.gz"
 # display(output_file)
 
-# %%
+# %% tags=[]
 # df.to_csv(output_file, sep="\t", index=False)
 
-# %% [markdown]
+# %% [markdown] tags=[]
 # ## Save as Excel
 
-# %%
+# %% tags=[]
 output_file = OUTPUT_DIR / "lv-gene_weights.xlsx"
 display(output_file)
 
-# %%
+# %% tags=[]
 with pd.ExcelWriter(output_file) as writer:
     for lv_id in df["LV identifier"].unique():
         print(lv_id, end=", ", flush=True)
@@ -206,7 +206,7 @@ with pd.ExcelWriter(output_file) as writer:
 
         lv_data.to_excel(writer, index=False, sheet_name=lv_id)
 
-# %%
+# %% tags=[]
 # adjust column widths
 wb = openpyxl.load_workbook(filename=output_file)
 
@@ -228,4 +228,4 @@ for worksheet in wb.worksheets:
 
 wb.save(output_file)
 
-# %%
+# %% tags=[]
