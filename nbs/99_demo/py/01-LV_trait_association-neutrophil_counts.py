1df4685fe2ce77268ab6944cd8ffff9f8406d9b5
Milton Pividori <miltondp@gmail.com>
2021-10-08 12:39:34 -0400

    demo: improve notebooks documentation

--- nbs/99_demo/py/01-LV_trait_association-neutrophil_counts.py
+++ nbs/99_demo/py/01-LV_trait_association-neutrophil_counts.py
@@ -18,7 +18,7 @@
 # # Description
 
 # %% [markdown]
-# TODO
+# This notebooks will show the structure of the main data matrices in PhenoPLIER, and will guide you in analyzing gene associations for a particular trait: neutrophil counts, which is presented in the [manuscript](https://greenelab.github.io/phenoplier_manuscript/#phenoplier-an-integration-framework-based-on-gene-co-expression-patterns) in Figure 1c.
 
 # %% [markdown]
 # # Modules
@@ -36,6 +36,9 @@ import conf
 # %% [markdown]
 # # Load gene module-gene membership matrix (matrix Z)
 
+# %% [markdown]
+# Here we load the gene module-gene membership matri, or "latent variables loadings matrix" (from the terminology of the [MultiPLIER article](https://doi.org/10.1016/j.cels.2019.04.003)).
+
 # %%
 matrix_z = pd.read_pickle(conf.MULTIPLIER["MODEL_Z_MATRIX_FILE"])
 
@@ -46,11 +49,18 @@ matrix_z.shape
 matrix_z.head()
 
 # %% [markdown]
-# Matrix Z contains the membership value for each gene across all LVs (or gene modules).
+# As you can see, this matrix Z contains the membership value for each gene across all LVs (or gene modules).
 # A value of zero means that the gene does not belong to that LV, whereas a larger value represents how strongly that gene belongs to the LV.
+# A group of genes that belong to the same LV represent a gene-set that has a similar expression profile across a set of tissues or cell types.
+# We'll cover this in more detail in the next notebook (`02-LV_cell_types-...`).
 
 # %% [markdown]
 # # Load information about LV alignment with pathways
+
+# %% [markdown]
+# LV in matrix Z can represent a group of genes that align well with prior pahtways (or prior knowledge) or be "novel" in the sense that the combination of genes do not represent a known unit but was found the PLIER when factorizing the recount2 data (see the MultiPLIER article for more details).
+#
+# Here we load that information, where for each LV and pathway, we have a p-value and area under the curve (AUC) that indicate how well the LV aligns to that pathway.
 
 # %%
 lv_metadata = pd.read_pickle(conf.MULTIPLIER["MODEL_SUMMARY_FILE"])
@@ -62,11 +72,13 @@ lv_metadata.shape
 lv_metadata.head()
 
 # %% [markdown]
-# # Load gene associations
+# # Load gene associations from PhenomeXcan
 
-# %%
-# FIXME: LINK PHENOMEXCAN PAPER AND EXPLAIN BRIEFLY WHAT ARE THESE RESULTS
-# MENTION TWAS AND LINK MULTIXCAN PAPER
+# %% [markdown]
+# Now we load the gene association across ~4,000 traits from [PhenomeXcan](https://doi.org/10.1126/sciadv.aba2083).
+# The file we load here are the Summary-MultiXcan (or S-MultiXcan) results, essentially a p-value for each gene-trait pair.
+# In the notebook I refer to these results generically as "TWAS results", meaning that we have gene-trait associations.
+# All these TWAS results were derived solely from GWAS summary stats, so you can also generate yours relatively easily by using [S-MultiXcan](https://doi.org/10.1371/journal.pgen.1007889).
 
 # %%
 phenomexcan_df = pd.read_pickle(conf.PHENOMEXCAN["SMULTIXCAN_MASHR_PVALUES_FILE"])
@@ -78,7 +90,7 @@ phenomexcan_df.shape
 phenomexcan_df.head()
 
 # %% [markdown]
-# Let's convert gene Ensembl IDs to symbols:
+# Convert gene Ensembl IDs to symbols:
 
 # %%
 phenomexcan_df = phenomexcan_df.rename(index=Gene.GENE_ID_TO_NAME_MAP)
@@ -90,7 +102,7 @@ phenomexcan_df = phenomexcan_df.loc[~phenomexcan_df.index.duplicated()]
 phenomexcan_df.head()
 
 # %% [markdown]
-# Let's keep genes present in our matrix Z only
+# Keep genes present in our matrix Z only:
 
 # %%
 common_genes = phenomexcan_df.index.intersection(matrix_z.index)
@@ -111,13 +123,14 @@ matrix_z.shape
 # %% [markdown]
 # # Take a look at genes associated with neutrophil counts
 
+# %% [markdown]
+# Below I search the PhenomeXcan results to find traits related to "neutrophils".
+
 # %%
 phenomexcan_df.columns[phenomexcan_df.columns.str.lower().str.contains("neutrophil")]
 
-# %%
-# trait_code = "30140_raw-Neutrophill_count"
-# t = Trait.get_trait(full_code=trait_code)
-# display(f"{trait_code} - sample size: {t.n}")
+# %% [markdown]
+# For this demo, I select the the "neutrophil count" TWAS derived from the GWAS performed by [Astle et. al](https://doi.org/10.1016/j.cell.2016.10.042). Below you can see the sample size:
 
 # %%
 trait_code = "Astle_et_al_2016_Neutrophil_count"
@@ -125,10 +138,7 @@ t = Trait.get_trait(full_code=trait_code)
 display(f"{trait_code} - sample size: {t.n}")
 
 # %%
-# traits_df = phenomexcan_df[["30140_raw-Neutrophill_count", "Astle_et_al_2016_Neutrophil_count"]].dropna()
-
-# %%
-traits_df = phenomexcan_df[["Astle_et_al_2016_Neutrophil_count"]].dropna()
+traits_df = phenomexcan_df[[trait_code]].dropna()
 
 # %%
 traits_df.shape
@@ -136,20 +146,24 @@ traits_df.shape
 # %%
 traits_df.head()
 
+# %% [markdown]
+# Here I quickly show the data summary for this trait's gene associations:
+
 # %%
 traits_df.apply(lambda x: -np.log10(x)).describe()
 
-# %%
-# traits_df.apply(lambda x: -np.log10(x)).corr()
+# %% [markdown]
+# Make sure we don't have missing values or NaN.
 
 # %%
 assert not traits_df.isna().any().any()
+assert not np.isinf(traits_df).any().any()
 
 # %% [markdown]
 # # Convert p-values to z-scores
 
 # %% [markdown]
-# This converts a p-value to a scalar > 0, where higher positive values mean stronger association, and lower values close to zero mean weaker associations.
+# This converts a p-value to a scalar > 0, where higher positive values mean stronger association, and lower values close to zero mean weaker associations. Check our manuscript for more details on this.
 
 # %%
 traits_zscores = pd.DataFrame(
@@ -160,6 +174,7 @@ traits_zscores = pd.DataFrame(
 
 # %%
 assert not traits_zscores.isna().any().any()
+assert not np.isinf(traits_zscores).any().any()
 
 # %%
 traits_zscores.head()
@@ -168,23 +183,25 @@ traits_zscores.head()
 traits_zscores.describe()
 
 # %% [markdown]
-# # Analysis of an neutrophil-termed LV
+# # Analysis of a neutrophil-termed LV
 
 # %% [markdown]
-# Let's take as an example an LV that was previously analyzed in another study [here](https://doi.org/10.1016/j.cels.2019.04.003), which we identify as `LV603`. This LV aligns well with pathways related to neutrophils, as you can see below.
+# Let's take as an example an LV that was previously analyzed in the MultiPLIER study, which we identify as `LV603`. This LV aligns well with pathways related to neutrophils, as you can see below.
 
 # %%
-lv_metadata[lv_metadata["LV index"] == "603"].sort_values("FDR")
+lv_metadata[
+    (lv_metadata["LV index"] == "603") & (lv_metadata["FDR"] < 0.05)
+].sort_values("FDR")
 
 # %% [markdown]
-# Let's see which genes more strongly belong to LV603:
+# Let's see which genes more strongly belong to LV603 (the numbers are the gene weights in this LV):
 
 # %%
 lv603_top_genes = matrix_z["LV603"].sort_values(ascending=False)
 display(lv603_top_genes.head(20))
 
 # %% [markdown]
-# Are these top genes associated with our neutrophil?
+# Are these top genes associated with our neutrophil count?
 
 # %%
 traits_zscores.loc[lv603_top_genes.index].head(20)
@@ -197,7 +214,8 @@ traits_zscores.loc[lv603_top_genes.index].head(20)
 traits_zscores.sample(n=20, random_state=0)
 
 # %% [markdown]
-# They do not seem as high as those with the top gene in LV603. If we compute the correlation between LV603 gene weights (`lv603_top_genes`) and gene associations for neutrophil counts (`traits_zscores`) we get this:
+# They do not seem as high as those within the top genes in LV603.
+# If we compute the correlation between LV603 gene weights (`lv603_top_genes`) and gene associations for neutrophil counts (`traits_zscores`) we get this:
 
 # %%
 stats.pearsonr(
@@ -206,7 +224,8 @@ stats.pearsonr(
 )
 
 # %% [markdown]
-# Although the correlation is significant (`8.16e-10`) and the slope positive (we are interested only in genes at the top of the LV), we need to account for correlated predicted expression from the TWAS models (for example, if the expression of two genes at the top of the LV is correlated, results will not be as significant after accounting for this). We have a class implemented in Python that computes this, as shown below.
+# Although the correlation is significant (`8.16e-10`) and the slope positive (we are interested only in genes at the top of the LV), we need to account for correlated predicted expression from the TWAS models (for example, if the expression of two genes at the top of the LV is correlated that would invalidate our test).
+# We have a class implemented in Python that computes this, as shown below.
 
 # %% [markdown]
 # # Association between an LV and a trait
@@ -218,7 +237,7 @@ stats.pearsonr(
 from gls import GLSPhenoplier
 
 # %% [markdown]
-# We need to save our trait gene association to a pickle file before:
+# We need to save our trait gene association to a pickle file before moving on. Here I generate a temporary file path and save:
 
 # %%
 with tempfile.NamedTemporaryFile(suffix=".pkl") as f:
@@ -247,7 +266,7 @@ gls_model = GLSPhenoplier(
 gls_model.fit_named(lv_code, phenotype_code)
 
 # %% [markdown]
-# You can show the entire table of results from the statsmodels model:
+# It will take a few seconds. You can show the entire table of results from the [statsmodels's GLS model](https://www.statsmodels.org/stable/generated/statsmodels.regression.linear_model.GLS.html):
 
 # %%
 res = gls_model.results
@@ -268,20 +287,25 @@ print(res.summary())
 res.params
 
 # %% [markdown]
-# Two-sided p-values:
-
-# %%
-res.pvalues
-
-# %% [markdown]
-# One-sided p-values
+# The one-sided p-values (the ones we used in our manuscript, since we are only interested in the top genes of an LV):
 
 # %%
 res.pvalues_onesided
 
 # %% [markdown]
+# And the two-sided p-values (in case you need them or are interested in a different hypothesis):
+
+# %%
+res.pvalues
+
+# %% [markdown]
 # # Conclusions
 
 # %% [markdown]
-# - summary of what we did
-# - next steps
+# Hopefully, now have a more clear idea of the main data matrixes involved in PhenoPLIER (matrix Z, PhenomeXcan gene-trait associations, etc).
+# We also see how to compute a p-value between an LV (group of genes or gene module) and a trait of interest.
+# To do this with your own data, you only need to compute the S-MultiXcan TWAS results (gene-based) from your GWAS summary stats.
+#
+# In the next notebook (`02-LV_cell_types-...`), we'll see how to check in which tissues or cell types are our LV603'genes expressed.
+
+# %%
--- nbs/99_demo/py/02-LV_cell_types-neutrophil_counts.py
+++ nbs/99_demo/py/02-LV_cell_types-neutrophil_counts.py
@@ -19,12 +19,12 @@
 
 # %% [markdown]
 # In the previous notebook, we found that LV603 gene weight's are predictive of gene associations for neutrophil counts.
-# In a real application, you would run the `GLSPhenoplier` for your trait of interested across all LVs in our models, and get the significant ones. Then you can see in which cell types the LV's genes are expressed, and this is what we are going to do in this notebook for LV603.
+# In a real application, you would run the `GLSPhenoplier` for your trait of interested across all LVs in our models, and get the significant ones. Then you can see in which cell types the LVs' genes are expressed, and this is what we are going to do in this notebook for LV603.
 #
-# To find the cell types associated with an LV, we'll use the matrix B (see our manuscript).
-# We can connected samples in matrix B with gene expression metadata and explore which cell types are associated with the LV.
-# However, metadata in gene expression datasets is usually hard to read, process and interpret.
-# For example, for some samples that could be important for our LV, we might not have access to the cell types where the expriments were conducted. We'll show here what we can do to try to overcome this.
+# To find the cell types associated with an LV, we'll use matrix B (see our [manuscript](https://greenelab.github.io/phenoplier_manuscript/#phenoplier-an-integration-framework-based-on-gene-co-expression-patterns)).
+# We can link RNA-seq samples in matrix B with gene expression metadata and explore which cell types are associated with the LV.
+# However, metadata in gene expression datasets is usually hard to read, process and interpret, and many times important attributes (such as `tissue` or `cell type` are missing).
+# We'll show here what we can do to try to overcome this.
 
 # %% [markdown]
 # # Modules
@@ -44,13 +44,17 @@ import conf
 # %% [markdown] tags=[]
 # # Settings
 
+# %% [markdown] tags=[]
+# Specify below the LV id you are interested in:
+
 # %% tags=["parameters"]
 LV_NAME = "LV603"
 
-# %%
-# LV_AXIS_THRESHOLD = 3.0
-# N_TOP_SAMPLES = 400
-# N_TOP_ATTRS = 25
+# %% [markdown] tags=[]
+# # Paths
+
+# %% [markdown] tags=[]
+# These are paths to folder and files where we'll save our figures.
 
 # %%
 OUTPUT_FIGURES_DIR = Path(conf.RESULTS_DIR, "demo", f"{LV_NAME.lower()}").resolve()
@@ -58,7 +62,6 @@ display(OUTPUT_FIGURES_DIR)
 OUTPUT_FIGURES_DIR.mkdir(parents=True, exist_ok=True)
 
 # %%
-# this is the path of the figure that we will generate, in case you want to export it
 OUTPUT_CELL_TYPE_FILEPATH = OUTPUT_FIGURES_DIR / f"{LV_NAME.lower()}-cell_types.svg"
 display(OUTPUT_CELL_TYPE_FILEPATH)
 
@@ -75,16 +78,22 @@ multiplier_model_summary.shape
 multiplier_model_summary.head()
 
 # %% [markdown] tags=[]
-# # Load data
+# # Load LV data
 
-# %% [markdown]
-# ## LV data
+# %% [markdown] tags=[]
+# You can use the `LVAnalysis` class to explore an LV.
 
 # %%
-lv_obj = LVAnalysis(LV_NAME)  # FIXME: does it work without data as argument?
+lv_obj = LVAnalysis(LV_NAME)
+
+# %% [markdown] tags=[]
+# Here I show the top 20 genes for our LV. You can see gene symbols, the LV weight (in column `LV603`) and the cytoband.
 
 # %%
 lv_obj.lv_genes.head(20)
+
+# %% [markdown]
+# Show the pathways our LV is aligned to (neutrophils):
 
 # %%
 multiplier_model_summary[
@@ -94,6 +103,11 @@ multiplier_model_summary[
         & (multiplier_model_summary["AUC"] >= 0.75)
     )
 ]
+
+# %% [markdown]
+# Here I load the LV metadata.
+# This is, for each sample from our matrix B, we load its metadata.
+# The `LVAnalysis` class takes care of downloading all necessary files from recount2.
 
 # %%
 lv_data = lv_obj.get_experiments_data()
@@ -110,9 +124,16 @@ lv_data.head()
 # %% [markdown]
 # ## Get top attributes
 
+# %% [markdown]
+# Here I quickly show the top attributes in our metadata for which we have the largest LV variance.
+# This is just to have an idea of the variance across different attributes
+
 # %%
 lv_attrs = lv_obj.get_attributes_variation_score()
 display(lv_attrs.head(20))
+
+# %% [markdown]
+# Since we are interested in cell types and tissues, the code below will find which attributes contain "cell type" or "tissue", so we can select from these results.
 
 # %%
 # show those with cell type or tissue in their name
@@ -124,6 +145,9 @@ lv_attrs[
         flags=re.IGNORECASE,
     ).values
 ].sort_values(ascending=False)
+
+# %% [markdown]
+# Select from the output above those you are interested in. Usually all of them.
 
 # %%
 lv_attrs_selected = [
@@ -141,35 +165,55 @@ _tmp = lv_data.loc[
 # %%
 _tmp_seq = list(chunker(_tmp.sort_values(LV_NAME, ascending=False), 25))
 
-# %%
-# if you change the number here you can paginate over the entire set of results; for example, with `_tmp_seq[1]` you'll see the next "page"
-_tmp_seq[0]
+# %% [markdown]
+# Now that we selected attributes that might be providing information about the cell type, we can see which are the top ones.
+#
+# If you change the number in the cell below, you can paginate over the entire set of results; for example, with `_tmp_seq[1]` you'll see the next "page" in a descending order with respect to the LV value. So the ones at the top are the most important ones.
 
 # %%
-# what is there in these projects?
+_tmp_seq[0]
+
+# %% [markdown]
+# You can see above that the RNA-seq samples are from neutrophils or granulocytes.
+# If we move to the next "page", we'll see this:
+
+# %%
+_tmp_seq[1]
+
+# %% [markdown]
+# Although the attributes we selected (`cell type`, `celltype` and `tissue`) seem to be enough for most of the RNA-seq samples to get their cell types, this is not the case for some of them. For example, we have empty values (NaN) for `SRP015360`.
+#
+# You can explore the metadata provided for a particular SPR with the code below. Let's see what we have in `SRP015360`:
+
+# %%
+# list the top 10 samples from this project
 lv_data.loc[["SRP015360"]].dropna(how="all", axis=1).sort_values(
     LV_NAME, ascending=False
 ).head(10)
 
 # %% [markdown]
-# MENTION HERE THAT YOU CAN GO TO THIS PLACE FOR THIS SRP: https://trace.ncbi.nlm.nih.gov/Traces/sra/?study=SRP015360
+# So for `SRP015360` we only have three attributes: `age`, `Sex` and `treatment`. No cell type information. Here you can start grasping the challenges in analyzing this data.
 #
-# And in this case we see it's neutrophils under different stimuli.
+# If you are really interested in this metadata for this project, you can use this URL:
+# ```
+# https://trace.ncbi.nlm.nih.gov/Traces/sra/?study=SRP015360
+# ```
+# where you can put the SRP code you want. If you check the URL above, you'll see that these samples were taken from neutrophils as well (treated with different stimuli), although we don't have metadata about the cell type/tissue.
+
+# %% [markdown]
+# Ok, we do not want several attributes conveying the same information (cell types), just a single one, so here we'll combine the attributes we found before with a particular _order_.
+# In this case I select `cell type` _first_ (the order is important), `celltype` as second, and `tissue` as third/last.
+# This means that I will use the information in `cell type` first, but if this is empty (as in `SRP045500`), we'll try with `celltype`, and so on.
 
 # %%
-# TODO: select here in the order desired.
 SELECTED_ATTRIBUTES = [
     "cell type",
     "celltype",
     "tissue",
 ]
 
-# %%
-# it has to be in the order desired for filling nans in the SELECTED_ATTRIBUTE
-# SECOND_ATTRIBUTES = ["celltype", ]
-
 # %% [markdown]
-# ## Get plot data
+# ## Get a single attribute with "cell type/tissue" information
 
 # %%
 plot_data = lv_data.loc[:, SELECTED_ATTRIBUTES + [LV_NAME]]
@@ -192,12 +236,14 @@ plot_data.head(20)
 # %% [markdown]
 # You can see that now all attributes ("celltype", "cell type" and "tissue") are combined under a single attribute named "cell type".
 # For example, if you look at `SRP045500`, which had a value under "celltype" but an empty (NaN) value for attribute "cell type", that now the value was moved to "cell type" (it was unified into this single attribute).
+#
+# If you see "NOT CATEGORIZED", it means that none of the attributes you used had a value (they were all empty or NaN). In this case you would go the URL before and read the article/RNA-seq data description to find out the cell type or tissue (or whatever attribute your are interested in).
 
 # %% [markdown]
-# ## Customize x-axis values
+# ## Standardize cell type names
 
 # %% [markdown]
-# When cell type values are not very clear, customize their names by looking at their specific studies to know exactly what the authors meant.
+# When cell type values are different but represent the same cell type, we unify them:
 
 # %%
 final_plot_data = plot_data.replace(
@@ -228,6 +274,9 @@ final_plot_data = plot_data.replace(
     }
 )
 
+# %% [markdown]
+# The code below is more advanced, but you can use it to customize even further your final attribute value (for example, if you want to use the cell type, but put the treatment name with parenthesis).
+
 # %%
 # # add also tissue information to these projects
 # _srp_code = "SRP061881"
@@ -236,41 +285,23 @@ final_plot_data = plot_data.replace(
 #     + f" ({lv_data.loc[(_srp_code, x.name), 'tissue']})",
 #     axis=1,
 # )
-# final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
-
-
-# _srp_code = "SRP056049"
-# _tmp = final_plot_data.loc[(_srp_code,)].apply(
-#     lambda x: lv_data.loc[(_srp_code, x.name), "cell population"]
-#     + f" ({lv_data.loc[(_srp_code, x.name), 'diagnosis']})",
-#     axis=1,
-# )
-# final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
-
-
-# _srp_code = "SRP059057"
-# _tmp = final_plot_data.loc[(_srp_code,)].apply(
-#     lambda x: lv_data.loc[(_srp_code, x.name), "cell stimulation"]
-#     + f" ({lv_data.loc[(_srp_code, x.name), 'phenotype']})",
-#     axis=1,
-# )
-# final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTE] = _tmp.values
+# final_plot_data.loc[(_srp_code, _tmp.index), SELECTED_ATTRIBUTES[0]] = _tmp.values
 
 # %%
-# # all samples from SRP049593 are fibroblasts
+# # all samples from SRP015360 are neutrophils
 # final_plot_data[SELECTED_ATTRIBUTES[0]] = final_plot_data.apply(
-#     lambda x: "Myeloma cells" if x.name[0] in ("SRP027015",) else x["cell type"],
+#     lambda x: "Neutrophils" if x.name[0] in ("SRP015360",) else x[SELECTED_ATTRIBUTES[0]],
 #     axis=1,
 # )
 
 # %%
-# take the top samples only
-final_plot_data = final_plot_data.sort_values(
-    LV_NAME, ascending=False
-)  # [:N_TOP_SAMPLES]
+final_plot_data = final_plot_data.sort_values(LV_NAME, ascending=False)
 
 # %% [markdown]
 # ## Threshold LV values
+
+# %% [markdown]
+# The code below is useful in case you want to put a threshold to the plot (for example, you have same samples with very high values).
 
 # %%
 # final_plot_data.loc[
@@ -280,13 +311,17 @@ final_plot_data = final_plot_data.sort_values(
 # %% [markdown]
 # ## Delete samples with no tissue/cell type information
 
+# %% [markdown]
+# If you are not interested in samples for which we couldn't find cell type/tissue information, you can delete them by uncommenting the code below.
+# However, if you want, you can take a look at those later (we'll show you how at the end).
+
 # %%
 # final_plot_data = final_plot_data[
 #     final_plot_data[SELECTED_ATTRIBUTE] != "NOT CATEGORIZED"
 # ]
 
 # %% [markdown]
-# ## Set x-axis order
+# ## Set top cell types to show
 
 # %%
 N_TOP_ATTRS = 10
@@ -307,7 +342,11 @@ len(attr_order)
 attr_order[:5]
 
 # %% [markdown]
-# ## Plot
+# # Plot
+
+# %% [markdown]
+# Now we create the plot with RNA-seq samples as points, cell types in the x-axis, and the LV value in the y-axis.
+# This shows you in which cell types genes in LV603 are primirily expressed.
 
 # %%
 with sns.plotting_context("paper", font_scale=1.5), sns.axes_style("whitegrid"):
@@ -322,6 +361,9 @@ with sns.plotting_context("paper", font_scale=1.5), sns.axes_style("whitegrid"):
     )
     plt.xticks(rotation=45, horizontalalignment="right")
 
+    # You can save the figure if you uncomment the code below
+    # the figure will be created under ${DATA_FOLDER}/results/demo
+
 #     plt.savefig(
 #         OUTPUT_CELL_TYPE_FILEPATH,
 #         bbox_inches="tight",
@@ -330,6 +372,11 @@ with sns.plotting_context("paper", font_scale=1.5), sns.axes_style("whitegrid"):
 
 # %% [markdown]
 # # Debug
+
+# %% [markdown]
+# Now that you have the figure for your LV, you can debug some things.
+# For example, you'll see above that some samples are "NOT CATEGORIZED".
+# Let's see which are those:
 
 # %%
 with pd.option_context(
@@ -340,10 +387,22 @@ with pd.option_context(
     ]
     display(_tmp.head(20))
 
+# %% [markdown]
+# We don't have information about cell type/tissue for these.
+# But we can manually go to the URL showed before and check out.
+# For example, for `SRP015360` we can open our browser with this URL:
+# https://trace.ncbi.nlm.nih.gov/Traces/sra/?study=SRP015360, and see that the samples were taken from neutrophils.
+#
+# If you need to see which information is provided by each SRP, you can again use the code below:
+
 # %%
 # what is there in these projects?
 lv_data.loc[["SRP015360"]].dropna(how="all", axis=1).sort_values(
     LV_NAME, ascending=False
 ).head(60)
+
+# %% [markdown]
+# Now you can go back to section `Standardize cell type names` in this notebook and adjust according to this findings to improve the figure.
+# Once you are happy, you can remove the `NOT CATEGORIZED` items by uncommenting the code in `Delete samples with no tissue/cell type information`.
 
 # %%
