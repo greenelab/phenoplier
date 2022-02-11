8be9a30529e216768c6a4ce813937c1ef8cd3806
Milton Pividori <miltondp@gmail.com>
2022-02-11 10:38:48 -0500

    asthma: add env.sh file

---
+++ scripts/asthma/env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This file is project-specific and it exports some common environmental
+# variables to run the code.  It has to be customized for your need by changing
+# the BASE_DIR and N_JOBS below.
+
+#
+# Your settings here
+#
+
+# BASE_DIR is the parent directory where the code and manuscript repos are
+# located.
+BASE_DIR=/home/miltondp/projects/labs/greenelab/phenoplier-asthma
+
+# Project name
+PROJECT_NAME=phenoplier-asthma
+
+# Number of CPUs to use
+export N_JOBS=3
+
+#
+# Do not edit below
+#
+export PHENOPLIER_ROOT_DIR=${BASE_DIR}/${PROJECT_NAME}/base
+echo "PHENOPLIER_ROOT_DIR=${PHENOPLIER_ROOT_DIR}"
+
+export PHENOPLIER_MANUSCRIPT_DIR=${BASE_DIR}/${PROJECT_NAME}-manuscript/
+echo "PHENOPLIER_MANUSCRIPT_DIR=${PHENOPLIER_MANUSCRIPT_DIR}"
+
+export PYTHONPATH=${BASE_DIR}/${PROJECT_NAME}/libs/
+echo "PYTHONPATH=${PYTHONPATH}"
+
