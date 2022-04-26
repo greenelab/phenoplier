3f538a0983b5fc4b480a5b32e201aa0b51f7f763
Milton Pividori <miltondp@gmail.com>
2022-04-26 17:43:01 -0400

    styler.r: add documentation

--- scripts/styler.r
+++ scripts/styler.r
@@ -1,5 +1,9 @@
 #!/usr/bin/env Rscript
 
+# It takes a filepath as argument (an R script) and apply a code style using
+# the R package styler (it's similar to black in Python).
+
 args <- commandArgs(trailingOnly = TRUE)
 file_name <- args[1L]
 styler::style_file(file_name)
+
