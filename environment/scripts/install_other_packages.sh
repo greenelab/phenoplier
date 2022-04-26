c2531f3f59208059a3cd79cf4fa59133639f20d1
Milton Pividori <miltondp@gmail.com>
2022-04-26 16:55:33 -0400

    environment: migrate some changes from CCC repo

--- environment/scripts/install_other_packages.sh
+++ environment/scripts/install_other_packages.sh
@@ -5,13 +5,11 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Fix tqdm with JupyterLab: https://github.com/tqdm/tqdm/issues/394#issuecomment-384743637
-jupyter nbextension enable --py widgetsnbextension
-
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-
-jupyter labextension install @jupyterlab/toc
+# jupyter nbextension enable --py widgetsnbextension
+# jupyter labextension install @jupyter-widgets/jupyterlab-manager
 
 #
 # R dependencies
 #
 TAR=$(which tar) Rscript ${SCRIPT_DIR}/install_r_packages.r
+
