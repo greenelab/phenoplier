c72c9ef91757bb3f3917dcd7c4097dc6522052d9
Milton Pividori <miltondp@gmail.com>
2022-04-27 16:43:18 -0400

    gls/null/gwas: use plink 1.9

--- nbs/15_gsa_gls/20-null_simulations/01-preprocess.sh
+++ nbs/15_gsa_gls/20-null_simulations/01-preprocess.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+N_JOBS="${PHENOPLIER_GENERAL_N_JOBS}"
 INPUT_DIR="${PHENOPLIER_A1000G_GENOTYPES_DIR}"
 SUBSETS_DIR="${PHENOPLIER_A1000G_GENOTYPES_DIR}/subsets"
-PLINK2="${PHENOPLIER_PLINK2_EXECUTABLE}"
+PLINK2="${PHENOPLIER_PLINK_EXECUTABLE_VERSION_2}"
+PLINK19="${PHENOPLIER_PLINK_EXECUTABLE_VERSION_1_9}"
 
 mkdir -p ${SUBSETS_DIR}
 
@@ -10,7 +12,8 @@ mkdir -p ${SUBSETS_DIR}
 $PLINK2 --zst-decompress ${INPUT_DIR}/all_phase3.pgen.zst > ${INPUT_DIR}/all_phase3.pgen
 
 # convert to plink 1 binary format
-$PLINK2 --pfile ${INPUT_DIR}/all_phase3 vzs \
+$PLINK19 --pfile ${INPUT_DIR}/all_phase3 vzs \
+    --threads ${N_JOBS} \
     --max-alleles 2 \
     --make-bed \
     --out ${SUBSETS_DIR}/all_phase3
