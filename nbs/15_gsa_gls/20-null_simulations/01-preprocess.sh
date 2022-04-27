4bd57075e53950261fac3a68dc1812b044e383c1
Milton Pividori <miltondp@gmail.com>
2022-04-27 15:52:22 -0400

    gls/null/gwas: fix and rename script for preprocessing

--- nbs/15_gsa_gls/20-null_simulations/01-preprocess.sh
+++ nbs/15_gsa_gls/20-null_simulations/01-preprocess.sh
@@ -4,6 +4,8 @@ INPUT_DIR="${PHENOPLIER_A1000G_GENOTYPES_DIR}"
 SUBSETS_DIR="${PHENOPLIER_A1000G_GENOTYPES_DIR}/subsets"
 PLINK2="${PHENOPLIER_PLINK2_EXECUTABLE}"
 
+mkdir -p ${SUBSETS_DIR}
+
 # decompress genotype data
 $PLINK2 --zst-decompress ${INPUT_DIR}/all_phase3.pgen.zst > ${INPUT_DIR}/all_phase3.pgen
 $PLINK2 --pfile ${INPUT_DIR}/all_phase3 vzs --max-alleles 2 --make-bed --out ${SUBSETS_DIR}/all_phase3
