0f6c9d3736c35c518d6873b22eb36777797c7a4b
Milton Pividori <miltondp@gmail.com>
2022-04-28 17:19:27 -0400

    gls/null/gwas: add sex discrepancy scripts

--- nbs/15_gsa_gls/20-null_simulations/03_05-qc-sex_discrepancy.sh
+++ nbs/15_gsa_gls/20-null_simulations/03_05-qc-sex_discrepancy.sh
@@ -6,18 +6,18 @@ SUBSETS_DIR="${PHENOPLIER_A1000G_GENOTYPES_DIR}/subsets"
 PLINK2="${PHENOPLIER_PLINK_EXECUTABLE_VERSION_2}"
 PLINK19="${PHENOPLIER_PLINK_EXECUTABLE_VERSION_1_9}"
 
-# sex discrepancy
+# --split-x
 $PLINK19 --bfile ${SUBSETS_DIR}/all_phase3.1 \
     --threads ${N_JOBS} \
+    --allow-extra-chr \
+    --split-x b37 'no-fail' \
+    --make-bed \
+    --out ${SUBSETS_DIR}/all_phase3.1.split_x
+
+# sex discrepancy (--check-sex)
+$PLINK19 --bfile ${SUBSETS_DIR}/all_phase3.1.split_x \
+    --threads ${N_JOBS} \
+    --extract ${SUBSETS_DIR}/all_phase3.1.chrX.indepSNP.prune.in \
     --allow-extra-chr \
     --check-sex \
-    --out ${SUBSETS_DIR}/all_phase3.1.sexcheck
-
-grep "PROBLEM" ${SUBSETS_DIR}/all_phase3.1.sexcheck.sexcheck | awk '{print$1,$2}'> ${SUBSETS_DIR}/all_phase3.1.sexcheck.sex_discrepancy.txt
-
-$PLINK19 --bfile ${SUBSETS_DIR}/all_phase3.1 \
-    --threads ${N_JOBS} \
-    --allow-extra-chr \
-    --remove ${SUBSETS_DIR}/all_phase3.1.sexcheck.sex_discrepancy.txt \
-    --make-bed \
-    --out ${SUBSETS_DIR}/all_phase3.1.1
+    --out ${SUBSETS_DIR}/all_phase3.1.split_x.sexcheck
