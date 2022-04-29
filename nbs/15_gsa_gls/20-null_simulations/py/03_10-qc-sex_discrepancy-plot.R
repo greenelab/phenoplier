bbb13e8ab072dbc1956c00aad39b3637333e7c7e
Milton Pividori <miltondp@gmail.com>
2022-04-29 13:02:56 -0400

    gls/null/gwas: add GWAS runs and checks/plots

---
+++ nbs/15_gsa_gls/20-null_simulations/py/03_10-qc-sex_discrepancy-plot.R
@@ -0,0 +1,104 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all,-execution,-papermill,-trusted
+#     text_representation:
+#       extension: .R
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+# %% [markdown] tags=[]
+# # Description
+
+# %% [markdown] tags=[]
+# Read the missing info computed previously (by individual and variant) and plots some histograms.
+
+# %% [markdown] tags=[]
+# # Modules
+
+# %% tags=[]
+library(tidyverse)
+
+# %% [markdown] tags=[]
+# # Paths
+
+# %% tags=[]
+A1000G_GENOTYPES_DIR <- Sys.getenv("PHENOPLIER_A1000G_GENOTYPES_DIR")
+
+# %% tags=[]
+A1000G_GENOTYPES_DIR
+
+# %% tags=[]
+SUBSETS_DIR <- file.path(A1000G_GENOTYPES_DIR, "subsets")
+
+# %% tags=[]
+SUBSETS_DIR
+
+# %% [markdown] tags=[]
+# # Load data
+
+# %%
+gender <- read.table(file.path(SUBSETS_DIR, "all_phase3.1.split_x.sexcheck.sexcheck"), header=T,as.is=T)
+
+# %%
+dim(gender)
+
+# %%
+head(gender)
+
+# %% [markdown] tags=[]
+# # Histogram
+
+# %% tags=[]
+hist(gender[,6],main="Gender", xlab="F")
+
+# %% tags=[]
+gender %>% summarise(mean = mean(F), sd = sd(F), max = max(F), min = min(F))
+
+# %% tags=[]
+gender %>%
+  filter(F < 0.20) %>%
+  dim_desc()
+
+# %% tags=[]
+gender %>%
+  filter(F < 0.21) %>%
+  dim_desc()
+
+# %% tags=[]
+gender %>%
+  filter(F < 0.22) %>%
+  dim_desc()
+
+# %% tags=[]
+gender %>%
+  filter(F < 0.23) %>%
+  dim_desc()
+
+# %% tags=[]
+gender %>%
+  filter(F < 0.24) %>%
+  dim_desc()
+
+# %% tags=[]
+gender %>%
+  filter(F < 0.25) %>%
+  dim_desc()
+
+# %% tags=[]
+gender %>%
+  filter(F > 0.80) %>%
+  dim_desc()
+
+# %% tags=[]
+gender %>%
+  filter(STATUS == "PROBLEM") %>%
+  dim_desc()
+
+# %% tags=[]
