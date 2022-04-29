970c48bfa2f013989ed039387061bfd6f39723ba
Milton Pividori <miltondp@gmail.com>
2022-04-29 12:31:11 -0400

    gls/null/gwas: add scripts/notebooks for samples filtering (het)

--- nbs/15_gsa_gls/20-null_simulations/05_00-qc-variants-indep.sh
+++ nbs/15_gsa_gls/20-null_simulations/05_00-qc-variants-indep.sh
@@ -7,9 +7,9 @@ SUBSETS_DIR="${PHENOPLIER_A1000G_GENOTYPES_DIR}/subsets"
 PLINK2="${PHENOPLIER_PLINK_EXECUTABLE_VERSION_2}"
 PLINK19="${PHENOPLIER_PLINK_EXECUTABLE_VERSION_1_9}"
 
-sed 's/"// g' ${SUBSETS_DIR}/all_phase3.4.fail-het-qc.txt | awk '{print$1, $2}'> ${SUBSETS_DIR}/all_phase3.4.het_fail_ind.txt
-
+# independent snps (not correlated)
 $PLINK19 --bfile ${SUBSETS_DIR}/all_phase3.4 \
-    --remove ${SUBSETS_DIR}/all_phase3.4.het_fail_ind.txt \
-    --make-bed \
-    --out ${SUBSETS_DIR}/all_phase3.5
+    --threads ${N_JOBS} \
+    --exclude range ${SCRIPT_DIR}/inversion.txt \
+    --indep-pairwise 50 5 0.2 \
+    --out ${SUBSETS_DIR}/all_phase3.4.indepSNP
