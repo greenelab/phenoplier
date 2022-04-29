a6bec8b48dda25c6d29e0ef38cc319f596fa4900
Milton Pividori <miltondp@gmail.com>
2022-04-29 12:32:49 -0400

    gls/null/gwas: add scripts/notebooks for samples filtering (relatedness)

---
+++ nbs/15_gsa_gls/20-null_simulations/06_05-qc-samples-crypted_rel-ibd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+N_JOBS="${PHENOPLIER_GENERAL_N_JOBS}"
+INPUT_DIR="${PHENOPLIER_A1000G_GENOTYPES_DIR}"
+SUBSETS_DIR="${PHENOPLIER_A1000G_GENOTYPES_DIR}/subsets"
+PLINK2="${PHENOPLIER_PLINK_EXECUTABLE_VERSION_2}"
+PLINK19="${PHENOPLIER_PLINK_EXECUTABLE_VERSION_1_9}"
+
+# # crypted relatedness in founders
+# $PLINK19 --bfile ${SUBSETS_DIR}/all_phase3.6 \
+#     --threads ${N_JOBS} \
+#     --extract ${SUBSETS_DIR}/all_phase3.4.indepSNP.prune.in \
+#     --genome \
+#     --min 0.2 \
+#     --out ${SUBSETS_DIR}/all_phase3.6.pihat_min0.2
