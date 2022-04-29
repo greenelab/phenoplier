dcc089ea90fc6545e6166fab05684ede07e991cf
Milton Pividori <miltondp@gmail.com>
2022-04-29 12:29:25 -0400

    gls/null/gwas: add scripts/notebooks for variant filtering

--- nbs/15_gsa_gls/20-null_simulations/py/04_04-qc-variants-maf-plot.R
+++ nbs/15_gsa_gls/20-null_simulations/py/04_04-qc-variants-maf-plot.R
@@ -44,62 +44,28 @@ SUBSETS_DIR
 # # Load data
 
 # %% tags=[]
-hwe <- as.data.frame(read_table(file.path(SUBSETS_DIR, "all_phase3.3.hwe"), col_types = cols_only(P = "n")))
+maf_freq <- as.data.frame(read_table(file.path(SUBSETS_DIR, "all_phase3.2.maf.frq"), col_types = cols_only(MAF = "n")))
 
 # %% tags=[]
-dim(hwe)
+dim(maf_freq)
 
 # %% tags=[]
-head(hwe)
+head(maf_freq)
 
 # %% [markdown] tags=[]
-# # Histogram on all SNPs
+# # Histogram
 
 # %% tags=[]
-hist(hwe[, 1], main = "Histogram HWE")
-
-# %% [markdown] tags=[]
-# # Histogram on strongly HWE deviating SNPs only
+hist(maf_freq[,1],main = "MAF distribution", xlab = "MAF")
 
 # %% tags=[]
-hwe %>%
-  filter(P < 1e-1) %>%
+maf_freq %>%
+  filter(MAF > 0.01) %>%
   dim_desc()
 
 # %% tags=[]
-hwe %>%
-  filter(P < 1e-2) %>%
+maf_freq %>%
+  filter(MAF > 0.05) %>%
   dim_desc()
-
-# %% tags=[]
-hwe %>%
-  filter(P < 1e-4) %>%
-  dim_desc()
-
-# %% tags=[]
-hwe %>%
-  filter(P < 1e-6) %>%
-  dim_desc()
-
-# %% tags=[]
-hwe %>%
-  filter(P < 1e-8) %>%
-  dim_desc()
-
-# %% tags=[]
-hwe %>%
-  filter(P < 1e-10) %>%
-  dim_desc()
-
-# %% tags=[]
-hwe %>%
-  filter(P < 1e-15) %>%
-  dim_desc()
-
-# %% tags=[]
-hwe_zoom <- hwe %>% filter(P < 5e-6)
-
-# %% tags=[]
-hist(hwe_zoom[, 1], main = "Histogram HWE: strongly deviating SNPs only")
 
 # %% tags=[]
