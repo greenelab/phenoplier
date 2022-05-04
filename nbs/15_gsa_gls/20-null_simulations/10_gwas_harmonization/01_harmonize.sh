386e3768af7b3efb2b8526d080369afbbf033317
Milton Pividori <miltondp@gmail.com>
2022-05-04 11:37:34 -0400

    gls/null: improve harmonization script

--- nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/01_harmonize.sh
+++ nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/01_harmonize.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Runs the harmonization step for all GWAS on random phenotypes.
+# Runs the harmonization step.
 
+# read arguments
 POSITIONAL_ARGS=()
 
 while [[ $# -gt 0 ]]; do
   case $1 in
     -i|--input-gwas-file)
       INPUT_GWAS_FILE="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    -v|--variants-metadata)
-      VARIANTS_METADATA_FILE="$2"
       shift # past argument
       shift # past value
       ;;
@@ -39,26 +35,60 @@ done
 
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
+#
+# check arguments
+#
+if [ -z "${INPUT_GWAS_FILE}" ]; then
+    echo "Error, --input-gwas-file <value> not provided"
+    exit 1
+fi
 
-SOFTWARE_DIR="/home/miltondp/projects/labs/greenelab/phenoplier/nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/_software/summary-gwas-imputation"
-#DATA_DIR="/home/miltondp/projects/labs/greenelab/phenoplier/nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/_data"
-#GWAS_DIR="/home/miltondp/projects/labs/greenelab/phenoplier/base/data/1000g/genotypes/gwas"
-#A1000G_REFERENCE_DATA="/home/miltondp/projects/labs/greenelab/phenoplier/base/data/phenomexcan/ld_blocks/reference_panel_1000G"
+if [ -z "${OUTPUT_DIR}" ]; then
+    echo "Error, --output-dir <value> not provided"
+    exit 1
+fi
 
-mkdir -p ${OUTPUT_DIR}
-
+# liftover argument (optional)
 LIFTOVER_ARG=""
 if [ ! -z "${LIFTOVER_CHAIN_FILE}" ]; then
     LIFTOVER_ARG="-liftover ${LIFTOVER_CHAIN_FILE}"
 fi
 
-OUTPUT_FILENAME=${INPUT_GWAS_FILE%.*}
-echo $OUTPUT_FILENAME
+#
+# Global PhenoPLIER environmental variables
+#
 
-python3 ${SOFTWARE_DIR}/src/gwas_parsing.py \
+# make sure we have environment variables with configuration
+if [ -z "${PHENOPLIER_ROOT_DIR}" ] || [ -z "${PHENOPLIER_GWAS_IMPUTATION_BASE_DIR}" ]; then
+    echo "PhenoPLIER configuration was not loaded"
+    exit 1
+fi
+
+PYTHON_EXECUTABLE="${PHENOPLIER_GWAS_IMPUTATION_CONDA_ENV}/bin/python"
+if [ ! -f ${PYTHON_EXECUTABLE} ]; then
+    echo "The python executable does not exist: ${PYTHON_EXECUTABLE}"
+    exit 1
+fi
+
+A1000G_VARIANTS_METADATA_FILE="${PHENOPLIER_PHENOMEXCAN_LD_BLOCKS_1000G_GENOTYPE_DIR}/variant_metadata.txt.gz"
+if [ ! -f ${A1000G_VARIANTS_METADATA_FILE} ]; then
+    echo "The 1000 Genomes variants metadata file does not exist: ${A1000G_VARIANTS_METADATA_FILE}"
+    exit 1
+fi
+
+
+# Create output directory
+mkdir -p ${OUTPUT_DIR}
+
+INPUT_GWAS_FILENAME=$(basename ${INPUT_GWAS_FILE})
+OUTPUT_FILENAME=${INPUT_GWAS_FILENAME%.*}
+
+${PYTHON_EXECUTABLE} ${PHENOPLIER_GWAS_IMPUTATION_BASE_DIR}/src/gwas_parsing.py \
     -gwas_file ${INPUT_GWAS_FILE} \
-    -snp_reference_metadata ${VARIANTS_METADATA_FILE} METADATA \
+    -separator " " \
+    -snp_reference_metadata ${A1000G_VARIANTS_METADATA_FILE} METADATA \
     --chromosome_format \
+    -output_column_map "#CHROM" chromosome \
     -output_column_map ID variant_id \
     -output_column_map A1 effect_allele \
     -output_column_map REF non_effect_allele \
@@ -66,9 +96,8 @@ python3 ${SOFTWARE_DIR}/src/gwas_parsing.py \
     -output_column_map BETA effect_size \
     -output_column_map SE standard_error \
     -output_column_map P pvalue \
-    -output_column_map "#CHROM" chromosome \
     -output_column_map POS position \
-    -output_column_map OBS_CT sample_size \
+    --insert_value OBS_CT sample_size \
     -output_order variant_id panel_variant_id chromosome position effect_allele non_effect_allele frequency pvalue zscore effect_size standard_error sample_size n_cases \
     ${LIFTOVER_ARG} -output ${OUTPUT_DIR}/${OUTPUT_FILENAME}-harmonized.txt
 
