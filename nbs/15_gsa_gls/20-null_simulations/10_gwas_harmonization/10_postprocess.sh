d935e7abd366ba59ae3c3b1d0ee8912260eab865
Milton Pividori <miltondp@gmail.com>
2022-05-05 11:18:11 -0400

    gls/null: add postprocessing scripts

--- nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/10_postprocess.sh
+++ nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/10_postprocess.sh
@@ -1,27 +1,106 @@
 #!/bin/bash
 
-# Global parameters
-SOFTWARE_DIR="/home/miltondp/projects/labs/greenelab/phenoplier/nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/_software/summary-gwas-imputation"
-DATA_DIR="/home/miltondp/projects/labs/greenelab/phenoplier/nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/_data"
-GWAS_DIR="/home/miltondp/projects/labs/greenelab/phenoplier/base/data/1000g/genotypes/gwas"
-A1000G_REFERENCE_DATA="/home/miltondp/projects/labs/greenelab/phenoplier/base/data/phenomexcan/ld_blocks/reference_panel_1000G"
+# Runs the imputation step.
 
-INPUT_GWAS_FILE="random_pheno0.glm.linear"
+POSITIONAL_ARGS=()
 
-OUTPUT_HARMONIZED_DIR="outputs/harmonized"
-INPUT_HARMONIZED_FILE="${INPUT_GWAS_FILE}.harmo.txt"
-INPUT_HARMONIZED_FILE_PREFIX=${INPUT_HARMONIZED_FILE%.txt}
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -i|--input-gwas-file)
+      INPUT_GWAS_FILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -f|--imputed-gwas-folder)
+      IMPUTED_GWAS_FOLDER="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -n|--phenotype-name)
+      PHENOTYPE_NAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -o|--output-dir)
+      OUTPUT_DIR="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
 
-OUTPUT_IMPUTED_DIR="outputs/imputed"
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
-OUTPUT_DIR="outputs/final"
+#
+# check arguments
+#
+if [ -z "${INPUT_GWAS_FILE}" ]; then
+    echo "Error, --input-gwas-file <value> not provided"
+    exit 1
+fi
+
+if [ -z "${IMPUTED_GWAS_FOLDER}" ]; then
+    echo "Error, --imputed-gwas-folder <value> not provided"
+    exit 1
+fi
+
+if [ -z "${PHENOTYPE_NAME}" ]; then
+    echo "Error, --phenotype-name <value> not provided"
+    exit 1
+fi
+
+if [ -z "${OUTPUT_DIR}" ]; then
+    echo "Error, --output-dir <value> not provided"
+    exit 1
+fi
+
+
+#
+# Global PhenoPLIER environmental variables
+#
+
+# make sure we have environment variables with configuration
+if [ -z "${PHENOPLIER_ROOT_DIR}" ] || [ -z "${PHENOPLIER_GWAS_IMPUTATION_BASE_DIR}" ]; then
+    echo "PhenoPLIER configuration was not loaded"
+    exit 1
+fi
+
+PYTHON_EXECUTABLE="${PHENOPLIER_GWAS_IMPUTATION_CONDA_ENV}/bin/python"
+if [ ! -f ${PYTHON_EXECUTABLE} ]; then
+    echo "The python executable does not exist: ${PYTHON_EXECUTABLE}"
+    exit 1
+fi
+
+
+# make sure the the input gwas file is for the phenotype name given
+INPUT_GWAS_FILENAME=$(basename ${INPUT_GWAS_FILE})
+if [[ ! "${INPUT_GWAS_FILENAME}" == *"${PHENOTYPE_NAME}"* ]]; then
+  echo "Phenotype name given (${PHENOTYPE_NAME}) is not present in input GWAS file name (${INPUT_GWAS_FILENAME})."
+  exit 1
+fi
+
+# make sure the number of files with imputed variants is 22 * 10 = 220
+N_FILES_IMP_VARIANTS=`ls -dq ${IMPUTED_GWAS_FOLDER}/${PHENOTYPE_NAME}* | wc -l`
+if [ "${N_FILES_IMP_VARIANTS}" -ne "220" ]; then
+  echo "Number of expected files with imputed variants (220) does not match expected value (${N_FILES_IMP_VARIANTS}). Check your phenotype name."
+  exit 1
+fi
+
+# Create output directory
 mkdir -p ${OUTPUT_DIR}
 
-
-python3 ${SOFTWARE_DIR}/src/gwas_summary_imputation_postprocess.py \
-    -gwas_file ${OUTPUT_HARMONIZED_DIR}/${INPUT_HARMONIZED_FILE} \
-    -folder ${OUTPUT_IMPUTED_DIR} \
-    -pattern ${INPUT_HARMONIZED_FILE_PREFIX}.* \
+${PYTHON_EXECUTABLE} ${PHENOPLIER_GWAS_IMPUTATION_BASE_DIR}/src/gwas_summary_imputation_postprocess.py \
+    -gwas_file ${INPUT_GWAS_FILE} \
+    -folder ${IMPUTED_GWAS_FOLDER} \
+    -pattern ${PHENOTYPE_NAME}.* \
     -parsimony 7 \
-    -output ${OUTPUT_DIR}/${INPUT_GWAS_FILE}.imputed.txt.gz
+    -output ${OUTPUT_DIR}/${PHENOTYPE_NAME}-imputed.txt.gz
 
---
+++ nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization/cluster_jobs/10_postprocessing_job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#BSUB -J random_pheno[1-100]
+#BSUB -cwd _tmp/postprocessing
+#BSUB -oo random_pheno%I.%J.out
+#BSUB -eo random_pheno%I.%J.error
+#-#BSUB -u miltondp@gmail.com
+#-#BSUB -N
+#BSUB -n 1
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -M 5GB
+#BSUB -W 0:15
+
+# make sure we use the number of CPUs specified
+export CM_N_JOBS=1
+export NUMBA_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPEN_BLAS_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+CODE_DIR=${PHENOPLIER_CODE_DIR}/nbs/15_gsa_gls/20-null_simulations/10_gwas_harmonization
+HARMONIZED_GWAS_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/harmonized_gwas"
+IMPUTED_GWAS_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/imputed_gwas"
+OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/final_imputed_gwas"
+
+GWAS_JOBINDEX=`expr $LSB_JOBINDEX - 1`
+
+bash ${CODE_DIR}/10_postprocess.sh \
+  --input-gwas-file ${HARMONIZED_GWAS_DIR}/random.pheno${GWAS_JOBINDEX}.glm-harmonized.txt \
+  --imputed-gwas-folder ${IMPUTED_GWAS_DIR} \
+  --phenotype-name random.pheno${GWAS_JOBINDEX} \
+  --output-dir ${OUTPUT_DIR}
+
