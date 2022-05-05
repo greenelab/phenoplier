14fb1c202e0b5d81e2ce3f6be4956d31fa6a477a
Milton Pividori <miltondp@gmail.com>
2022-05-05 17:13:48 -0400

    gls/null: adjust resources for S-PrediXcan scripts

--- nbs/15_gsa_gls/20-null_simulations/15_spredixcan/cluster_jobs/01_spredixcan_job-template.sh
+++ nbs/15_gsa_gls/20-null_simulations/15_spredixcan/cluster_jobs/01_spredixcan_job-template.sh
@@ -6,9 +6,9 @@
 # -#BSUB -u miltondp@gmail.com
 # -#BSUB -N
 # BSUB -n 1
-# BSUB -R "rusage[mem=10GB]"
-# BSUB -M 10GB
-# BSUB -W 0:30
+# BSUB -R "rusage[mem=5GB]"
+# BSUB -M 5GB
+# BSUB -W 0:15
 
 # IMPORTANT: this is not a ready-for-submission script, it's a template.
 #   see README.md to know how to generate the actual job scripts.
