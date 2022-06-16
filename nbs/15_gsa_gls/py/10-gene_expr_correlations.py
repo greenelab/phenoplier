5418a2813a186ea62a508d3dd88e95b4249c7454
Milton Pividori <miltondp@gmail.com>
2022-06-16 17:07:10 -0400

    gls/gene_corrs: improve gene correlations notebook

--- nbs/15_gsa_gls/py/10-gene_expr_correlations.py
+++ nbs/15_gsa_gls/py/10-gene_expr_correlations.py
@@ -34,8 +34,9 @@
 # %autoreload 2
 
 # %% tags=[]
-from random import sample
+from random import sample, seed
 import warnings
+from pathlib import Path
 
 import numpy as np
 from scipy.spatial.distance import squareform
@@ -165,18 +166,6 @@ genes_info.shape
 genes_info.head()
 
 # %% [markdown] tags=[]
-# ## Get tissues names
-
-# %% tags=[]
-tissues = conf.PHENOMEXCAN["PREDICTION_MODELS"][f"{EQTL_MODEL}_TISSUES"].split(" ")
-
-# %% tags=[]
-tissues[:5]
-
-# %% tags=[]
-assert len(tissues) == 49
-
-# %% [markdown] tags=[]
 # # Test
 
 # %%
@@ -184,6 +173,7 @@ chromosome_genes_info = genes_info[genes_info["chr"] == chromosome]
 display(chromosome_genes_info)
 
 # %%
+seed(0)
 _gene_list_ids = sample(chromosome_genes_info["id"].tolist(), 5)
 selected_chromosome_genes_obj = [Gene(ensembl_id=g_id) for g_id in _gene_list_ids]
 display(len(selected_chromosome_genes_obj))
@@ -205,6 +195,19 @@ for gene_idx1 in range(0, len(selected_chromosome_genes_obj) - 1):
 # # Compute correlation per chromosome
 
 # %%
+output_dir = OUTPUT_DIR_BASE / "by_chr"
+output_dir.mkdir(exist_ok=True, parents=True)
+output_file = output_dir / f"gene_corrs-chr{chromosome}.pkl"
+display(output_file)
+
+# %%
+if output_file.exists():
+    warning.warn("Output file exists, skipping")
+    import sys
+
+    sys.exit(1)
+
+# %%
 warnings.filterwarnings("error")
 
 # %%
@@ -218,107 +221,93 @@ assert chromosome in all_chrs
 # run only on the chromosome specified
 all_chrs = [chromosome]
 genes_chr = genes_info[genes_info["chr"] == chromosome]
-print(f"Number of genes in chromosome: {genes_chr.shape[0]}", flush=True)
 
 # For testing purposes
 # genes_chr = genes_chr.sample(n=20)
 
+print(f"Number of genes in chromosome: {genes_chr.shape[0]}", flush=True)
+
 # sort genes by starting position to make visualizations better later
 genes_chr = genes_chr.sort_values("start_position")
 
+gene_chr_objs = [Gene(ensembl_id=gene_id) for gene_id in genes_chr["id"]]
+gene_chr_ids = [g.ensembl_id for g in gene_chr_objs]
 
-for chr_num in all_chrs:
-    print(f"Chromosome {chr_num}", flush=True)
+n = len(gene_chr_objs)
+n_comb = int(n * (n - 1) / 2.0)
+print(f"Number of gene combinations: {n_comb}", flush=True)
 
-    # check if results exist
-    output_dir = OUTPUT_DIR_BASE / "by_chr"
-    output_file = output_dir / f"gene_corrs-chr{chr_num}.pkl"
+gene_corrs = []
 
-    if output_file.exists():
-        _tmp_data = pd.read_pickle(output_file)
+pbar = tqdm(ncols=100, total=n_comb)
+i = 0
+for gene_idx1 in range(0, len(gene_chr_objs) - 1):
+    gene_obj1 = gene_chr_objs[gene_idx1]
 
-        if _tmp_data.shape[0] > 0:
-            print("Already run, stopping.")
-            continue
+    for gene_idx2 in range(gene_idx1 + 1, len(gene_chr_objs)):
+        gene_obj2 = gene_chr_objs[gene_idx2]
 
-    gene_chr_objs = [Gene(ensembl_id=gene_id) for gene_id in genes_chr["id"]]
-    gene_chr_ids = [g.ensembl_id for g in gene_chr_objs]
+        pbar.set_description(f"{gene_obj1.ensembl_id} / {gene_obj2.ensembl_id}")
 
-    n = len(gene_chr_objs)
-    n_comb = int(n * (n - 1) / 2.0)
-    print(f"Number of gene combinations: {n_comb}", flush=True)
-
-    gene_corrs = []
-
-    pbar = tqdm(ncols=100, total=n_comb)
-    i = 0
-    for gene_idx1 in range(0, len(gene_chr_objs) - 1):
-        gene_obj1 = gene_chr_objs[gene_idx1]
-
-        for gene_idx2 in range(gene_idx1 + 1, len(gene_chr_objs)):
-            gene_obj2 = gene_chr_objs[gene_idx2]
-
-            pbar.set_description(f"{gene_obj1.ensembl_id} / {gene_obj2.ensembl_id}")
-
-            try:
-                # FIXME: compute the correlation of the sum of squares of the model using all
-                # the available tissues; this could be problematic because for some results
-                # (computed on a specific phenotype/GWAS) we might not have all the tissues
-                # available, which can certainly bias the correlation estimation.
-                # Since this depends on the GWAS on a specific phenotype, we should ideally have
-                # one correlation matrix per GWAS. I should look at how we can improve this.
-                gene_corrs.append(
-                    gene_obj1.get_ssm_correlation(
-                        other_gene=gene_obj2,
-                        condition_number=SMULTIXCAN_CONDITION_NUMBER,
-                        reference_panel=REFERENCE_PANEL,
-                        model_type=EQTL_MODEL,
-                    )
+        try:
+            # FIXME: compute the correlation of the sum of squares of the model using all
+            # the available tissues; this could be problematic because for some results
+            # (computed on a specific phenotype/GWAS) we might not have all the tissues
+            # available, which can certainly bias the correlation estimation.
+            # Since this depends on the GWAS on a specific phenotype, we should ideally have
+            # one correlation matrix per GWAS. I should look at how we can improve this.
+            gene_corrs.append(
+                gene_obj1.get_ssm_correlation(
+                    other_gene=gene_obj2,
+                    condition_number=SMULTIXCAN_CONDITION_NUMBER,
+                    reference_panel=REFERENCE_PANEL,
+                    model_type=EQTL_MODEL,
                 )
-            except Warning:
-                print(
-                    f"RuntimeWarning for genes {gene_obj1.ensembl_id} and {gene_obj2.ensembl_id}",
-                    flush=True,
-                )
-                import traceback
+            )
+        except Warning:
+            print(
+                f"RuntimeWarning for genes {gene_obj1.ensembl_id} and {gene_obj2.ensembl_id}",
+                flush=True,
+            )
+            import traceback
 
-                print(traceback.format_exc(), flush=True)
-            except Exception as e:
-                print(
-                    f"Exception for genes {gene_obj1.ensembl_id} and {gene_obj2.ensembl_id}",
-                    flush=True,
-                )
-                import traceback
+            print(traceback.format_exc(), flush=True)
+        except Exception as e:
+            print(
+                f"Exception for genes {gene_obj1.ensembl_id} and {gene_obj2.ensembl_id}",
+                flush=True,
+            )
+            import traceback
 
-                print(traceback.format_exc(), flush=True)
-                gene_corrs.append(np.nan)
+            print(traceback.format_exc(), flush=True)
+            gene_corrs.append(np.nan)
 
-            pbar.update(1)
+        pbar.update(1)
 
-    pbar.close()
+pbar.close()
 
-    # testing
-    gene_corrs_flat = pd.Series(gene_corrs)
-    print(f"Min/max values: {gene_corrs_flat.min()} / {gene_corrs_flat.max()}")
-    assert gene_corrs_flat.min() >= -1.001
-    assert gene_corrs_flat.max() <= 1.001
+# testing
+gene_corrs_flat = pd.Series(gene_corrs)
+print(f"Min/max values: {gene_corrs_flat.min()} / {gene_corrs_flat.max()}")
+assert gene_corrs_flat.min() >= -1.001
+assert gene_corrs_flat.max() <= 1.001
 
-    # save
-    # FIXME: consider saving only the condenced matrix here. See here for
-    # more details: https://github.com/greenelab/phenoplier/pull/38#discussion_r634600813
-    gene_corrs_data = squareform(np.array(gene_corrs, dtype=np.float64))
-    np.fill_diagonal(gene_corrs_data, 1.0)
+# save
+# FIXME: consider saving only the condenced matrix here. See here for
+# more details: https://github.com/greenelab/phenoplier/pull/38#discussion_r634600813
+gene_corrs_data = squareform(np.array(gene_corrs, dtype=np.float64))
+np.fill_diagonal(gene_corrs_data, 1.0)
 
-    gene_corrs_df = pd.DataFrame(
-        data=gene_corrs_data,
-        index=gene_chr_ids,
-        columns=gene_chr_ids,
-    )
+gene_corrs_df = pd.DataFrame(
+    data=gene_corrs_data,
+    index=gene_chr_ids,
+    columns=gene_chr_ids,
+)
 
-    output_dir.mkdir(exist_ok=True, parents=True)
-    display(output_file)
+output_dir.mkdir(exist_ok=True, parents=True)
+display(output_file)
 
-    gene_corrs_df.to_pickle(output_file)
+gene_corrs_df.to_pickle(output_file)
 
 # %% [markdown]
 # # Testing
