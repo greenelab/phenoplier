bf9f2effe99f11847f3859b99bc17b45b41558e7
Milton Pividori <miltondp@gmail.com>
2022-07-11 01:24:50 -0400

    scripts/run_docker_dev.sh: fixes

--- scripts/run_docker_dev.sh
+++ scripts/run_docker_dev.sh
@@ -29,14 +29,10 @@ POSITIONAL_ARGS=()
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -d|--docker-args)
+    --docker-args)
       DOCKER_ARGS="$2"
       shift # past argument
       shift # past value
-      ;;
-    -*|--*)
-      echo "Unknown option $1"
-      exit 1
       ;;
     *)
       POSITIONAL_ARGS+=("$1") # save positional arg
