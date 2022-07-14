36935e2c7d949ef6a961a8d4f15bf5a70f53679b
Milton Pividori <miltondp@gmail.com>
2022-07-13 21:39:38 -0400

    gls/nulls/gls: add/rename scripts to run on all random phenotypes

--- nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier/cluster_jobs/10_gls_phenoplier-full_corr-template.sh
+++ nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier/cluster_jobs/10_gls_phenoplier-full_corr-template.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# BSUB -J random_pheno${pheno_id}-${batch_id}
-# BSUB -cwd _tmp/gls_phenoplier_lv45
+# BSUB -J random_pheno${pheno_id}
+# BSUB -cwd _tmp/gls_phenoplier
 # BSUB -oo random_pheno${pheno_id}.%J.out
 # BSUB -eo random_pheno${pheno_id}.%J.error
 # -#BSUB -u miltondp@gmail.com
 # -#BSUB -N
-# BSUB -n 2
+# BSUB -n 1
 # BSUB -R "rusage[mem=3GB]"
 # BSUB -M 3GB
-# BSUB -W 1:00
+# BSUB -W 0:30
 
 # IMPORTANT: this is not a ready-for-submission script, it's a template.
 #   see README.md to know how to generate the actual job scripts.
@@ -25,15 +25,14 @@ export OMP_NUM_THREADS=${n_jobs}
 CODE_DIR=${PHENOPLIER_CODE_DIR}/nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier
 INPUT_SMULTIXCAN_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/twas/smultixcan"
 
-# GTEx v8 / MASHR / mean gene expression across tissues
+# 1000G / MASHR
 GENE_CORR_FILE="${PHENOPLIER_PHENOMEXCAN_LD_BLOCKS_GENE_CORRS_DIR}/1000g/mashr/multiplier_genes-gene_correlations-gene_symbols.pkl"
-OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/phenoplier/gls-1000g_mashr-test_lv45"
+OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/phenoplier/gls-1000g_mashr-full_corr"
 
 mkdir -p ${OUTPUT_DIR}
 
 bash ${CODE_DIR}/01_gls_phenoplier.sh \
   --input-file ${INPUT_SMULTIXCAN_DIR}/random.pheno${pheno_id}-gtex_v8-mashr-smultixcan.txt \
   --gene-corr-file ${GENE_CORR_FILE} \
-  --lv-list LV45 \
-  --output-file ${OUTPUT_DIR}/random.pheno${pheno_id}-test_lv45-gls_phenoplier.tsv.gz
+  --output-file ${OUTPUT_DIR}/random.pheno${pheno_id}-gls_phenoplier.tsv.gz
 
