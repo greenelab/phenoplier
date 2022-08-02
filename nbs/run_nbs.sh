14e2a684eadb1ddc93fe4614f60d89e7e27bf483
Milton Pividori <miltondp@gmail.com>
2022-08-02 16:06:22 -0400

    nbs/run_nbs.sh: remove dependency on git

--- nbs/run_nbs.sh
+++ nbs/run_nbs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-GIT_ROOT_DIR=$(git rev-parse --show-toplevel)
+GIT_ROOT_DIR=${PHENOPLIER_CODE_DIR}
+if [ ! -d "$GIT_ROOT_DIR" ]; then
+  echo "$GIT_ROOT_DIR does not exist. Make sure PhenoPLIER configuration is loaded."
+  exit 1
+fi
 
 # This script runs a Jupyter notebook (.ipynb) from the command line using
 # papermill.
