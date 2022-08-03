b98083751b935bcd799ab3e6bc00b5c94d67a4e9
Milton Pividori <miltondp@gmail.com>
2022-08-03 14:24:21 -0400

    gls/nulls: update and reorganize scripts to run GLS on random phenotypes

--- nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier/cluster_jobs/gtex_v8/no_covars/10_gls_phenoplier-full_corr-template.sh
+++ nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier/cluster_jobs/gtex_v8/no_covars/10_gls_phenoplier-full_corr-template.sh
@@ -22,22 +22,24 @@ export OPEN_BLAS_NUM_THREADS=${n_jobs}
 export NUMEXPR_NUM_THREADS=${n_jobs}
 export OMP_NUM_THREADS=${n_jobs}
 
+# Settings
+COHORT_NAME="1000g_eur"
+REFERENCE_PANEL="gtex_v8"
+USING_COVARS="no_covars"
+
+# Paths
 CODE_DIR=${PHENOPLIER_CODE_DIR}/nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier
 INPUT_SMULTIXCAN_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/twas/smultixcan"
 
-# 1000G / MASHR
-COHORT_NAME="1000g_eur"
-GENE_CORR_FILE="${PHENOPLIER_RESULTS_GLS}/gene_corrs/cohorts/1000g_eur/1000g/mashr/all_genes/gene_corrs-symbols.per_lv/"
-#GENE_CORR_FILE="${PHENOPLIER_RESULTS_GLS}/gene_corrs/cohorts/1000g_eur/1000g/mashr/within_distance/gene_corrs-symbols.per_lv/"
-OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/phenoplier/gls-1000g_mashr-sub_corr"
-
+OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/phenoplier/${COHORT_NAME}/${USING_COVARS}/gls-${REFERENCE_PANEL}_mashr-full_corr"
 mkdir -p ${OUTPUT_DIR}
+
+# Gene correlation matrix
+GENE_CORR_FILE="${PHENOPLIER_RESULTS_GLS}/gene_corrs/cohorts/${COHORT_NAME}/${REFERENCE_PANEL}/mashr/gene_corrs-symbols.pkl"
 
 bash ${CODE_DIR}/01_gls_phenoplier.sh \
   --input-file ${INPUT_SMULTIXCAN_DIR}/random.pheno${pheno_id}-gtex_v8-mashr-smultixcan.txt \
   --gene-corr-file ${GENE_CORR_FILE} \
-  --covars "gene_n_snps_used_density gene_n_snps_used_density_log" \
   --cohort-name ${COHORT_NAME} \
-  --debug-use-sub-gene-corr 1 \
   --output-file ${OUTPUT_DIR}/random.pheno${pheno_id}-gls_phenoplier.tsv.gz
 
