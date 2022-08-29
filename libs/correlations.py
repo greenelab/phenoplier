96eefbac1c2adbbc6c682dc925677ea04fb20536
Milton Pividori <miltondp@gmail.com>
2022-08-29 09:59:40 -0400

    correlation.py: improve code and add unit tests

--- libs/correlations.py
+++ libs/correlations.py
@@ -1,9 +1,17 @@
+"""
+It contains functions to support the creation of gene correlation matrices.
+"""
+
 import numpy as np
 import pandas as pd
+from statsmodels.stats.correlation_tools import corr_nearest
 from IPython.display import display
 
 
 def check_pos_def(matrix: pd.DataFrame):
+    """
+    Checks that a correlation matrix is positive definite.
+    """
     # show nonpositive eigenvalues
     eigs = np.linalg.eigvals(matrix.to_numpy())
     neg_eigs = eigs[eigs <= 0]
@@ -13,48 +21,39 @@ def check_pos_def(matrix: pd.DataFrame):
     # check what statsmodels.GLS expects
     try:
         # decomposition used by statsmodels.GLS
-        cholsigmainv = np.linalg.cholesky(np.linalg.inv(matrix.to_numpy())).T
+        np.linalg.cholesky(np.linalg.inv(matrix.to_numpy())).T
         print("Works! (statsmodels.GLS)")
     except Exception as e:
         print(f"Cholesky decomposition failed (statsmodels.GLS): {str(e)}")
 
     # check
     CHOL_DECOMPOSITION_WORKED = None
-    chol_mat = None
-    chol_inv = None
 
     try:
-        chol_mat = np.linalg.cholesky(matrix.to_numpy())
-        chol_inv = np.linalg.inv(chol_mat)
+        np.linalg.inv(np.linalg.cholesky(matrix.to_numpy()))
         print("Works!")
         CHOL_DECOMPOSITION_WORKED = True
     except Exception as e:
         print(f"Cholesky decomposition failed: {str(e)}")
         CHOL_DECOMPOSITION_WORKED = False
 
-    return CHOL_DECOMPOSITION_WORKED, chol_mat, chol_inv
+    return CHOL_DECOMPOSITION_WORKED
 
 
-def compare_matrices(matrix1, matrix2, check_max=1e-10):
-    _diff = (matrix1 - matrix2).unstack()
-    display(_diff.describe())
-    display(_diff.sort_values())
-
-    if check_max is not None:
-        assert _diff.abs().max() < check_max
-
-
-def correct_corr_mat(corr_mat, threshold):
+def correct_corr_mat(corr_mat: pd.DataFrame, threshold):
     """
-    It fixes a correlation matrix using its eigenvalues. The approach uses this function:
+    If necessary, it fixes a correlation matrix using its eigenvalues. The approach uses this function:
 
         https://www.statsmodels.org/dev/generated/statsmodels.stats.correlation_tools.corr_nearest.html
 
     However, it could be slow in some cases. An alternative implementation is commented out below (read
     details below).
+
+    It always returns a numpy array.
     """
 
-    from statsmodels.stats.correlation_tools import corr_nearest
+    if check_pos_def(corr_mat):
+        return corr_mat.to_numpy()
 
     return corr_nearest(corr_mat, threshold=threshold, n_fact=100)
 
@@ -70,6 +69,10 @@ def correct_corr_mat(corr_mat, threshold):
 
 
 def adjust_non_pos_def(matrix, threshold=1e-15):
+    """
+    It is the same as correct_corr_mat, but it returns a dataframe with the same
+    row and columns as the original matrix.
+    """
     matrix_fixed = correct_corr_mat(matrix, threshold)
 
     return pd.DataFrame(
@@ -77,3 +80,19 @@ def adjust_non_pos_def(matrix, threshold=1e-15):
         index=matrix.index.copy(),
         columns=matrix.columns.copy(),
     )
+
+
+def compare_matrices(matrix1, matrix2, check_max=1e-10):
+    """
+    Compares two matrices of the same dimension and returns the differences.
+    It is used to compare how different is the original correlation matrix from
+    the corrected one.
+    """
+    _diff = (matrix1 - matrix2).unstack()
+    display(_diff.describe())
+    display(_diff.sort_values())
+
+    if check_max is not None:
+        assert _diff.abs().max() < check_max, "Difference is larger than threshold"
+
+    return _diff
--- tests/test_correlations.py
+++ tests/test_correlations.py
@@ -1,7 +1,13 @@
 import numpy as np
 import pandas as pd
+import pytest
 
-from correlations import check_pos_def
+from correlations import (
+    check_pos_def,
+    correct_corr_mat,
+    adjust_non_pos_def,
+    compare_matrices,
+)
 
 
 def test_check_pos_def_matrix_is_not_pos_def():
@@ -10,7 +16,7 @@ def test_check_pos_def_matrix_is_not_pos_def():
     _eigvals = np.linalg.eigvals(input_matrix)
     assert _eigvals[_eigvals <= 0].shape[0] > 0
 
-    assert not check_pos_def(input_matrix)[0]
+    assert not check_pos_def(input_matrix)
 
 
 def test_check_pos_def_matrix_is_pos_def():
@@ -20,4 +26,69 @@ def test_check_pos_def_matrix_is_pos_def():
     _eigvals = np.linalg.eigvals(input_matrix)
     assert _eigvals[_eigvals <= 0].shape[0] == 0
 
-    assert check_pos_def(input_matrix)[0]
+    assert check_pos_def(input_matrix)
+
+
+def test_correct_corr_mat_no_need_to_correct():
+    rs = np.random.RandomState(0)
+    data = pd.DataFrame(rs.rand(1000, 10))
+    input_matrix = data.corr()
+    _eigvals = np.linalg.eigvals(input_matrix)
+    assert _eigvals[_eigvals <= 0].shape[0] == 0
+
+    corrected_matrix = correct_corr_mat(input_matrix, 1e-10)
+    assert np.array_equal(corrected_matrix, input_matrix.to_numpy())
+
+
+def test_correct_corr_mat_need_to_correct():
+    rs = np.random.RandomState(0)
+    data = pd.DataFrame(rs.rand(9, 10))
+    input_matrix = data.corr()
+    _eigvals = np.linalg.eigvals(input_matrix)
+    assert _eigvals[_eigvals <= 0].shape[0] > 0
+
+    corrected_matrix = correct_corr_mat(input_matrix, 1e-10)
+    assert not np.array_equal(corrected_matrix, input_matrix.to_numpy())
+    assert np.allclose(corrected_matrix, input_matrix.to_numpy())
+
+
+def test_adjust_non_pos_def_need_to_correct():
+    rs = np.random.RandomState(0)
+    data = pd.DataFrame(rs.rand(9, 10))
+    input_matrix = data.corr()
+    _eigvals = np.linalg.eigvals(input_matrix)
+    assert _eigvals[_eigvals <= 0].shape[0] > 0
+
+    corrected_matrix = adjust_non_pos_def(input_matrix)
+    assert not corrected_matrix.equals(input_matrix)
+    assert np.allclose(corrected_matrix, input_matrix.to_numpy())
+    assert corrected_matrix.index.equals(input_matrix.index)
+    assert corrected_matrix.columns.equals(input_matrix.columns)
+
+
+def test_compare_matrices_need_to_correct():
+    rs = np.random.RandomState(0)
+    data = pd.DataFrame(rs.rand(9, 10))
+    input_matrix = data.corr()
+    _eigvals = np.linalg.eigvals(input_matrix)
+    assert _eigvals[_eigvals <= 0].shape[0] > 0
+    corrected_matrix = adjust_non_pos_def(input_matrix)
+
+    diff = compare_matrices(input_matrix, corrected_matrix)
+    assert diff is not None
+    assert diff.min() < 0
+    assert diff.max() > 0
+
+
+def test_compare_matrices_need_to_correct_diff_too_big():
+    rs = np.random.RandomState(0)
+    data = pd.DataFrame(rs.rand(9, 10))
+    input_matrix = data.corr()
+    _eigvals = np.linalg.eigvals(input_matrix)
+    assert _eigvals[_eigvals <= 0].shape[0] > 0
+    corrected_matrix = adjust_non_pos_def(input_matrix)
+
+    with pytest.raises(Exception) as e_info:
+        compare_matrices(input_matrix, corrected_matrix, check_max=1e-300)
+
+    assert "Difference is larger than threshold" in str(e_info.value)
