887b31d0efa5763f4308a94b1c603018824ee8f9
Milton Pividori <miltondp@gmail.com>
2022-08-29 22:38:28 -0400

    gls/nulls: reorganize cluster jobs for GLS runs

--- nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier/cluster_jobs/covars/10_gls_phenoplier-sub_corr-template.sh
+++ nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier/cluster_jobs/covars/10_gls_phenoplier-sub_corr-template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # BSUB -J random_pheno${pheno_id}
-# BSUB -cwd _tmp/gls_phenoplier_ols
+# BSUB -cwd _tmp/gls_phenoplier
 # BSUB -oo random_pheno${pheno_id}.%J.out
 # BSUB -eo random_pheno${pheno_id}.%J.error
 # -#BSUB -u miltondp@gmail.com
@@ -9,6 +9,7 @@
 # BSUB -R "rusage[mem=3GB]"
 # BSUB -M 3GB
 # BSUB -W 0:30
+# BSUB -R 'select[hname!=lambda25]'
 
 # IMPORTANT: this is not a ready-for-submission script, it's a template.
 #   see README.md to know how to generate the actual job scripts.
@@ -24,19 +25,30 @@ export OMP_NUM_THREADS=${n_jobs}
 
 # Settings
 COHORT_NAME="1000g_eur"
-# REFERENCE_PANEL="1000g"
+REFERENCE_PANEL="gtex_v8"
 USING_COVARS="covars"
 
 # Paths
 CODE_DIR=${PHENOPLIER_CODE_DIR}/nbs/15_gsa_gls/20-null_simulations/20_gls_phenoplier
 INPUT_SMULTIXCAN_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/twas/smultixcan"
 
-OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/phenoplier/${COHORT_NAME}/${USING_COVARS}/gls-debug_use_ols"
+OUTPUT_DIR="${PHENOPLIER_RESULTS_GLS_NULL_SIMS}/phenoplier/${COHORT_NAME}/${USING_COVARS}/gls-${REFERENCE_PANEL}_mashr-sub_corr"
 mkdir -p ${OUTPUT_DIR}
+
+# Gene correlation matrix
+GENE_CORR_FILE="${PHENOPLIER_RESULTS_GLS}/gene_corrs/cohorts/${COHORT_NAME}/${REFERENCE_PANEL}/mashr/gene_corrs-symbols.per_lv/"
+#GENE_CORR_FILE="${PHENOPLIER_RESULTS_GLS}/gene_corrs/cohorts/${COHORT_NAME}/${REFERENCE_PANEL}/mashr/gene_corrs-symbols-within_distance_10mb.per_lv/"
+#GENE_CORR_FILE="${PHENOPLIER_RESULTS_GLS}/gene_corrs/cohorts/${COHORT_NAME}/${REFERENCE_PANEL}/mashr/gene_corrs-symbols-within_distance_5mb.per_lv/"
+#GENE_CORR_FILE="${PHENOPLIER_RESULTS_GLS}/gene_corrs/cohorts/${COHORT_NAME}/${REFERENCE_PANEL}/mashr/gene_corrs-symbols-within_distance_2mb.per_lv/"
+
+# --covars "all"
+# --covars "gene_size gene_size_log gene_density gene_density_log"
+# --covars "gene_n_snps_used gene_n_snps_used_log gene_n_snps_used_density gene_n_snps_used_density_log"
 
 bash ${CODE_DIR}/01_gls_phenoplier.sh \
   --input-file ${INPUT_SMULTIXCAN_DIR}/random.pheno${pheno_id}-gtex_v8-mashr-smultixcan.txt \
-  --debug-use-ols 1 \
+  --gene-corr-file ${GENE_CORR_FILE} \
   --covars "gene_size gene_size_log gene_density gene_density_log" \
-  --cohort-name ${COHORT_NAME} \
+  --debug-use-sub-gene-corr 1 \
   --output-file ${OUTPUT_DIR}/random.pheno${pheno_id}-gls_phenoplier.tsv.gz
+
