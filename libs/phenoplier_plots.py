cdcc2d5a2f3863f903b4c466e537aeb147b1d5dc
Milton Pividori <miltondp@gmail.com>
2022-09-01 15:52:10 -0400

    gls/nulls: add analyses of null results

--- libs/phenoplier_plots.py
+++ libs/phenoplier_plots.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.graphics.gofplots import qqplot_2samples
@@ -23,17 +22,14 @@ def qqplot(observed_pvalues, expected_pvalues=None, font_scale=1.8, lines_marker
         uniform_data = np.array([i / (n + 1) for i in range(1, n + 1)])
         expected_data = -np.log10(uniform_data)
 
-    with sns.plotting_context("paper", font_scale=font_scale), mpl.rc_context(
-        {"lines.markersize": lines_markersize}
-    ):
-        fig, ax = plt.subplots(figsize=(8, 8), facecolor="white")
-        fig = qqplot_2samples(expected_data, observed_data, line="45", ax=ax)
+    fig, ax = plt.subplots(figsize=(8, 8), facecolor="white")
+    fig = qqplot_2samples(expected_data, observed_data, line="45", ax=ax)
 
-        ax.set_xlim(expected_data.min() - 0.05, expected_data.max() + 0.05)
+    ax.set_xlim(expected_data.min() - 0.05, expected_data.max() + 0.05)
 
-        ax.set_xlabel("$-\log_{10}$" + "(expected pvalue)")
-        ax.set_ylabel("$-\log_{10}$" + "(observed pvalue)")
+    ax.set_xlabel("$-\log_{10}$" + "(expected pvalue)")
+    ax.set_ylabel("$-\log_{10}$" + "(observed pvalue)")
 
-        ax.set_title(f"Number of data points:  {n}")
+    ax.set_title(f"Number of data points:  {n}")
 
     return fig, ax
