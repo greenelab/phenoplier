76b04b9c5ed96f97d38f69caf86baf1b66597f17
Milton Pividori <miltondp@gmail.com>
2022-09-02 10:08:13 -0400

    gls: export results to data directory

--- nbs/15_gsa_gls/py/99-gls-export.py
+++ nbs/15_gsa_gls/py/99-gls-export.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.7.1
+#       jupytext_version: 1.13.8
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -24,14 +24,18 @@
 # # Modules
 
 # %% tags=[]
+from pathlib import Path
+import shutil
+
 import pandas as pd
 import rpy2.robjects as ro
 from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 
 import conf
+from utils import remove_all_file_extensions
 
-# %%
+# %% tags=[]
 readRDS = ro.r["readRDS"]
 
 # %% tags=[]
@@ -41,19 +45,18 @@ saveRDS = ro.r["saveRDS"]
 # # Settings
 
 # %% tags=[]
-OUTPUT_DIR = conf.RESULTS["GLS"]
+OUTPUT_DIR = Path(conf.__file__).parent.parent / "data" / "gls"
 display(OUTPUT_DIR)
-
-assert OUTPUT_DIR.exists()
+OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 
 # %% [markdown] tags=[]
 # # Get results files
 
-# %%
-input_filepath = OUTPUT_DIR / "gls_phenotypes-combined-phenomexcan.pkl"
+# %% tags=[]
+input_filepath = conf.RESULTS["GLS"] / "gls-summary-phenomexcan.pkl.gz"
 display(input_filepath)
 
-# %%
+# %% tags=[]
 data = pd.read_pickle(input_filepath)
 
 # %% tags=[]
@@ -62,45 +65,52 @@ data.shape
 # %% tags=[]
 data.head()
 
-# %%
+# %% tags=[]
 assert not data.isna().any().any()
 
 # %% [markdown] tags=[]
 # # Save
 
 # %% [markdown] tags=[]
+# ## Pickle format
+
+# %% tags=[]
+shutil.copy(input_filepath, OUTPUT_DIR)
+
+# %% [markdown] tags=[]
 # ## RDS format
 
 # %% tags=[]
-output_file = input_filepath.with_suffix(".rds")
+output_file = remove_all_file_extensions(input_filepath).with_suffix(".rds")
+output_file = OUTPUT_DIR / output_file.name
 display(output_file)
 
-# %%
+# %% tags=[]
 with localconverter(ro.default_converter + pandas2ri.converter):
     data_r = ro.conversion.py2rpy(data)
 
-# %%
+# %% tags=[]
 data_r
 
 # %% tags=[]
 saveRDS(data_r, str(output_file))
 
-# %%
+# %% tags=[]
 # testing: load the rds file again
 data_r = readRDS(str(output_file))
 
-# %%
+# %% tags=[]
 with localconverter(ro.default_converter + pandas2ri.converter):
     data_again = ro.conversion.rpy2py(data_r)
     data_again.index = data_again.index.astype(int)
 
-# %%
+# %% tags=[]
 data_again.shape
 
-# %%
+# %% tags=[]
 data_again.head()
 
-# %%
+# %% tags=[]
 pd.testing.assert_frame_equal(
     data,
     data_again,
@@ -112,33 +122,33 @@ pd.testing.assert_frame_equal(
 
 # %% tags=[]
 # tsv format
-output_file = input_filepath.with_suffix(".tsv.gz")
+output_file = remove_all_file_extensions(input_filepath).with_suffix(".tsv.gz")
+output_file = OUTPUT_DIR / output_file.name
 display(output_file)
 
 # %% tags=[]
 data.to_csv(output_file, sep="\t", index=False, float_format="%.5e")
 
-# %%
+# %% tags=[]
 # testing
 data2 = data.copy()
 data2.index = list(range(0, data2.shape[0]))
 
 data_again = pd.read_csv(output_file, sep="\t")
-
 data_again.index = list(data_again.index)
-data_again["part_k"] = data_again["part_k"].astype(float)
 
-# %%
+# %% tags=[]
 data_again.shape
 
-# %%
+# %% tags=[]
 data_again.head()
 
-# %%
+# %% tags=[]
 pd.testing.assert_frame_equal(
     data2,
     data_again,
+    check_categorical=False,
     check_dtype=False,
 )
 
-# %%
+# %% tags=[]
