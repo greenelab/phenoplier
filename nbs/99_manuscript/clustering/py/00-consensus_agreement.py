83791a540b77c0e2d03bc47997e50a565f76fe66
Milton Pividori <miltondp@gmail.com>
2022-09-16 15:08:00 -0400

    clustering: add plots of of null simulations

--- nbs/99_manuscript/clustering/py/00-consensus_agreement.py
+++ nbs/99_manuscript/clustering/py/00-consensus_agreement.py
@@ -7,9 +7,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.7.1
+#       jupytext_version: 1.13.8
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -49,6 +49,12 @@ OUTPUT_FIGURES_DIR.mkdir(parents=True, exist_ok=True)
 # # Load best partitions
 
 # %%
+scenarios_results = {}
+
+# %% [markdown] tags=[]
+# ## Runs on real data
+
+# %%
 CONSENSUS_CLUSTERING_DIR = Path(
     conf.RESULTS["CLUSTERING_DIR"], "consensus_clustering"
 ).resolve()
@@ -68,24 +74,117 @@ best_partitions.shape
 # %% tags=[]
 best_partitions.head()
 
-# %% [markdown] tags=[]
-# # Threshold
+# %%
+best_partitions["selected"].value_counts()
 
-# %% tags=[]
+# %%
+# get threshold
 best_parts_stats = best_partitions["ari_median"].describe()
 display(best_parts_stats)
 
-# %% tags=[]
+# %%
 best_threshold = best_parts_stats["75%"]
 best_threshold_description = "75th percentile"
 display(best_threshold)
 
+# %%
+scenarios_results["Real data"] = (best_partitions, best_parts_stats, best_threshold)
+
+# %% [markdown] tags=[]
+# ## Runs on simulated null scenario 1
+
+# %%
+CONSENSUS_CLUSTERING_DIR = Path(
+    conf.RESULTS["CLUSTERING_DIR"], "null_sims", "consensus_clustering"
+).resolve()
+
+display(CONSENSUS_CLUSTERING_DIR)
+
+# %% tags=[]
+input_file = Path(CONSENSUS_CLUSTERING_DIR, "best_partitions_by_k.pkl").resolve()
+display(input_file)
+
+# %% tags=[]
+best_partitions = pd.read_pickle(input_file)
+
+# %% tags=[]
+best_partitions.shape
+
+# %% tags=[]
+best_partitions.head()
+
+# %%
+best_partitions["selected"].value_counts()
+
+# %%
+# get threshold
+best_parts_stats = best_partitions["ari_median"].describe()
+display(best_parts_stats)
+
+# %%
+best_threshold = best_parts_stats["75%"]
+best_threshold_description = "75th percentile"
+display(best_threshold)
+
+# %%
+scenarios_results["Null #1"] = (best_partitions, best_parts_stats, best_threshold)
+
+# %% [markdown] tags=[]
+# ## Runs on simulated null scenario 2
+
+# %%
+CONSENSUS_CLUSTERING_DIR = Path(
+    conf.RESULTS["CLUSTERING_DIR"], "null_sims_lv_shuffle", "consensus_clustering"
+).resolve()
+
+display(CONSENSUS_CLUSTERING_DIR)
+
+# %% tags=[]
+input_file = Path(CONSENSUS_CLUSTERING_DIR, "best_partitions_by_k.pkl").resolve()
+display(input_file)
+
+# %% tags=[]
+best_partitions = pd.read_pickle(input_file)
+
+# %% tags=[]
+best_partitions.shape
+
+# %% tags=[]
+best_partitions.head()
+
+# %%
+best_partitions["selected"].value_counts()
+
+# %%
+# get threshold
+best_parts_stats = best_partitions["ari_median"].describe()
+display(best_parts_stats)
+
+# %%
+best_threshold = best_parts_stats["75%"]
+best_threshold_description = "75th percentile"
+display(best_threshold)
+
+# %%
+scenarios_results["Null #2"] = (best_partitions, best_parts_stats, best_threshold)
+
 # %% [markdown] tags=[]
 # # Plot of selected best partitions
 
+# %%
+real_threshold = scenarios_results["Real data"][2]
+display(real_threshold)
+
+# %%
+all_data = []
+for k, v in scenarios_results.items():
+    all_data.append(v[0].assign(scenario=k).reset_index())
+
 # %% tags=[]
-plot_data = best_partitions.reset_index()
+plot_data = pd.concat(all_data, ignore_index=True)
+display(plot_data.shape)
 display(plot_data.head(5))
+assert plot_data.shape[0] == 3 * 59
 
 # %%
 _tmp = best_partitions.columns[
@@ -108,18 +207,22 @@ with sns.plotting_context("talk", font_scale=0.75), sns.axes_style(
 
     fig, ax = plt.subplots(figsize=(18, 6))
     ax = sns.pointplot(
-        data=plot_data, x="k", y=_measure_col, color=next(current_palette)
+        # data=plot_data, x="k", y=_measure_col, color=next(current_palette), hue="scenario",
+        data=plot_data.rename(columns={"scenario": "Scenario"}),
+        x="k",
+        y=_measure_col,
+        hue="Scenario",
     )
     ax.axhline(
-        best_threshold,
+        real_threshold,
         ls="--",
-        color=next(current_palette),
+        # color=next(current_palette),
         label=best_threshold_description,
     )
     ax.set_ylabel(f"Agreement with ensemble\n({_selected_stat} {_selected_measure})")
     ax.set_xlabel("Number of clusters ($k$)")
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-    plt.legend()
+    # plt.legend()
     plt.grid(True)
     #     plt.tight_layout()
 
