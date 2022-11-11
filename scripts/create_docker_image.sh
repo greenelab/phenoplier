8dad795e63ab712feb68918be0b0bd416d6f339e
Milton Pividori <miltondp@gmail.com>
2022-11-11 17:04:25 -0500

    Docker: simplify script to create image

--- scripts/create_docker_image.sh
+++ scripts/create_docker_image.sh
@@ -2,24 +2,23 @@
 set -e
 
 PROJECT_NAME="phenoplier"
-VERSION="1.3"
+VERSION="1.3.2"
 
 CURRENT_IMAGE_ID=$(docker images --filter=reference=miltondp/${PROJECT_NAME}:latest --format "{{.ID}}")
 
-docker build -t miltondp/${PROJECT_NAME}:${VERSION} -t miltondp/${PROJECT_NAME}:latest .
+export DOCKER_BUILDKIT=1
+docker build \
+  -t miltondp/${PROJECT_NAME}:${VERSION} \
+  -f Dockerfile \
+  --target final .
 
-read -p "'docker push' new image and retag? " -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  # push version label
-  echo "Pushing new image to miltondp/${PROJECT_NAME}:${VERSION}"
-  docker push miltondp/${PROJECT_NAME}:${VERSION}
+# command to tag as latest:
+# docker tag miltondp/${PROJECT_NAME}:${VERSION} miltondp/${PROJECT_NAME}:latest
 
-  # push latest label
-  echo "Pushing new image as latest"
-  docker push miltondp/${PROJECT_NAME}:latest
+# command to tag and keep previous version
+# docker tag ${CURRENT_IMAGE_ID} miltondp/${PROJECT_NAME}:prev
 
-  # retag previous version
-  docker tag ${CURRENT_IMAGE_ID} miltondp/${PROJECT_NAME}:prev
-fi
+# command to push new version and latest
+# docker push miltondp/${PROJECT_NAME}:${VERSION}
+# docker push miltondp/${PROJECT_NAME}:latest
 
