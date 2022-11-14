caf6cfb044074aad411ec0c3b41efa81c0b85a86
Milton Pividori <miltondp@gmail.com>
2022-11-14 17:17:26 -0500

    demo: adjust notebooks to use gls_cli.py

--- nbs/99_demo/py/01-LV_trait_association-neutrophil_counts.py
+++ nbs/99_demo/py/01-LV_trait_association-neutrophil_counts.py
@@ -18,7 +18,7 @@
 # # Description
 
 # %% [markdown]
-# This notebook will show the structure of the main data matrices in PhenoPLIER, and will guide you in analyzing gene associations for a particular trait: neutrophil counts, which is presented in the [manuscript](https://greenelab.github.io/phenoplier_manuscript/#phenoplier-an-integration-framework-based-on-gene-co-expression-patterns) in Figure 1c.
+# This notebook will show the structure of the main data matrices in PhenoPLIER, and will guide you in analyzing gene associations for a particular trait: basophill percentage, which is presented in the [manuscript](https://greenelab.github.io/phenoplier_manuscript/#phenoplier-an-integration-framework-based-on-gene-co-expression-patterns) in Figure 1c.
 
 # %% [markdown]
 # # Modules
@@ -75,70 +75,47 @@ lv_metadata.head()
 # # Load gene associations from PhenomeXcan
 
 # %% [markdown]
-# Now we load the gene association across ~4,000 traits from [PhenomeXcan](https://doi.org/10.1126/sciadv.aba2083).
-# The file we load here are the Summary-MultiXcan (or S-MultiXcan) results, essentially a p-value for each gene-trait pair.
+# Now we load gene-trait association from [PhenomeXcan](https://doi.org/10.1126/sciadv.aba2083).
+# PhenomeXcan provides TWAS results (using [Summary-MultiXcan](https://doi.org/10.1371/journal.pgen.1007889) and [Summary-PrediXcan](https://doi.org/10.1038/s41467-018-03621-1)) across ~4,000 traits.
+# If you are interested in PhenomeXcan you can also check out the [Github repo](https://github.com/hakyimlab/phenomexcan) to know how to download results.
+#
+# For this demo, we'll load a file that contains Summary-MultiXcan (or S-MultiXcan) results for basophill percentage.
+# This file contains a list of p-values for ~22k genes, where a significant p-value means that the gene's predicted expression (across different tissues) is associated with basophill percentage.
 # In the notebook I refer to these results generically as "TWAS results", meaning that we have gene-trait associations.
 # All these TWAS results were derived solely from GWAS summary stats, so you can also generate yours relatively easily by using [S-MultiXcan](https://doi.org/10.1371/journal.pgen.1007889).
 
-# %%
-phenomexcan_df = pd.read_pickle(conf.PHENOMEXCAN["SMULTIXCAN_MASHR_PVALUES_FILE"])
+# %% language="bash"
+# # download S-MultiXcan results for basophill percentage
+# wget https://uchicago.box.com/shared/static/g70nq1c6wjvado242t9yg05jrhvdykrv.gz -O /tmp/smultixcan_30220_raw_ccn30.tsv.gz
 
 # %%
-phenomexcan_df.shape
+df = pd.read_csv("/tmp/smultixcan_30220_raw_ccn30.tsv.gz", sep="\t")
 
 # %%
-phenomexcan_df.head()
+df.shape
+
+# %%
+df.head()
 
 # %% [markdown]
-# Convert gene Ensembl IDs to symbols:
-
-# %%
-phenomexcan_df = phenomexcan_df.rename(index=Gene.GENE_ID_TO_NAME_MAP)
-
-# %%
-phenomexcan_df = phenomexcan_df.loc[~phenomexcan_df.index.duplicated()]
-
-# %%
-phenomexcan_df.head()
+# # Take a look at genes associated with basophill percentage
 
 # %% [markdown]
-# Keep genes present in our matrix Z only:
+# Show the sample size for this trait.
 
 # %%
-common_genes = phenomexcan_df.index.intersection(matrix_z.index)
-display(common_genes)
-
-# %%
-phenomexcan_df = phenomexcan_df.loc[common_genes]
-
-# %%
-phenomexcan_df.shape
-
-# %%
-matrix_z = matrix_z.loc[common_genes]
-
-# %%
-matrix_z.shape
-
-# %% [markdown]
-# # Take a look at genes associated with neutrophil counts
-
-# %% [markdown]
-# Below I search the PhenomeXcan results to find traits related to "neutrophils".
-
-# %%
-phenomexcan_df.columns[phenomexcan_df.columns.str.lower().str.contains("neutrophil")]
-
-# %% [markdown]
-# For this demo, I select the the "neutrophil count" TWAS derived from the GWAS performed by [Astle et. al](https://doi.org/10.1016/j.cell.2016.10.042). Below you can see the sample size:
-
-# %%
-trait_code = "Astle_et_al_2016_Neutrophil_count"
+trait_code = "30220_raw-Basophill_percentage"
 t = Trait.get_trait(full_code=trait_code)
 display(f"{trait_code} - sample size: {t.n}")
 
+# %% [markdown]
+# Below I list the top associated genes for basophill percentage.
+
 # %%
-traits_df = phenomexcan_df[[trait_code]].dropna()
+traits_df = df[["gene_name", "pvalue"]].dropna().set_index("gene_name")
+
+# remove duplicated gene names
+traits_df = traits_df.loc[~traits_df.index.duplicated()]
 
 # %%
 traits_df.shape
@@ -150,43 +127,35 @@ traits_df.head()
 # Here I quickly show the data summary for this trait's gene associations:
 
 # %%
-traits_df.apply(lambda x: -np.log10(x)).describe()
+traits_df["pvalue"].apply(lambda x: -np.log10(x)).describe()
 
 # %% [markdown]
-# Make sure we don't have missing values or NaN.
+# # Get a set of common genes between TWAS and LVs
 
 # %%
-assert not traits_df.isna().any().any()
-assert not np.isinf(traits_df).any().any()
-
-# %% [markdown]
-# # Convert p-values to z-scores
-
-# %% [markdown]
-# This converts a p-value to a scalar > 0, where higher positive values mean stronger association, and lower values close to zero mean weaker associations. Check our manuscript for more details on this.
+common_genes = traits_df.index.intersection(matrix_z.index)
 
 # %%
-traits_zscores = pd.DataFrame(
-    data=np.abs(stats.norm.ppf(traits_df / 2)),
-    index=traits_df.index.copy(),
-    columns=traits_df.columns.copy(),
-)
+common_genes
 
 # %%
-assert not traits_zscores.isna().any().any()
-assert not np.isinf(traits_zscores).any().any()
+# keep only the genes in common
+traits_df = traits_df.loc[common_genes]
 
 # %%
-traits_zscores.head()
+traits_df.shape
 
 # %%
-traits_zscores.describe()
+matrix_z = matrix_z.loc[common_genes]
+
+# %%
+matrix_z.shape
 
 # %% [markdown]
 # # Analysis of a neutrophil-termed LV
 
 # %% [markdown]
-# Let's take as an example an LV that was previously analyzed in the MultiPLIER study, which we identify as `LV603`. This LV aligns well with pathways related to neutrophils, as you can see below.
+# Let's take as an example an LV that was previously analyzed in the MultiPLIER study, which we identify as `LV603`. This LV aligns well with pathways related to neutrophils, as you can see below. In the next notebook (`02-LV_cell_types...`) we'll see that this LV is expressed in neutrophils and other granulocytes.
 
 # %%
 lv_metadata[
@@ -201,102 +170,82 @@ lv603_top_genes = matrix_z["LV603"].sort_values(ascending=False)
 display(lv603_top_genes.head(20))
 
 # %% [markdown]
-# Are these top genes associated with our neutrophil count?
+# Are these top genes associated with our trait of interest?
 
 # %%
-traits_zscores.loc[lv603_top_genes.index].head(20)
+traits_df.loc[
+    lv603_top_genes.index
+].head(20)
 
 # %% [markdown]
 # It seems so. But what about the rest of the genes? They might be also strongly associated.
 # Let's take a random sample:
 
 # %%
-traits_zscores.sample(n=20, random_state=0)
+traits_df.sample(n=20, random_state=0)
 
 # %% [markdown]
-# They do not seem as high as those within the top genes in LV603.
-# If we compute the correlation between LV603 gene weights (`lv603_top_genes`) and gene associations for neutrophil counts (`traits_zscores`) we get this:
+# They do not seem as significant as those within the top genes in LV603.
+#
+# If we compute the correlation between LV603 gene weights (`lv603_top_genes`) and gene associations for basophill percentage (`traits_df`) we get this:
+
+# %%
+lv603_top_genes
 
 # %%
 stats.pearsonr(
-    traits_zscores.loc[lv603_top_genes.index].iloc[:, 0].to_numpy(),
+    traits_df["pvalue"].apply(lambda x: -np.log10(x)).loc[lv603_top_genes.index].to_numpy(),
     lv603_top_genes.to_numpy(),
 )
 
 # %% [markdown]
-# Although the correlation is significant (`8.16e-10`) and the slope positive (we are interested only in genes at the top of the LV), we need to account for correlated predicted expression from the TWAS models (for example, if the expression of two genes at the top of the LV is correlated that would invalidate our test).
-# We have a class implemented in Python that computes this, as shown below.
+# Although the correlation is significant (`2.94e-27`) and the slope positive (we are interested only in genes at the top of the LV), we need to account for correlated predicted expression from the TWAS models (for example, if the expression of two genes at the top of the LV is correlated that would invalidate our test).
+# We provide a class, `GLSPhenoplier` (implemented in Python) that computes this. We also provide a command-line tool, `gls_cli.py`, that performs several preprocessing steps, and below we show how to use it for our example.
 
 # %% [markdown]
-# # Association between an LV and a trait
+# # `gls_cli.py`: association between an LV and a trait
 
 # %% [markdown]
-# You can use our class `gls.GLSPhenoplier` to compute an association between an LV and a trait, in a similar way we did it in the previous cells by correlating LV603 gene weight's and neutrophil count gene associations (transformed to z-scores). However, `gls.GLSPhenoplier` takes into account correlations between the predicted expression of genes by using a generalized least squares (GLS) model.
+# The `gls_cli.py` command-line tool needs as input the S-MultiXcan TWAS results and a gene correlation matrix (which is specific to the TWAS results, see below).
 
-# %%
-from gls import GLSPhenoplier
-
-# %% [markdown]
-# We need to save our trait gene association to a pickle file before moving on. Here I generate a temporary file path and save:
-
-# %%
-with tempfile.NamedTemporaryFile(suffix=".pkl") as f:
-    output_filepath = f.name
-    display(output_filepath)
-
-traits_zscores.to_pickle(output_filepath)
-
-# %% [markdown]
-# Now select an LV id and the trait id, as shown below:
-
-# %%
-lv_code = "LV603"
-
-phenotype_code = traits_zscores.columns[0]
-display(phenotype_code)
-
-# %% [markdown]
-# Run the model:
-
-# %%
-gls_model = GLSPhenoplier(
-    smultixcan_result_set_filepath=output_filepath,
-)
-
-gls_model.fit_named(lv_code, phenotype_code)
-
-# %% [markdown]
-# It will take a few seconds. You can show the entire table of results from the [statsmodels's GLS model](https://www.statsmodels.org/stable/generated/statsmodels.regression.linear_model.GLS.html):
-
-# %%
-res = gls_model.results
-
-# %%
-print(res.summary())
-
-# %% [markdown]
-# This p-value (`3.76e-08`) is a two-sided test on the LV coefficient (`0.0698`).
-# You can see that the p-value is slightly less significant than the p-value from the Pearson correlation that we computed before.
-
-# %% [markdown]
-# This is how you can access the model's estimated parameters.
+# %% tags=[] scrolled=true language="bash"
+# # remove previously computed results (if exist)
+# rm /tmp/gls_phenoplier-basophill_percentage.tsv.gz
 #
-# These are the coefficients:
-
-# %%
-res.params
+# # print full path of gls_cli.py tool
+# echo ${PHENOPLIER_CODE_DIR}/libs/gls_cli.py
+#
+# # print full path of gene correlations file (which is trait-specific!)
+# COHORT_NAME="phenomexcan_rapid_gwas"
+# REFERENCE_PANEL="gtex_v8"
+# GENE_CORR_DIR="${PHENOPLIER_RESULTS_GLS}/gene_corrs/cohorts/${COHORT_NAME}/${REFERENCE_PANEL}/mashr/gene_corrs-symbols-within_distance_5mb.per_lv/"
+# echo ${GENE_CORR_DIR}
+#
+# python ${PHENOPLIER_CODE_DIR}/libs/gls_cli.py \
+#   --input-file /tmp/smultixcan_30220_raw_ccn30.tsv.gz \
+#   --duplicated-genes-action keep-first \
+#   --gene-corr-file ${GENE_CORR_DIR} \
+#   --debug-use-sub-gene-corr \
+#   --covars gene_size gene_size_log gene_density gene_density_log \
+#   --output-file /tmp/gls_phenoplier-basophill_percentage.tsv.gz
 
 # %% [markdown]
-# The one-sided p-values (the ones we used in our manuscript, since we are only interested in the top genes of an LV):
-
-# %%
-res.pvalues_onesided
+# As you can see from the output, the tool performs some preprocessing of the input TWAS file, and then computes an association for all LVs in the model (987 in our case) and the gene p-values from TWAS. The output is finally written to the path specified.
+#
+# **IMPORTANT:** keep in mind that you have to use a gene correlation matrix that is specific to your TWAS results. This is because gene correlations depend on the variants present in the original GWAS used. Check out [the notebooks here](https://github.com/greenelab/phenoplier/tree/main/nbs/15_gsa_gls) to see how to compute a gene correlation matrix specific for your trait of interest.
 
 # %% [markdown]
-# And the two-sided p-values (in case you need them or are interested in a different hypothesis):
+# # Load LV-trait results
 
 # %%
-res.pvalues
+lv_df = pd.read_csv("/tmp/gls_phenoplier-basophill_percentage.tsv.gz", sep="\t")
+
+# %%
+lv_df
+
+# %% [markdown]
+# As you can see, LV603 is at the top of the LVs associations for basophill percentage.
+# However, the onesided p-value here (`5.32e-15`) is larger than a simple correlation (`2.94e-27`), suggesting that we have correlated genes at the top of the LV.
 
 # %% [markdown]
 # # Conclusions
@@ -304,7 +253,7 @@ res.pvalues
 # %% [markdown]
 # Hopefully, now have a more clear idea of the main data matrixes involved in PhenoPLIER (matrix Z, PhenomeXcan gene-trait associations, etc).
 # We also see how to compute a p-value between an LV (group of genes or gene module) and a trait of interest.
-# To do this with your own data, you only need to compute the S-MultiXcan TWAS results (gene-based) from your GWAS summary stats.
+# To do this with your own data, you need to compute the S-MultiXcan TWAS results (gene-based) from your GWAS summary stats and generate your own gene correlation matrix.
 #
 # In the next notebook (`02-LV_cell_types-...`), we'll see how to check in which tissues or cell types are our LV603'genes expressed.
 
