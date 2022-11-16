e58a9022853ac4a671d5a2f7162753ca2f224c88
Milton Pividori <miltondp@gmail.com>
2022-11-15 20:27:38 -0500

    environment: update Python to 3.10 and fix R dependencies

--- environment/scripts/install_r_packages.r
+++ environment/scripts/install_r_packages.r
@@ -1,30 +1,31 @@
 # This script installs R packages. When installing BiocManager, the script updates all R packages
 # currently installed (options update=TRUE, ask=FALSE in BiocManager::install).
 
-default_repo = 'http://cran.us.r-project.org'
+default_repo <- "http://cran.us.r-project.org"
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager", repos=default_repo)
-BiocManager::install(version = "3.16", update=TRUE, ask=FALSE)
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager", repos = default_repo)
+}
+BiocManager::install(version = "3.16", update = TRUE, ask = FALSE)
+
+# BiocParallel: it's a dependency for some packages like fgsea
+BiocManager::install("BiocParallel", update = FALSE, ask = FALSE)
 
 # styler
 BiocManager::install("styler", update = FALSE, ask = FALSE)
 
 # clusterProfiler
-BiocManager::install("clusterProfiler", update=FALSE, ask=FALSE)
+BiocManager::install("clusterProfiler", update = FALSE, ask = FALSE)
 
 # org.Hs.eg.db
-BiocManager::install("org.Hs.eg.db", update=FALSE, ask=FALSE)
+BiocManager::install("org.Hs.eg.db", update = FALSE, ask = FALSE)
 
 # clustree
-BiocManager::install("clustree", update=FALSE, ask=FALSE)
+BiocManager::install("clustree", update = FALSE, ask = FALSE)
 
 # qqman
-BiocManager::install("qqman", update=FALSE, ask=FALSE)
+BiocManager::install("qqman", update = FALSE, ask = FALSE)
 
 # fgsea
-devtools::install_github("ctlab/fgsea", ref="v1.19.2")
-
-# PLIER
-#install_github("wgmao/PLIER", ref="v0.1.4")
+devtools::install_github("ctlab/fgsea", ref = "v1.19.2")
 
