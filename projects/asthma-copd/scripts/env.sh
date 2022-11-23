9a95a9067c641e9ea3f8ceb5c819fe806ae08e88
Milton Pividori <miltondp@gmail.com>
2022-11-23 15:47:18 -0500

    asthma-copd: update env.sh

--- projects/asthma-copd/scripts/env.sh
+++ projects/asthma-copd/scripts/env.sh
@@ -8,12 +8,12 @@
 # Your settings here
 #
 
+# Project name
+PROJECT_NAME="asthma-copd"
+
 # BASE_DIR is the parent directory where the code and manuscript repos are
 # located.
-BASE_DIR=/home/miltondp/projects/phenoplier/projects/asthma-copd
-
-# Project name
-PROJECT_NAME=asthma-copd
+BASE_DIR=/home/miltondp/projects/phenoplier/projects/${PROJECT_NAME}
 
 # Number of CPUs to use
 export PHENOPLIER_N_JOBS=10
@@ -25,13 +25,15 @@ DOCKER_IMAGE_TAG="asthma-copd"
 # Do not edit below
 #
 export PHENOPLIER_ROOT_DIR=${BASE_DIR}/code/base
-echo "PHENOPLIER_ROOT_DIR=${PHENOPLIER_ROOT_DIR}"
+echo -e "PHENOPLIER_ROOT_DIR=\t\t${PHENOPLIER_ROOT_DIR}"
 
 export PHENOPLIER_MANUSCRIPT_DIR=${BASE_DIR}/manuscript
-echo "PHENOPLIER_MANUSCRIPT_DIR=${PHENOPLIER_MANUSCRIPT_DIR}"
+echo -e "PHENOPLIER_MANUSCRIPT_DIR=\t${PHENOPLIER_MANUSCRIPT_DIR}"
+
+echo -e "PHENOPLIER_N_JOBS=\t\t${PHENOPLIER_N_JOBS}"
 
 export PHENOPLIER_DOCKER_IMAGE_TAG="${DOCKER_IMAGE_TAG:-latest}"
-echo "PHENOPLIER_DOCKER_IMAGE_TAG=${PHENOPLIER_DOCKER_IMAGE_TAG}"
+echo -e "PHENOPLIER_DOCKER_IMAGE_TAG=\t${PHENOPLIER_DOCKER_IMAGE_TAG}"
 
 export PYTHONPATH=${BASE_DIR}/code/libs/:${PYTHONPATH}
-echo "PYTHONPATH=${PYTHONPATH}"
+echo -e "PYTHONPATH=\t\t\t${PYTHONPATH}"
