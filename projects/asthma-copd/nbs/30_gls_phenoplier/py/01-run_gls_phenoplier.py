64f96a3101698d49b0247651338f665408a4e3da
Milton Pividori <miltondp@gmail.com>
2022-12-02 09:29:04 -0500

    asthma-copd/gls: fix indentation in bash scripts

--- projects/asthma-copd/nbs/30_gls_phenoplier/py/01-run_gls_phenoplier.py
+++ projects/asthma-copd/nbs/30_gls_phenoplier/py/01-run_gls_phenoplier.py
@@ -46,46 +46,46 @@ display(PHENOPLIER_NOTEBOOK_DIR)
 
 # %% tags=[] language="bash"
 # run_job () {
-#   # read trait information
-#   IFS=',' read -r pheno_id desc file sample_size n_cases <<< "$1"
+#     # read trait information
+#     IFS=',' read -r pheno_id desc file sample_size n_cases <<< "$1"
 #
-#   # CODE_RELATIVE_DIR="$1"
-#   CODE_DIR="${PHENOPLIER_NOTEBOOK_DIR}"
+#     # CODE_RELATIVE_DIR="$1"
+#     CODE_DIR="${PHENOPLIER_NOTEBOOK_DIR}"
 #
-#   # GWAS_DIR=${PHENOPLIER_PROJECTS_ASTHMA_COPD_RESULTS_DIR}/final_imputed_gwas
-#   INPUT_FILENAME=${file%.*}
-#   GENE_CORR_FILE="${PHENOPLIER_PROJECTS_ASTHMA_COPD_RESULTS_DIR}/gls_phenoplier/gene_corrs/${pheno_id}/gene_corrs-symbols.per_lv"
+#     # GWAS_DIR=${PHENOPLIER_PROJECTS_ASTHMA_COPD_RESULTS_DIR}/final_imputed_gwas
+#     INPUT_FILENAME=${file%.*}
+#     GENE_CORR_FILE="${PHENOPLIER_PROJECTS_ASTHMA_COPD_RESULTS_DIR}/gls_phenoplier/gene_corrs/${pheno_id}/gene_corrs-symbols.per_lv"
 #
-#   SMULTIXCAN_DIR="${PHENOPLIER_PROJECTS_ASTHMA_COPD_RESULTS_DIR}/twas/smultixcan"
-#   SMULTIXCAN_FILE="${SMULTIXCAN_DIR}/${INPUT_FILENAME}-gtex_v8-mashr-smultixcan.txt"
+#     SMULTIXCAN_DIR="${PHENOPLIER_PROJECTS_ASTHMA_COPD_RESULTS_DIR}/twas/smultixcan"
+#     SMULTIXCAN_FILE="${SMULTIXCAN_DIR}/${INPUT_FILENAME}-gtex_v8-mashr-smultixcan.txt"
 #
-#   OUTPUT_DIR="${PHENOPLIER_PROJECTS_ASTHMA_COPD_RESULTS_DIR}/gls_phenoplier/gls"
-#   mkdir -p "${OUTPUT_DIR}"
+#     OUTPUT_DIR="${PHENOPLIER_PROJECTS_ASTHMA_COPD_RESULTS_DIR}/gls_phenoplier/gls"
+#     mkdir -p "${OUTPUT_DIR}"
 #
-#   OUTPUT_FILENAME_BASE="${INPUT_FILENAME}-gls_phenoplier"
+#     OUTPUT_FILENAME_BASE="${INPUT_FILENAME}-gls_phenoplier"
 #
-#   LOGS_DIR="${CODE_DIR}/jobs_output"
-#   mkdir -p "${LOGS_DIR}"
+#     LOGS_DIR="${CODE_DIR}/jobs_output"
+#     mkdir -p "${LOGS_DIR}"
 #
-#   # make sure we are not also parallelizing within numpy, etc
-#   export NUMBA_NUM_THREADS=1
-#   export MKL_NUM_THREADS=1
-#   export OPEN_BLAS_NUM_THREADS=1
-#   export NUMEXPR_NUM_THREADS=1
-#   export OMP_NUM_THREADS=1
+#     # make sure we are not also parallelizing within numpy, etc
+#     export NUMBA_NUM_THREADS=1
+#     export MKL_NUM_THREADS=1
+#     export OPEN_BLAS_NUM_THREADS=1
+#     export NUMEXPR_NUM_THREADS=1
+#     export OMP_NUM_THREADS=1
 #
-#   echo "Running for $pheno_id"
-#   echo "Saving results in ${OUTPUT_DIR}/${OUTPUT_FILENAME_BASE}.tsv.gz"
-#   echo "Saving logs in ${LOGS_DIR}/${OUTPUT_FILENAME_BASE}.log"
+#     echo "Running for $pheno_id"
+#     echo "Saving results in ${OUTPUT_DIR}/${OUTPUT_FILENAME_BASE}.tsv.gz"
+#     echo "Saving logs in ${LOGS_DIR}/${OUTPUT_FILENAME_BASE}.log"
 #
-#   bash "${PHENOPLIER_CODE_DIR}/scripts/gls_phenoplier.sh" \
-#     --input-file "${SMULTIXCAN_FILE}" \
-#     --gene-corr-file "${GENE_CORR_FILE}" \
-#     --covars "gene_size gene_size_log gene_density gene_density_log" \
-#     --debug-use-sub-gene-corr 1 \
-#     --output-file "${OUTPUT_DIR}/${OUTPUT_FILENAME_BASE}.tsv.gz" > "${LOGS_DIR}/${OUTPUT_FILENAME_BASE}.log" 2>&1
+#     bash "${PHENOPLIER_CODE_DIR}/scripts/gls_phenoplier.sh" \
+#         --input-file "${SMULTIXCAN_FILE}" \
+#         --gene-corr-file "${GENE_CORR_FILE}" \
+#         --covars "gene_size gene_size_log gene_density gene_density_log" \
+#         --debug-use-sub-gene-corr 1 \
+#         --output-file "${OUTPUT_DIR}/${OUTPUT_FILENAME_BASE}.tsv.gz" > "${LOGS_DIR}/${OUTPUT_FILENAME_BASE}.log" 2>&1
 #
-#   echo
+#     echo
 # }
 #
 # # export function so GNU Parallel can see it
